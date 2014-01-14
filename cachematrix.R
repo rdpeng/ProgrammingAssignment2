27987823fcf81d46a5e2186391addd87c079b879
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-01-14 17:08:52 -0500

    Initial commit for Programming Assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,10 @@
+## [Put comments here that describe what your functions do]
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
