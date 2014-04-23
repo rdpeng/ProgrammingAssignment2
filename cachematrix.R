6afb603aeee8cc374398e31c2a5a05f36eaedb3e
nopy4 <avitro@gmail.com>
2014-04-23 16:20:17 +0400

    avitro
    avitro

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeChacheMatrix function creates a "special" matrix, that allows set its value, its inverse 
+# value, and also to get those values
 
-## Write a short comment describing this function
+# cacheSolve functions takes as an argument a "special" matrix created by makeCacheMatrix function
+# and checks whether the inversed value for it was calculated and cached: if it was, it returns it,
+# otherwise it calculates it, caches it in a "special" matrix and returns it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinversed <- function(inversed) m <<- inversed
+    getinversed <- function() m
+    list(set = set, get = get,
+         setinversed = setinversed,
+         getinversed = getinversed)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinversed()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinversed(m)
+    m
 }
