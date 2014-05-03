2460fc1709fb80ac27f0b130b6f857338d52f934
rafavelezpr <rafael.velez@hp.com>
2014-05-02 20:04:00 -0700

    Create makeCacheMatrix.R

---
+++ makeCacheMatrix.R
@@ -0,0 +1,18 @@
+makeCacheMatrix <- function(x=numeric()){
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+      
+        list (set=set, get=get,
+            setinverse = setinverse
+            getinverse = getinverse
+        
+}        
+        
+        
