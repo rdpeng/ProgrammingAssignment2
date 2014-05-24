8ddc0b0be2dd576d49ca0d54e83e9e8e3c0c8b11
mularz <MULARZ@MM206123-PC.MITRE.ORG>
2014-05-24 12:40:10 -0400

    Implemented the two prototype functions - makeCacheMatrix and cacheSolve.
    This implementation makes it possible to solve the inverse of any square matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -4,18 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    inverse <- NULL
-    set <-function(y) {
-        x<<-y
-        inverse<<- NULL
-    }
-    get <- function() x
-    setInverse<- function(solve) inverse <<- solve
-    getInverse <- function() inverse
-    list (set = set, 
-          get = get,
-          setInverse = setInverse,
-          getInverse = getInverse)
+
 }
 
 
@@ -23,13 +12,4 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    inverse <- x$getInverse()
-    if(!is.null(inverse)) {
-        message("getting cached inverse")
-        return(inverse)
-    }
-    matrix <- x$get()
-    inverse <- solve(matrix)
-    x$setInverse(inverse)
-    inverse
 }
