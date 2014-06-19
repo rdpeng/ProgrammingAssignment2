72516cbd267b9f9a92779a579aa14010819fcf9b
diegohornamunoz <diego.horna.munoz@gmail.com>
2014-06-18 21:15:57 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+#R-Programming
+#Programming Assignment 2
+#by Diego Horna Munoz
 
-## Write a short comment describing this function
+#Clearing the workspace
+rm(list=ls())
+
+#Setting the working directory
+setwd(dir="/Volumes/Diego/R/ProgrammingAssignment2/")
+
+#Verifying the working directory
+getwd()
+
+# This function creates a special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+# This function calculates the inverse of a special matrix created by the 
+# above function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data,...)
+    x$setinv(inv)
+    inv
 }
