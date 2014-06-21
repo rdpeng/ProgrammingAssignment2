eb0dd2098ef3f0297d2bc1ac62a87e4b282257b9
terencejohn2003 <tcd@humansystems.net>
2014-06-21 12:31:40 +0100

    Personalize the file template
    Change the instructions to generic descriptions of the functions that I
    hope to be able to create

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following pair of functions cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by the "makeCacheMatrix" function described above.
+## If the inverse has already been calculated and the matrix has not been changed,
+## then cacheSolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
