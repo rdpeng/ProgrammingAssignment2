c3e50f88eacbff9d18e6f6c718f471d0a2f192ea
David Parker <davparker@gmail.com>
2014-07-16 04:12:35 -0500

    rigorous testing of Programmiing Assignment 2 with cached inverse matrix and matrix multiplication

--- test-assignment.R
+++ test-assignment.R
@@ -1,5 +1,7 @@
-# Exercise to test samples???
+# Exercise to test matrices
+# Coursera provided bulk
 
+setwd("~/GitHub/DataScienceSpecialization/ProgrammingAssignment2")
 source("cachematrix.R")
 
 # create cacheable matrix object
@@ -21,7 +23,7 @@ solve(m$get)
 # Error in as.vector(x, mode) : 
 #    cannot coerce type 'closure' to vector of type 'any'
 solve(m$get())
-# [,1] [,2]
+#      [,1] [,2]
 # [1,]  0.0  0.5
 # [2,]  0.5  0.0
 
@@ -53,7 +55,7 @@ all.equal( diag(2), m$get() %*% cacheSolve(m) )
 #[1] TRUE             <-- R agrees it's an identity
 
 # save the inverse off and let's see if we can break it...
-m1<- cacheSolve(m)
+m1 <- cacheSolve(m)
 
 # set m to some new values
 m$set( matrix( rnorm(4), 2, 2) )
@@ -68,11 +70,74 @@ all.equal( diag(2), m$get() %*% m1 )
 
 # try a bigger matrix and see if we can notice the caching effects
 m$set( matrix( rnorm( 1000000 ), 1000, 1000 ) )
+m$get()
 cacheSolve(m)
 cacheSolve(m)
 # on my quad i5, seems like the second call is faster...
 
 # and double check it all still works...
 all.equal( diag( 1000 ), m$get() %*% cacheSolve(m) )
+# getting cached data
+# [1] TRUE
+
+###########
+# rproj-005
+###########
+# so there's no confusion
+rm(list=ls())
+setwd("~/GitHub/DataScienceSpecialization/ProgrammingAssignment2")
+source("cachematrix.R")
+
+a <- makeCacheMatrix(matrix(rnorm(16),4,4,
+     dimnames=list(NULL,c("A","B","C","D"))))
+
+a$get()
+
+cacheSolve(a)
+
+cacheSolve(a)
+# getting cached inverse matrix
+
+# product of matrix mult should be identity matrix AND we should get the cached message
+a$get() %*% cacheSolve(a)
+# getting cached inverse matrix
+
+
+all.equal( diag(2), a$get() %*% cacheSolve(a) )
+# getting cached inverse matrix
+# [1] "Attributes: < Component “dim”: Mean relative difference: 1 >"
+# [2] "Numeric: lengths (4, 16) differ" 
+
+# save the inverse off and let's see if we can break it...
+a1 <- cacheSolve(a)
+
+# set a to some new values
+a$set( matrix( rnorm(4),2,2) )
+a$get()
+
+# does fetching the inverse without any pre-caching work?
+all.equal( diag(2), a$get() %*% cacheSolve(a) )
+#[1] TRUE             <-- yup, sure does...
+
+# what about testing the inverse we squirreled away in m1?
+all.equal( diag(2), a$get() %*% a1 )
+# Error in a$get() %*% a1 : non-conformable arguments
+#[1] "Mean relative difference: xxxx"  <-- Nah... that's a train wreck.  
+
+a$set( matrix( rnorm( 1000000 ), 1000, 1000 ) )
+head(a$get())
+dim(a$get())
+
+system.time(cacheSolve(a))
+# user  system elapsed 
+# 0.97    0.00    0.97 
+system.time(cacheSolve(a))
+# getting cached inverse matrix
+# user  system elapsed 
+# 0.01    0.00    0.01 
+# the second call much is faster!!!
+
+# and double check it all still works...
+all.equal( diag( 1000 ), a$get() %*% cacheSolve(a))
 # getting cached data
 # [1] TRUE
