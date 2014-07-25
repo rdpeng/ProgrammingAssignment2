37fc7ef57f1a6d1f2153dad226629ec50a459df2
eyeampraveen <eyeampraveen@gmail.com>
2014-07-25 21:47:24 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -23,7 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-cachemean <- function(x = matrix(), ...) {
+cacheSolve <- function(x = matrix(), ...) {
         m <- x$getmatrixinv()
         if(!is.null(m)) {
                 message("getting cached data")
