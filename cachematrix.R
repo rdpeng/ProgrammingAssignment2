fdbeb41931f513bc2114aea85cd7e14963584f9d
scientistanton <anton.wasson@gmail.com>
2014-07-27 16:32:07 +1000

    Edits made to complete the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are based on the 'makeVector' and 'cachemean' from the examples in the Programming Assignment 2 of the R Programming Course (Coursera, July 2014).
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        im <- NULL # 'im' for inverse matrix
+        set <- function(y) {
+                x <<- y
+                im <<- NULL 
+                # cache im object as NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) im <<- solve 
+        # cache the inverse matrix in the im object
+        getinverse <- function() im
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special “matrix” returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        im <- x$getinverse()
+        # if the function has already been run the im variable will not be
+        # NULL
+        if(!is.null(im)) {
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data, ...)
+        x$setinverse(im)
+        # if being run for the first time, this will cause the im object to 
+        # be cached with the solved inverse matrix.
+        im
 }
