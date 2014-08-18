be07732d17339701671b9d68e8fd096e95f6293f
Nelson Brown <NBrown@Viejas.com>
2014-08-18 16:00:28 -0700

    Added the code to create the CacheMatrix inverse functionality

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions will return the inverse of a matrix that has been passed to it. 
+## It will first attempt to return a cached version of the value if it has been previously
+## computed. If the matrix value has not been computed or has been changed it will compute it.
 
-## Write a short comment describing this function
+## This function manages the input matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        i <- NULL ## Set the inverse matrix object to NULL
+        
+        ## This function sets the initial value of the matrix
+        ## and sets the inverse object to NULL
+
+        set <- function(m) { ## m contains the new matrix value 
+                
+                x <<- m ## Store the new matrix value in x
+                
+                i <<- NULL ## Set the inverse 
+        }
+        
+        get <- function() x
+        
+        setinverse <- function(solve) i <<- solve
+        
+        getinverse <- function() i
+        
+        list(set = set, get = get,
+             
+             setinverse = setinverse,
+             
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function will test for the existence of a cached value and return it if found
+## otherwise it will call the above function to perform the computation required 
+
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        
+        if(!is.null(i)) {
+                message("getting cached data")
+                return (i) ## Return the cached value
+        }
+        
+        data <- x$get() ## Get the new matrix values
+        
+        i <- solve(data, ...) ## Calculate the inverse matrix value
+        
+        x$setinverse(i) ## Cache the new inverse value
+        
+        i ## Return a matrix that is the inverse of 'x'
+        
 }
