d889fc48872bc8c3aa92ed91d3979c75eb021437
mayankagarwal123 <mayankagarwal123@users.noreply.github.com>
2014-09-18 08:57:17 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
-
+## makecacheMatrix() creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        
+        ## Method to set the matrix
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        ## Method the return the matrix
+        get <- function() {
+                x      
+        } 
+        
+        ## Method to set the inverse of the matrix
+        setinv <- function(m_inv) {
+                inv<<-m_inv  
+        } 
+        
+        ## Method to return the inverse of the matrix
+        getinv <- function() {
+                inv
+        }
+        
+        ## Returning a special matrix object containing methods to cache inverse
+        matrix(c(set,get,setinv,getinv),2,2)
+        
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve() computes the inverse of the special "matrix" returned by makeCacheMatrix()
+## If the inverse has already been calculated, then cachesolve() retrieves
+## the inverse from the cache
+cacheSolve <- function(x,...){
+       
+        x<-as.list(x)
+        inv <- x[[4]]()
+        
+        ## Return the inverse from cache if its already set
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        
+        ## Get the matrix from our object
+        data <- x[[2]]()
+        
+        ## Calculate the inverse using solve()
+        inv <-solve(data,...)
+        
+        ## Set the inverse to the object
+        x[[3]](inv)
+        
+        ## Return the inverse matrix
+        inv
 }
