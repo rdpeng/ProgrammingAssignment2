90189aebdbeabd14e5be0f3331a4222744f677e5
wigob81 <bertramw@staff.uni-marburg.de>
2014-09-19 21:10:06 +0200

    makeMatrix
    calculates and caches the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function: 
-
-makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
+makeMatrix <- function(x = numeric()) { # defines the funktion "makematrix" with the numeric argument x 
+        m <- NULL # initializes the variable m
+        set <- function(y) {  # defines the funktion "set" with the argument y. Set is a subfunktion of "makematrix"
+                x <<- y # associates the argument y of the function "set" with the argument x of the funktion "makematrix" in the parent environment
+                m <<- NULL # initializes the variable m in the parent environment
         }
-        get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
+        get <- function() x # defines the function "get" without arguments. x is returned as matrix
+        setinverse <- function(inverse) m <<- inverse # defines the function "setinverse", which contains the variable "inverse" as an argument, which is written into m in the parent environment
+        getinverse <- function() m # analoguous to the function "get". m is passed to the function as an object which contains the variable "inverse"
         list(set = set, get = get,
              setinverse = setinverse,
-             getinverse = getinverse)
+             getinverse = getinverse)# generates the special vector x, i.e. a list of functions
 }
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        m <- x$getinverse()
-        if(!is.null(m)) {
+cacheinverse <- function(x, ...) { # defines the function "cacheinverse" with the argument x and ...  as placeholder for additional arguments
+        m <- x$getinverse() # the function "getinverse", being a part of the vector x, is written into the object m
+        if(!is.null(m)) { # if m (the cache for the inverse matrix) is not empty, m and the message are returned
                 message("getting cached data")
                 return(m)
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        data <- x$get() # if m is empty, the matrix is written into the object "data", bei applying the "get" function from the list x to it
+        m <- solve(data, ...) # the matrix "data" is inverted and written into m
+        x$setinverse(m) # the function "setinverse" from the list x is applied to m 
+        m # m is returned
 }
+
+
