a501f49a1bbe439f4d6c63ee2a9850ea6e58524c
kpvichare <kpvichare@gmail.com>
2014-10-26 11:48:34 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,20 @@
+## Programming Assignment 2: Lexical scoping
+
+## Taking inverse of matrix repeatedly is usually a costly computation.
+
+## Caching the inverse of a matrix may beneficial rather than computing it repeatedly.
+
+## The "makeCacheMatrix" function creates a special matrix object, 
+## that can be used to cache it's inverse.
+
+## The "cacheSolve" function computes the inverse of the matrix,
+## defined in "makeCacheMatrix" function. 
+
+## If the inverse has already been calculated and the matrix has not changed, 
+## it retrieves from cache memory and gives appropriate message.
+
 makeCacheMatrix <- function(x = matrix()){
+  
   m <- NULL
   setm <- function(y){
     x <<- y
