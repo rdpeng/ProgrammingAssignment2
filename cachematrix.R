edba9b611ccbbf095017cae9de03f1f603352a4e
trieu <thtrieu@apcs.vn>
2014-10-26 16:07:17 +0700

    First change
    All features checked
    No comment yet

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(newx) {
+    x <<- newx
+    inverse <<- NULL
+  }
+  setinverse = function(newinv) {
+    inverse <<- newinv
+  }
+  get <- function() x
+  getinverse <- function() inverse
+  list(set = set, setinverse = setinverse, get = get, getinverse = getinverse)
 }
 
 
@@ -12,4 +23,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv = x$getinverse()
+  if (!is.null(inv)) {
+    message("getting cached value")
+    return (inv)
+  }
+  inv = solve(x$get(), ...)
+  x$setinverse(inv) 
+  inv
 }
