68d36ac43c0cbf48f3b5496fc110a6bf8073022c
Juan <juan.trujillopez@gmail.com>
2014-11-20 22:18:09 +0100

    Delete 1.18.R

--- R Programming/1.18.R
+++
@@ -1,5 +0,0 @@
-table <- read.table("hw1_data.csv",sep=",",stringsAsFactors=FALSE,skip=1)
-ozone <- table$V1
-temp <- table$V4
-values <- subset.data.frame(table, ozone >31 & temp >90)
-mean(values$V2)
