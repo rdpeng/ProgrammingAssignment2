6d0e5f71eba2b162b60eb52467c266d9a9adbe8c
86TU <BDMcCollum@gmail.com>
2014-11-20 19:06:07 -0500

    The R code has been updated for the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The storematrix function below contains 
+##four functions that perform the following:
+##1. Sets the value of a matrix
+##2. Gets the value of a matrix
+##3. Sets the inverse of a matrix
+##4. Gets the inverse of a matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+storematrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##The cachematrix function below contains
+##one function which does the following:
+##1. Returns the cached inverse of a matrix.
+##Additionally it computes, stores, and displays
+##the inverse of matrices not previously computed.
+
+cachematrix <- function(x, ...) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+
+
+
+##An example of these functions in use would be as follows:
+##startmatrix<-matrix(1:4,2,2)
+##stored<-storematrix(startmatrix)
+##cachematrix(stored)
