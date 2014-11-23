5c847533f6703cec2964e8013e2a2d4f7614e192
Shilpa Yakalaspur Andanappa <shilpa.ae@gmail.com>
2014-11-23 22:33:13 +0000

    Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## set the value of the matrix
   m<-NULL
   set<-function(y){
     x<<-y
     m<<-NULL
   }
+  ## get the value of the matrix
   get<-function() x
   setmatrix<-function(solve) m<<- solve
   getmatrix<-function() m
+  
+  ## get the inverse of the matrix
+  
   list(set=set, get=get,
        setmatrix=setmatrix,
        getmatrix=getmatrix)
