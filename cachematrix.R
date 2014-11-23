377cf84a339691fbc11afaf4723bcd735bdcb589
AylaYala <AylaYala@users.noreply.github.com>
2014-11-23 00:03:41 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
-makeCacheMatrix <- function(m = matrix() ) {
+makeCacheMatrix <- function(m = matrix() ) {      ## create a function for the matrix, set the matrix and put the inverse property
     i <- NULL
     set <- function(matrix) {
             m <<- matrix
             i <<- NULL
     }
-    get <- function() {
+    get <- function() {             ## get the function, set inverse of th matrix and get the inverse of the matrix
   	m
     }
     setInverse <- function(inverse) {
@@ -14,7 +14,7 @@ makeCacheMatrix <- function(m = matrix() ) {
         i
     }
 
-    list(set = set, get = get,
+    list(set = set, get = get,       ## make a list
          setInverse = setInverse,
          getInverse = getInverse)
 }
