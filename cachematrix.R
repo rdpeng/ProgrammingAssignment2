a1c0737610247f40c8156706867e74d853b269d7
Kamila Szumilak <kamila.szumilak@gmail.com>
2014-11-23 13:24:57 +0100

    Ready solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Function checks if we have inv x calculated. 
+
+makeCacheMatrix <- function(x = matrix()) 
+  {
+  invx <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    invx <<- NULL
+  }
+  get <- function() {x}
+  setinv <- function(solve) {invx <- solve}
+  getinv <- function() {invx}
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  }
+
+
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+{
+   invx <- x$getinv()
+   if(!is.null(invx))
+   {
+     message ("getting cached data")
+     return(inxv)
+   }
+   data <-x$get()
+   invx <- solve(x, ...)
+   x$setinv(invx)
+   invx
         ## Return a matrix that is the inverse of 'x'
 }
