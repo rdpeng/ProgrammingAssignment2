058b69ca523956bf8f64bb9d2d5c6fdd9e18826b
jairgs <jair.garzas@gmail.com>
2014-12-02 10:24:28 -0600

    Vector Mean changed to Matrix Inverse
    With this change the program no longer calculates the mean of a vector
    but the Inverse of a Matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is an object with 3 functions: get, setinv and getinv
+## which are called by another function named cacheSolve. The object cacheSolve
+## is a function which calls the list of functions in makeCacheMatrix. 
 
-## Write a short comment describing this function
+## This "object list" has the main objective of storing a matrix and its
+## inverse. The list of functions are accessed by another function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL  
+  get <- function() x
+  setinv <- function(inv) inv <<- inv
+  getinv <- function() inv
+  list(get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## This function calculates the inverse of the matrix defined in the object
+## makeCacheMatrix. If it's already stored it returns a message and retrieves the 
+## value; otherwise, it makes the calculation and stores the value in the 
+## makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
