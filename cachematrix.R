b20e1ad9d1454b6f20897a7e7496b7a24ec74e0d
mogliang <mogliang@gmail.com>
2014-12-14 10:54:13 +0800

    https://class.coursera.org/rprog-017 ex2
    https://class.coursera.org/rprog-017 execise2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+	rx=NULL
+	get=function()x
+	set=function(y){
+		x<<-y
+		rx<<-NULL
+	}
+	getSolve=function()rx
+	setSolve=function(ry)rx<<-ry
+	list(get=get,set=set,getSolve=getSolve,setSolve=setSolve)
 }
 
-
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x, ...){
+	s=x$getSolve()
+	if(!is.null(s)){
+		return(s)
+	}
+	x$setSolve(solve(x$get()))
+	x$getSolve()
 }
