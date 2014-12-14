2f7080551be21b0e12b2111a229e866bf779279c
Swaraj2904 <swarajpaul29@gmail.com>
2014-12-14 14:17:12 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function caches and returns the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
