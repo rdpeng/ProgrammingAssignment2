484e95128e8473d299559bd5e1e41a482c113832
Selda <seldacodes@users.noreply.github.com>
2014-12-17 16:34:06 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,9 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+        x<<-y
+        inv<<-NULL
+        }
 
+get<-function()x
+setinverse <- function(inverse)inv <<-inverse
+getinverse <- function() inv
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
 
 ## Write a short comment describing this function
 
