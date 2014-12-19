8120708d45a6ba8026798ad4558a84d5f966134b
Ekaterina <ekaterina.loshchinina@gmail.com>
2014-12-19 01:08:39 +0000

    Execution of assessment guidelines
    Created two working functions as required by the task

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below is a pair of functions that cache the inverse of a matrix.
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+## retrieves the inverse from the cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special object, that stores the matrix user is looking forward to inverse, 
+## and an inversed matrix, when the inversion has been done. The object has three functions:
+##  -- get() - returns the original matrix
+##  -- setInverse() - stores the inversed matrix
+##  -- getInverse() - returns inversed matrix if was previously stored or NULL
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { #input x will be a matrix
+        
+        i <- NULL # placeholder to store inversed matrix; resets to NULL everytime the function is called
+        
+        get <- function() x # returns the original matrix
+        
+        setInverse <- function(inverseMatrix) { # stores the inversed matrix
+                i <<- inverseMatrix
+        }
+        
+        getInverse <- function() i # returns matrix that is alredy cached
+        
+        list(get = get, setInverse = setInverse, getInverse = getInverse) # returns an object with correspondig methods        
 }
 
+## cashSolve gets object, created by makeCacheMatrix, checks whether the inversed matrix has been already stored. 
+## If yes, it returns stored inversed matrix, if no, it calculates the inversed matrix, stores it to the object and 
+## returns it.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { # x is an object created by makeCacheMatrix
+        
+        i <- x$getInverse() # accesses cached matrix
+        
+        if(!is.null(i)) {
+                message("getting cached data") # indicates that cached data was used
+                return(i) # casheSolve returns cached data
+        }
+        
+        # code below get accessed if no cached data has been previously stored
+        
+        matrix <- x$get() # gets matrix from an argument
+        i <- solve(matrix) # inverses this matrix
+        x$setInverse(i) # stores inversed matrix into cache
+        i # function returns inversed matrix
 }
