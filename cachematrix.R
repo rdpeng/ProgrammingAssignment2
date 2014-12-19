f2ab2eddfa4bd42f9059694cca41aece07d8d763
penguinskid <penguinskid@users.noreply.github.com>
2014-12-18 23:21:58 -0500

    Simon Coplan assignment 2
    This is my addition to the

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Just as the assignment requires, my two functions gobble up a matrix, return the 
+##inverse of that matrix, and also cache the inverse so it can be retrieved quickly.
 
-## Write a short comment describing this function
+## This function gobbles up a matrix (and holds onto it) and sets the inverse as NULL.  It also 
+##creates a list of functions that: get the stored matrix, sets the inverse in the cache, and 
+##also gets the inverse the  for the next function "solveCache".  In essense, this function
+##function delivers a set of functions to solveCache and also stores the values created in solveCache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  s<-NULL
+  
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  
+  getoriginalmatrix <- function() {x}
+  
+  setinverse <- function(inverse) {s <<- inverse}
+  
+  getinverse <- function() {s}
+  
+  list(set = set, getoriginalmatrix = getoriginalmatrix, setinverse = setinverse, getinverse = getinverse)
+  
 }
 
+##This function uses the list of functions created in makeCacheMatrix to test if the inverse
+##has been cached. If the cached inverse isn't empty, then solveCache returns the
+##inverse.  Otherwise, solveCache retrieves the original matrix stored in makeCacheMatrix, 
+##solves for the inverse, sets the inverse into the cache, and then returns to inverse so you can see it.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+solveCache <- function(y,...) {
+  
+  s <- y$getinverse()
+  
+  if(!is.null(s)){
+    
+    message("Getting cached inverse")
+  
+    return(s)
+    
+  }
+    
+    originalmatrix <- y$getoriginalmatrix()
+    
+    s <- solve(originalmatrix,...)
+    
+    y$setinverse(s)
+  
+    s        
 }
