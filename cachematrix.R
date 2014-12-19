65078e39db63a7e86bd9ddfca43ae751bbad1f12
Patrick Silva <patrickalex@gmail.com>
2014-12-19 01:15:11 -0200

    Fixes some typos in function "makeCacheMatrix"

--- cachematrix.R
+++ cachematrix.R
@@ -12,13 +12,13 @@
 # 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        
+        s <- NULL
         set <- function(y){
                 x <<- y
                 s <<- NULL
         }
         get <- function x
-        setsolve <<- function(solve) s <<- solve
+        setsolve <- function(solve) s <<- solve
         getsolve <- function() s
         list(set = set,
              get = get,
@@ -34,6 +34,5 @@ makeCacheMatrix <- function(x = matrix()) {
 # from the cache.
 
 cacheSolve <- function(x, ...) {
-        
         ## Return a matrix that is the inverse of 'x'
 }
