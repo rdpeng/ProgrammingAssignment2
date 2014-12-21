6ec0508304e8ca6fe9f4197e0a180a4383ef14a7
Mark Friedman <hsgpgapl@gmail.com>
2014-12-20 22:32:02 -0500

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+## Put comments here that give an overall description of what your functions do
+###############################################################################
 ## Write a short comment describing this function
+
+## No idea.  This assignment was completely mysterious and vague.  I even read 2
+## posts that explained waht it was supposed to be, but I stil didn't hav a clue.
+## I did spend a lovely 4 hours playing with Git to where I could actually set up
+## the fork and have files go up/down.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
+###############################################################################
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
