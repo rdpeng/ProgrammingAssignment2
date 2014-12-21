0dd2a7b8188fde1240265e5fe3fd7f9c58d66d2d
Mark Friedman <hsgpgapl@gmail.com>
2014-12-20 23:31:41 -0500

    more playing around with it

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,52 @@
 ## Put comments here that give an overall description of what your functions do
 ###############################################################################
-## Write a short comment describing this function
-
 ## No idea.  This assignment was completely mysterious and vague.  I even read 2
-## posts that explained waht it was supposed to be, but I stil didn't hav a clue.
+## posts that explained what it was supposed to be, but I stil didn't hav a clue.
 ## I did spend a lovely 4 hours playing with Git to where I could actually set up
 ## the fork and have files go up/down.
+###############################################################################
+## Helper functions provided
+
+###############################################################################
+## Write a short comment describing this function
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+  
 }
 
 ###############################################################################
 ## Write a short comment describing this function
 
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above.
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+    m <- x$getmean()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(solve(m))
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    
+    solve(m)
+    
 }
