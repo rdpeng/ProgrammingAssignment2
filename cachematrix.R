ae18d50b6ac18951bc29fc8042afd0805ccff855
Alberto Martin <amd@tid.es>
2015-01-15 16:42:07 +0100

    Update cachematrix.R
    Add functionality of inverse matrix (almardo@gmail.com, User ID: 9242660)

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+        inv <- NULL
+        setmat <- function(mat)  {
+                x <<- mat
+                inv <<- NULL
+        }
+        getmat <- function() x
+        setinv <- function(minv)  {
+                inv <<- minv
+        }
+        getinv <- function() inv
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## Compare matrix with cache. If matrix already exists and the inverse is calculated
+        if(getinv() != NULL & identical(x, getmat()){
+                return getinv()
+        }
+        ## New matrix or inverse not calculated.
+        else{
+                invMat <- solve(x)
+                setmat(x)
+                setinv(invMat)
+                return(invMat)
+        }
 }
