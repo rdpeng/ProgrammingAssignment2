cdc898bd7828ff2f69981b49b65f0ed52e207fc6
Lukas Snyder <Lukas.Snyder@milliman.com>
2015-01-23 15:19:41 -0500

    Add Comments
    Add Comments to working code

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
 
+## These two function, when called in conjuncture, will either calculate
+## the inverse of a matrix, or, if already calculated, call the inverse
+## matrix from the cache
 
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-z<-matrix(c(1,2,3, 2,3,2, 3,2,1), nrow = 3, ncol = 3)
-h<-as.data.frame(z)
+## This first function creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  
   m <- NULL
   setM <- function(y) {
     x <<- y
@@ -24,11 +23,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+## This second function computes the inverse of the special matrix returned by makeCacheMatrix above.
+## If the inverse has already been calculated, then the cacheSolve should retrieve the inverse from the cache
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
   m<- x$getinv
   if(!is.null(m)) {
     message("getting cached data")
@@ -39,6 +39,3 @@ cacheSolve <- function(x, ...) {
   x$setinv(m)
   m
 }
-
-
-
