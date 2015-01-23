b05f158c64df669c4489ede12a32cbf35b11c7a7
Binamrata1 <binamratah@fractalanalytics.com>
2015-01-23 19:57:16 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  xinv <- NULL 
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL 
+  }
+  
+  get <- function() x 
+  setInv <- function(inv) xinv <<- inv 
+  getInv <- function() xinv 
+  
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  
+  if(!is.null(m)) { 
+    message("getting cached data")
+    return(m) 
+  }
+  data <- x$get() 
+  m <- solve(data) 
+  x$setInv(m) 
+  m
 }
+
+
+test <- matrix(runif(9,1,100),3,3)
+testCached <- makeCacheMatrix(test)
+
+testInv <- cacheSolve(testCached)
