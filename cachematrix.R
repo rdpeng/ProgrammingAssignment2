28a67a4fa75ed503980db64941afb3e8b8ecb95d
Mary Anne <maryanne.thygesen@gmail.com>
2015-01-25 13:25:57 -0800

    homework programming assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+###New comments
+####Make vector to take mean of, How about cats in room
+
+cats <- function(x = numeric() ){
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
 
 ## Write a short comment describing this function
+### catch the cached cats
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,6 +40,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+### I am out of time to catch the cats and debug this program
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
