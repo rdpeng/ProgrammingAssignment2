fe2f41dd13d4e7d705c11bae404c809c9adfddeb
syang1984 <syang1984@users.noreply.github.com>
2015-01-25 13:36:14 -0600

    Update cachematrixassign.R

--- cachematrixassign.R
+++ cachematrixassign.R
@@ -2,44 +2,28 @@
 ## makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
-    x <- matrix(rnorm(64), ncol = 8, nrow = 8)
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
+
 
 ##cachesolve
 
 cacheSolve <- function(x, ...) {
-    solve(x)
-    x 
-}
-##cacheinversion
-
-cacheInversion <- function(x, ...) {
-    start_time <- Sys.time()
-    m <- x$getinversion()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
     }
     data <- x$get()
-    m <- solve(data, ...)
-    x$setinversion(m)
-    end_time <- Sys.time()
-    time_duration <- end_time - start_time
-    sprintf("the time to calculate was %s", time_duration)
-    
-    m
-}
-
-makeMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setinversion <- function(solve) m <<- solve
-    getinversion <- function() m
-    list(set = set, get = get,
-         setinversion = setinversion,
-         getinversion = getinversion)
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
