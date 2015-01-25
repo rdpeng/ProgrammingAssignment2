51414eaa98773b02f091a2dedd48528c518e8cfe
MSamySobihe <m.samy.s@hotmail.com>
2015-01-25 12:50:50 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions are used to find the inverse of a matrix. 
 
-## Write a short comment describing this function
+# This will create an object to save inversed matrix - the funtion getmtrx() will 
+# get the original matrix we need to inverse - function stmtrx() will assign 
+# computed inverse to MtrxInvrsd - function gtmtrx() will get the cached inversed matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+    MtrxInvrsd <- NULL
+    getmtrx <- function() x
+    stmtrx <- function(matrixI) MtrxInvrsd <<- matrixI
+    gtmtrx <- function() MtrxInvrsd   
+    list(getmtrx=getmtrx, stmtrx=stmtrx, gtmtrx=gtmtrx)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+#This Function will get the matrix and inverse it and return the inversed matrix
+
+cacheSolve <- function(x) {
+    MtrxInvrsd <- x$gtmtrx()
+    if(!is.null(MtrxInvrsd)){    
+        return(MtrxInvrsd)
+        }else {        
+        mtrxDTA <- x$getmtrx() 
+        MtrxInvrsd <- solve(mtrxDTA) 
+        x$stmtrx(MtrxInvrsd) 
+        return(MtrxInvrsd)
+    }
 }
