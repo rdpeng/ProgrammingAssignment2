14ef5d0bde8b32df842150cb4eff90689a019e1f
Gonzalo Gasca Meza <gg608f@att.com>
2015-01-25 12:24:43 -0800

    Remove sample functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
-}
-
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
