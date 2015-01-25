a2f00e4af12af7c66e80fcef3240e431850cf4f9
syang1984 <syang1984@users.noreply.github.com>
2015-01-25 13:27:55 -0600

    Update cachematrixassign.R

--- cachematrixassign.R
+++ cachematrixassign.R
@@ -1,1 +1,45 @@
 #Fork a new file
+## makeCacheMatrix
+
+makeCacheMatrix <- function(x = matrix()) {
+    x <- matrix(rnorm(64), ncol = 8, nrow = 8)
+}
+
+##cachesolve
+
+cacheSolve <- function(x, ...) {
+    solve(x)
+    x 
+}
+##cacheinversion
+
+cacheInversion <- function(x, ...) {
+    start_time <- Sys.time()
+    m <- x$getinversion()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinversion(m)
+    end_time <- Sys.time()
+    time_duration <- end_time - start_time
+    sprintf("the time to calculate was %s", time_duration)
+    
+    m
+}
+
+makeMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinversion <- function(solve) m <<- solve
+    getinversion <- function() m
+    list(set = set, get = get,
+         setinversion = setinversion,
+         getinversion = getinversion)
+}
