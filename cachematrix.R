2339b8facb81ef5ad2009cef9497bd9cad98df9c
quitus <quitus@users.noreply.github.com>
2015-02-09 21:44:03 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Function to calculate the invert of a matrix (x)
+makeCacheMatrix <- function(x = matrix()){
+  # Variable to keep track of the cached inverterd matrix
+  cached <- NULL
+  # New function to change the matrix to be used (y)
+  set <- function(y = matrix()){
+    x <<- y
+    cached <<- NULL
+  }
+  # Variable that contains the matrix, either x (original) or y (modified)
+  get <- function() x
+  # Calculates the inverted matrix from x
+  setInvert <- function(solve) cached <<- solve
+  # Stores the inverted matrix
+  getInvert <- function() cached
+  # Lists the different functions within the makeCacheMatrix function
+  list(set = set, get = get, setInvert = setInvert, getInvert = getInvert)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Function to return the cached or recalculated value for the invert of a matrix
+cacheSolve <- function(x, ...){
+  # Reading the cached invert matrix from the makeCacheMatrix function
+  cached <- x$getInvert()
+  # Evaluates whether the value of the variable cached is not NULL 
+  if(!is.null(cached)){
+    message("Recovering the chached data")
+    return(cached)
+  }
+  # If the value from cahed is NULL the new matrix is read into a new variable (newMatrix)
+  newMatrix <- x$get()
+  # The value of the variable cached, which keeps track of the inverted matrix, is rewrited 
+  # to the value of the inverted matrix
+  cached <- solve(newMatrix)
+  # Calculates the inverted matrix from x
+  x$setInvert(cached)
+  # Print the inverted matrix
+  return(cached)
 }
