a340a21bf04ae63d522c1e4f1e767feca741cf00
StreetsefeR <StreetsefeR@users.noreply.github.com>
2015-02-21 17:45:34 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions cache the inverse of a matrix
+## limitation: the matrix supplied must be invertible - error handler is not included
 
-## Write a short comment describing this function
+## creation of special "matrix" object which is designed to store the matrix and related inversed maxrix 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<- function(x = matrix()) {
+  s <- NULL # assigning local m standing for inversed matrix to NULL
+  set <- function(y) { # specifying set subfunction
+    x <<- y  # assigning to x in containing (global in this case) envinronment given value 
+    s <<- NULL	# freeing m globally
+  }
+  get <- function() x	# subfunction returning initial matrix
+  setsolve <- function(solve) s <<- solve	# subfunction "memorazing" or caching invesed matrix
+  getsolve <- function() s	# subfunction for extracting saved inversed matrix
+  list(set = set, get = get, # setting list of subfunctions and their names
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
+## finding of inversed matrix (at first in cache, if not - calculated)
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x, ...) {	# computation of the inverse of the special "matrix"
+  s <- x$getsolve() # retrieving saved inversed matrix
+  if(!is.null(s)) { # if the inversed value was previously saved
+    message("retrieving cache") # indicates the usage of cache, may be omitted
+    return(s)
+  }
+  s <- solve(x$get()) # to evade using intermediary variables saved like matr in equal construct:
+  # matr <- x$get()
+  # s <- solve(matr)
+  x$setsolve(s)	# caching calculated inversed matrix
+  s # returning inverted matrix as a rsult of function
+}	
