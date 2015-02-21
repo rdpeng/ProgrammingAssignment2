19f742f81e417315ac23f11c378d6a7b5e39fdd8
Alina Solovjova <alina@gocardless.com>
2015-02-21 20:53:00 +0000

    Added cacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -32,5 +32,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
 
