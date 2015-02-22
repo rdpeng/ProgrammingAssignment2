3a4db9486bdb349eb807bde8309a71b32658b125
Darjushka <spark.acc@gmail.com>
2015-02-22 22:48:11 +0300

    Assignment 2.
    Create solution of caching  inverse of matrix  problem.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+##==============================================================================##
+## his source allows you to cache the inverse matrix without extra calculations.##
+##   First function helps you to create a specific matrix object with attribute ##
+## inverse matrix.                                                              ##
+##   Second function helps you to get the inverse matrix from created specific  ##
+## matrix object.                                                               ##
+##==============================================================================##
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+##==============================================================================##
+## Function 1.                                                                  ##
+##                                                                              ##
+## Input: standart R-matrix m                                                    ##
+## Output: specific matrix object that can cache its inverse                    ##
+## - with methods: set, get, set.inv, get.inv.                                  ##
+##==============================================================================##
+
+makeCacheMatrix <- function(m = matrix()) {
+# the inverse of matrix m
+        m_inv <- NULL 
+
+# set new matrix value
+	  set <- function(x,...) {
+                m <<- matrix(x,...)
+                m_inv <<- NULL
+        }
+
+# get old matrix value	
+        get <- function() m
+
+# set new inverse matrix value
+        set.inv <- function(solve) m_inv <<- solve
+
+# get olf inverse matrix value
+        get.inv <- function() m_inv
+
+# create a specific matrix object
+        list(set = set, get = get,
+             set.inv = set.inv,
+             get.inv = get.inv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##==============================================================================##
+## Function 2.                                                                  ##
+##                                                                              ##
+## Input: specific matrix object                                                ##
+## Output: inverse of this matrix                                               ##
+##==============================================================================##
+
+cacheSolve <- function(m, ...) {
+# get known inverse matrix value
+	  m_inv <- m$get.inv() 
+
+# try to return known inverse matrix value (if it's known)
+        if(!is.null(m_inv)) {
+                message("Yippee! Getting cached data!")
+                return(m_inv)
+        }
+
+# calculate inverse matrix value (if it's unknown)
+        data <- m$get()
+        m_inv <- solve(data, ...)
+        m$set.inv(m_inv)
+
+# return calculated inverse matrix value
+        m_inv 
 }
