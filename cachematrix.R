64a34c2f5ca160ac3ef870f203212289ea940ee1
exarchiasghost <exarchiasghost@gmail.com>
2015-02-22 23:10:15 +0100

    Remove the examples from the comments.
    And now i am ready to add my very own comments.
    WOW!

--- cachematrix.R
+++ cachematrix.R
@@ -3,19 +3,6 @@
 
 ## Write a short comment describing this function
 
-##makeVector <- function(x = numeric()) {
-##m <- NULL
-##set <- function(y) {
-##  x <<- y
-##  m <<- NULL
-##}
-##get <- function() x
-##setmean <- function(mean) m <<- mean
-##getmean <- function() m
-##list(set = set, get = get,
-##     setmean = setmean,
-##     getmean = getmean)
-#}
 
 makeCacheMatrix <- function(x = matrix()) {
   ma <- NULL 
@@ -33,17 +20,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-##cachemean <- function(x, ...) {
-##  m <- x$getmean()
-##  if(!is.null(m)) {
-##    message("getting cached data")
-##    return(m)
-##  }
-##  data <- x$get()
-##  m <- mean(data, ...)
-##  x$setmean(m)
-##  m
-##}
 
 cacheSolve <- function(x, ...) {
   ma <- x$getinv() 
