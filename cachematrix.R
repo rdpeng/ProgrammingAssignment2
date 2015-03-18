604f696813e6017d01b76c337606a6fc55739ea3
MaliniAnandaPrakash <malini.anandaprakash@gmail.com>
2015-03-17 20:35:30 -0500

    Revert "new"
    This reverts commit c699c487fdfdf7358f93defb931e278a877982f8.

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 ## Functions makeCacheMatrix and cacheSolve are used to cache the inverse of a matrix which has not changed so far without calling the actual program
 
 ## makeCacheMatrix create special vectors to set and get the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-=======
-## Functions makeCacheMatrix and cacheSolve are used to cache the inverse of a matrix which has not changed so far 
-## without calling the actual program.
-
-## makeCacheMatrix create special vectors to set and get the inverse of matrix
-
-makeCacheMatrix <- function(x=matrix()){
->>>>>>> parent of 17a806a... Revert "cachematrix.R"
   m<-NULL
   set <-function (y){
     x<<-y
@@ -23,24 +14,15 @@ makeCacheMatrix <- function(x=matrix()){
   list(set=set,get=get,
        setinverse=setinverse,
        getinverse=getinverse)
-<<<<<<< HEAD
   
-=======
->>>>>>> parent of 17a806a... Revert "cachematrix.R"
 }
 
-## cacheSolve functions caches the inverse of a matrix and 
-## use it if the same matrix is called again.
 
-<<<<<<< HEAD
 ## cacheSolve functions caches the inverse of a matrix and
 ## 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-=======
-cacheSolve <- function(x, ...){
->>>>>>> parent of 17a806a... Revert "cachematrix.R"
   m<-x$getinverse()
   if(!is.null(m)){
     message ("getting cached data")
@@ -50,9 +32,5 @@ cacheSolve <- function(x, ...){
   m <- solve(data, ...)
   x$setinverse(m)
   m
-<<<<<<< HEAD
   
-=======
->>>>>>> parent of 17a806a... Revert "cachematrix.R"
 }
-
