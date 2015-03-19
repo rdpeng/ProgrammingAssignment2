738419644f662c0af154402bc9f67ba20971b61c
Trent <trent.j.mckay@seagate.com>
2015-03-19 08:14:51 -0500

    My Assignment #2
    This is the first version.  I am missing a test to see if the input
    matrix has changed.  This is a test to make sure I get a version
    uploaded in GitHub.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,52 @@
-## [Put comments here that describe what your functions do]
+## simple test matrix to use while testing
+a<-1:2
+b<-3:4
+c<-rbind(a,b)  
+d<- solve(c)
+
+## makeCacheMatrix function description
+## Creates the get and set functions that will be part of the global environment in the "test" list
+## functions will be called from another function to get or set values
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    print(environment())
+    evn <- environment()
+    print(parent.env(evn))
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    getevn <- function() environment()
+    test <<- list(set = set, get = get, setinv = setinv, getinv = getinv, getevn <- getevn)
+#    list(set = set, get = get, setinv = setinv, getinv = getinv, getevn = getevn)
 }
 
 
+## The cacheSolve function is used to get values from cache
+## if the value returned is null, then the inverse of a matrix will be calculated and stored (set) in cache
+## if the value returned is not null, then the inverse of the matrix will pulled (get) from cache
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    x2 <- test$get()
+    inv2 <- test$getinv()
+    message("x2=")
+    print(x2)
+    message("x=")
+    print(x)
+    message("inv2=")
+    print(inv2)
+    if (!is.null(inv2)) {
+        message ("getting inverse from cache")
+        inv2
+    }
+    else {
+        inv2 <- solve(x)
+        message ("calc inverse of matrix and cache it")
+        test$setinv(inv2)
+    }
 }
