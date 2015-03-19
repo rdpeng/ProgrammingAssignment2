839b354a276747a7ef4f8b8954fd103d7112b522
mulgkal <kalyan.mulge@gmail.com>
2015-03-20 00:34:28 +0530

    Exercise completed
    Defined functions makeCacheMatrix and cacheSolve that Inverse a Matrix
    and use it from Cache if already performed.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	s <- NULL
+	set <- function (y){
+		x <<- y
+		s <<- NULL
+	}
+	get <- function () x
+	setsolve <- function( solve) s<<- solve
+	getsolve <- function() s
+list ( set = set. get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
@@ -12,4 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+s <- getsolve()
+if(!is.null(s){
+	message(" getting from cached data")
+	return(s)
+	}
+data = x$get()
+s <- solve(data,...)
+s
 }
