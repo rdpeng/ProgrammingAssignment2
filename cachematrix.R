4d8c03d82e32027594f1781d37659ae343297418
AmeliaPessoa <m.amelia.pessoa@gmail.com>
2015-04-23 11:58:40 -0300

    Functions
    Functions makeVector and cachemean

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeVector is a function that returns a list of functions
+# Its puspose is to store a vector and a cached value of the mean of the 
+# vector. Contains the following functions:
+# * set      set the value of a vector
+# * get      get the value of a vector
+# * setmean   set the cached value (mean)
+# * getmean     get the cached value (mean)
+makeVector <- function(x = numeric()) {
+        
+        # holds the cached value or NULL if nothing is cached
+        # initially nothing is cached so set it to NULL
+        m <- NULL
+        
+        # store a vector
+        set <- function(newValue) {
+                x <<- newValue
+                # since the vector is assigned a new value, flush the mean
+                m <<- NULL
+        }
 
-## Write a short comment describing this function
+        # returns the stored vector
+        get <- function() {
+                x
+        }
 
-makeCacheMatrix <- function(x = matrix()) {
+        # m the given argument 
+        setmean <- function(mean) {
+                m <<- mean
+        }
 
+        # get the cached value
+        getmean <- function() {
+                m
+        }
+        
+        # return a list. Each named element of the list is a function
+        list(set = set, get = get, setmean = setmean, getmean = getmean)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# The following function calculates the mean of a "special" vector created with 
+# makeVector
+cachemean <- function(y, ...) {
+        # get the cached value
+        mean <- y$getmean()
+        # if a cached value exists return it
+        if(!is.null(mean)) {
+                message("getting cached data")
+                return(mean)
+        }
+        # otherwise get the vector, calculate the mean and store it in
+        # the cache
+        data <- y$get()
+        mean <- mean(data)
+        y$setmean(mean)
+        
+        # return the mean
+        mean
 }
