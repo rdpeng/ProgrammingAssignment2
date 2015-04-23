afeea0c65a1b9150db30f3dcc290a155034725b8
matheusunifei <matheus.eco.2010@gmail.com>
2015-04-22 17:47:09 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(x = matrix()) 
+{
+## x: Receives a square invertible matrix. 
+## Returns a list containing a function to:
+        ## 1. set the Original matrix and NULL to the Inverse Variable
+        ## 2. get the Original matrix 
+        ## 3. set the Matrix already inverted 
+        ## 4. get the Matrix already inverted 
 
-## Write a short comment describing this function
+        Inverse <- NULL # set the variable as NULL
+        set <- function(NewMatrix) 
+        {
+                x <<- NewMatrix # Receive the new matrix value  
+                Inverse <<- NULL # Defines NULL to the variable
+        } # end set funtion
+        get <- function() x # Shows the value of the variable "x"
+        setInverse <- function(InversionCalculated)
+        {
+                Inverse <<- InversionCalculated # set the value of a Inversion already calculated
+        } # end get funtion
+        getInverse <- function() Inverse # Shows the value of the variable "Inverse"
+        list(set = set, 
+                get = get,
+                setInverse = setInverse,
+                getInverse = getInverse) # OutputList
+} # end makeCacheMatrix function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(OutputedList, ...) 
+{
+## OutputedList: Receives the output list from makeCacheMatrix function 
+## Returns the Inverted Matrix
+        
+        MatrixInverted <- OutputedList$getInverse() # Get the value of variable "Inverse" from makeCacheMatrix function 
+        if(!is.null(MatrixInverted)) { # Enter in this function if MatrixInverted isn't NULL
+                message("Found cached data... Getting it!") # print message
+                return(MatrixInverted) # break the function returned the Inverted Matrix
+        } # endif
+        OriginalMatrix <- OutputedList$get() # get the Original Matrix (before the inversion)
+        MatrixInverted <- solve(OriginalMatrix, ...) # Calculate the Inversion
+        OutputedList$setInverse(MatrixInverted) # Define the value of "Inverse" from makeCacheMatrix function 
+        MatrixInverted # Returns the Inverted Matrix
 }
