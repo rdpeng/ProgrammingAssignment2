713341f753c4d793ae0477cca777e6ffe99dcd84
Francisco Blasco <f.blasco.crespo@gmail.com>
2015-04-25 13:28:22 +0200

    Ejercicio 2
    Ejercicio 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
         # Controles de la matriz     
         # Control matriz númerica
@@ -30,9 +25,6 @@ makeCacheMatrix <- function(x = matrix()) {
              getinv = getinv)
 }
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         inv <- x$getinv()
