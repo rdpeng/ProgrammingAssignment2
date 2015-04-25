1026b7ccf3114045e5537d2590538760173f0d39
Francisco Blasco <f.blasco.crespo@gmail.com>
2015-04-25 13:24:17 +0200

    Ejercicio asignación 2
    Solución al ejercicio 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # Controles de la matriz     
+        # Control matriz númerica
+        if(!is.numeric(x)){
+                stop("Matriz no númerica")
+        }
+        # Control matriz cuadrada
+        if (nrow(x) != ncol(x)){
+                stop("Matriz no cuadrada")
+        }
+        # Control matriz con inversa
+        if(det(x) == 0) {
+                stop("Matriz con determinante 0")
+        }
+        inv <- NULL
+        set <- function(y) {
+                x    <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function()      inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+}
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        data <- x$get()
+        inv  <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
