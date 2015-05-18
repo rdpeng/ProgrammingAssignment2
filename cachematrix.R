aa878bfb5e555a0931b81fe839e6277f8ba261a1
LavakumarM <lavakumar.mudduluru@gmail.com>
2015-05-18 22:39:53 +0530

    evised fr inverse function

--- cachematrix.R
+++ cachematrix.R
@@ -2,40 +2,14 @@
 ## functions do
 
 ## Write a short comment describing this function
-## makeVector creates a special "vector", which is really a list containing a function to
-## set the value of the vector
-## get the value of the vector
-## set the value of the mean
-## get the value of the mean
 
-makeVector <- function(x = numeric()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
+
 ## Write a short comment describing this function
-## The following function calculates the mean of the special "vector" 
-## created with the above function. However, it first checks to see if the 
-## mean has already been calculated. If so, it gets the mean from the cache 
-## and skips the computation. Otherwise, it calculates the mean of the data 
-## and sets the value of the mean in the cache via the setmean function.
-cachemean <- function(x, ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
