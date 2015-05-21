f01bf885be49aa3c59fac53e51fa83844917ec23
akhilendraps <akhi_pratap@hotmail.com>
2015-05-20 23:47:52 -0500

    Update cachematrix.R
    Programming Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#set the value of the matrix
+#get the value of the matrix
+#set the value of the mean
+#get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean() #get mean
+  
+  data <- x$get()
+  invx <- solve(data) # matrix inversion, x inversion
+  m <- mean(invx, ...)
+  x$setmean(m)
+  # check first or cached value
+  if(!is.null(m)) {
+    message("this is cached calculation")
+    return (m)    
+  } 
+    
+    m 
 }
+
+
+#testing functions, first create a matrix
+
+l <- matrix(runif(1000), 5,5)
+#print l
+l
+# test makeCacheMatrix
+makeCacheMatrix(l)
+# test cacheSolve
+
+o <-makeCacheMatrix(l)
+o
+p <- cacheSolve(o)
+p
