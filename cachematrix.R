d6a72fb244e79778892a16284eacf1f1769e2256
vflo <vflosierra@gmail.com>
2015-06-16 12:43:02 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to invert squared matrices
 
-## Write a short comment describing this function
+## MakeCacheMatrix create a list of 4 functions, 
+## setmatrix, getmatrix, setinvert, getinvert 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  getmatrix<-function() x
+  setmatrix<-function(y){
+    x<<-y
+    i<<-NULL
+  }
+  setinvert<-function(invert) i<<-invert
+  getinvert<-function() i
+  list(getmatrix=getmatrix,setmatrix=setmatrix,
+       setinvert=setinvert,getinvert=getinvert)
 }
 
 
-## Write a short comment describing this function
+## Cachesolve apply funtion "solve" to set matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinvert()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i) ## Return stored invert matrix
+  } 
+  data<-x$getmatrix() 
+  i<-solve(data, ...) ## Return a matrix that is the inverse of 'x'
+  x$setinvert(i) ## Store invert in cache
+  i ## Return invert matrix
 }
