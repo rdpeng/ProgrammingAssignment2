6339380c47d73948ce018b8ed0505c5bf8b067c6
Alexander Vershilov <alexander.vershilov@gmail.com>
2015-06-17 11:08:36 +0300

    Implement cachematrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# This document is writtent according to Google's R Style Guide
+# https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
+# except indetation rules, because in cource lectorer insisted
+# on 4 or 8 spaces indetation.
+#
+# No extra arguments are passed into cacheSolve because otherwise
+# cache will be invalidated in case if different parameters
+# are passed.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Create matrix with it's inverse being cached.
+    #
+    # Args:
+    #   x - input argument
+    #
+    # Returns:
+    #   "Object" that that keeps cacked information
+    inv <- NULL
+    set <- function(y) {
+       x <<- y
+       inv <<- NULL
+    }
+    get    <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set        = set,
+         get        = get,
+         setinverse = setinv,
+         getinverse = getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    # Execute solve with cached result.
+    #
+    # Args:
+    #   x - cache matrix object
+    #
+    # Returns:
+    #   Matrix inverse.
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv  <- solve(data)
+    x$setinverse(inv)
+    inv
 }
