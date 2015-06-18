b1b7931353bb0bcc250e70bc613e4fb3d0c52742
Roy <anw1950@gmail.com>
2015-06-18 10:19:21 -0400

    Updated comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Roy W. Wilson 06-2015
 ## Given an ordinary R matrix x, the "constructor" function makeCacheMatrix 
 ## creates an "object" that stores x. 
 makeCacheMatrix <- function(x = matrix()) {
