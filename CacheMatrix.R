8b4e80a9df32049129445d9cb2bd76c91bcb5b5b
SamiBoy2001 <SurfKip2@gmail.com>
2015-06-21 16:39:06 -0400

    changed matrix

--- CacheMatrix.R
+++ CacheMatrix.R
@@ -1,25 +1,27 @@
-## Put comments here that give an overall description of what your
+ 
+# Put comments here that give an overall description of what your
 ## functions do
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix(1.1,1.2,1.3,2.1.2.2.2.3),2,3,byrow=TRUE  )) {
-     m <- x$getmean()
-         if(!is.null(m)) {
-             message("getting cashed data")
-            return(m)
-          }
-          data <- x$get()
-             m <- mean(data,na.rm=TRUE)
-             x$setmean(m)
-             m
-}
+theData <- c(1.1, 1.2, 2.1, 2.2, 3.1, 3.2) 
+mat <- matrix(theData, 2, 3)
 
+cachemean <- function(x,mat) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data)
+  x$setmean(m)
+  m
+}
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, solve(m) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 }
-
 
