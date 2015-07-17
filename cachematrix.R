16cdde5403a09c044004ec6d1cad63f2606c73b8
Len Greski <leonard.m@greski.com>
2015-07-17 06:37:16 -0700

    first edits to comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## JHU Coursera R Programming Assignment #2
+## July 2015
+## author: leonard.m@greski.com
+##
+## Invert a matrix and cache its result, so subsequent requests for matrix inversion
+## retrieve from cache rather than redoing the inversion calculation
 
-## Write a short comment describing this function
+## makeCacheMatrix() - create a cache for a matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() invert a matrix, or retrieve from cache if it already exists
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
