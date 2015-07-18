a2bee42568071eff488bddb670266e22b6f1983e
lgreski <len@ageretechnology.com>
2015-07-18 16:56:02 -0700

    first substantive changes to makeCacheMatrix and cacheSolve
    First draft of changes to the two functions. Next step is to create an
    invertible matrix in R and test the functions.

--- cachematrix.R
+++ cachematrix.R
@@ -8,6 +8,17 @@
 ## makeCacheMatrix() - create a cache for a matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
+     theMatrix <- NULL
+     set <- function(y) {
+          x <<- y
+          theMatrix <<- NULL
+     }
+     get <- function() x
+     setsolve <- function(solve) theMatrix <<- solve
+     getsolve <- function() theMatrix 
+     list(set = set, get = get,
+          setsolve = setsolve,
+          getsolve = getsolve)
 
 }
 
@@ -15,5 +26,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve() invert a matrix, or retrieve from cache if it already exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+    
+      ## Attempt to retrieve matrix from cache
+     theMatrix <- x$getsolve()
+     if (!is.null(theMatrix)) {
+          message("getting cached inverse")
+          return(theMatrix)
+     }
+     ## if we get past the if() statement, the cache is empty
+     
+     ## invert the matrix, set the cache, and return
+     data <- x$get()
+     theMatrix <- solve(data)
+     x$setsolve(theMatrix)
+     theMatrix
 }
