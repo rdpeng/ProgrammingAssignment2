d9e631cf8a52dc628595eb159e9c61c4f97fcf72
Lexislex <alexey.alepko@gmail.com>
2015-07-20 12:04:35 +1000

    test commiting

--- cachematrix.R
+++ cachematrix.R
@@ -11,5 +11,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  
+  
         ## Return a matrix that is the inverse of 'x'
 }
