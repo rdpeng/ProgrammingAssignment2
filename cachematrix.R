b8b327c1ce5dc1289a44ea260ff26ec83debf6c0
Shyam Sundar <shyamsram@gmail.com>
2015-07-20 22:15:18 -0400

    Updating work in progress code to git

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmatrix <- NULL
+  get <- function() x
+  putinverseintocache <- function(matrix) {
+    invmatirx <<- matrix
+    print(c('inside put method : ',matrix))
+  }
+  getinversefromcache <- function() invmatrix
+  
+  print('makeCacheMatrix called with x ')
+  list(get = get, getinversefromcache = getinversefromcache,
+       putinverseintocache = putinverseintocache)
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   ##first try of calling the inverse function solve
-  print(solve(x))
+  invmatrixfromcache <- makeCacheMatrix(x)$getinversefromcache()
+  print(c('from cache ', invmatrixfromcache))
+  
+  ##if (is.null(invmatrixfromcache)) {
+    ##return(invmatrixfromcache)
+  ##}
+  
+  ##call the solve function to get the inverse
+  invmatrix <- solve(x)
+  
+  ##put the inverse into cache
+  print('adding inverse matrix to cache')
+  makeCacheMatrix(x)$putinverseintocache(invmatrix)
 }
