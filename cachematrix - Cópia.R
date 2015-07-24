b0e09a668087c866e4608172fb0ddc811370bfba
CSoaresF <csoaresf@yahoo.com>
2015-07-24 16:51:51 -0300

    última alteração nos comentários
    alterações nos comentários

--- cachematrix - Cópia.R
+++ cachematrix - Cópia.R
@@ -1,22 +1,20 @@
 ## cachematrix.R - Programming Assignment 2
 ## Caching the inverse of matrix
 
+## Write a short comment describing this function
+# makeCacheMatrix(): criates cached matrix (cria  matriz em cache)
 #########################################################################################
 ###     SAMPLE RUN:                                                                     #
 ### 1)  >source("cachematrix.R")   ## load the program R                                #                                                    #
-### 2)  >m1 <- matrix(1:4,2,2)     ## put matrix (1:4,2,2) in variable 'm1'             #
+### 2)  >m1 <- matrix(1:4,2,2)     ## put matrix (1:4,2,2) in variable 'm1'                       #
 ###  follow 3.1 or 3.2                                                                  #
 ### 3.1)>a  <- makeCacheMatrix()   ## load only functions in variable 'a'               #
 ### 3.2)>a$setm(m1)                ## load matrix "m1" in cache                         #
 ### 3.2)>a  <- makeCacheMatrix(m1) ## load functions and matrix 'm1'                    #
 ### 4)  >a$seti()          ## compute inverse, if it has not been calculated            #
 ### 5)  >a$geti()          ## get inverse cache (if already has calculated)             #
-### 6)  >cacheSolve(m1,a)  ## return inverse matrix 'm1', cache in variable 'a'         #
+### 6)  >cacheSolve(m1,a)  ## return inverse matrix                                     #
 #########################################################################################
-
-####################################################################
-# makeCacheMatrix(): criate cached matrix (cria  matriz em cache)  #
-####################################################################
 makeCacheMatrix <- function(x=matrix()) {
                 m <<- NULL
                 setm <- function(x=matrix()) {
@@ -27,7 +25,7 @@ makeCacheMatrix <- function(x=matrix()) {
                             message("Is not square matrix or null!")
                     }
                 }
-                getm <- function() {xm}                           # retorna a matriz em cache
+                getm <- function() {xm}                           # exibe a matriz em cache
                 seti <- function(x=matrix()) {                    # calcula a inversa da matriz no arg x  
                         if(nrow(x)==ncol(x) & !is.null(x)){       # testa se quadrada e não nula
                                 if(is.null(m)) {                  # se a inversa não estiver na cache
@@ -37,7 +35,7 @@ makeCacheMatrix <- function(x=matrix()) {
                                     return(m)
                                 } else {                          # a inversa já foi calculada
                                     message("in cache!")
-                                    return(m)                     # retorna a inversa em cache
+                                    return(m)                     # exibe a inversa em cache
                                 }
                         } else {
                             message("Is not square matrix or null!")
@@ -48,11 +46,11 @@ makeCacheMatrix <- function(x=matrix()) {
                      seti=seti, geti=geti)
 }
 
-############################################################################################################
-# cacheSolve(): return inverse matrix in cache or calculate inverse (retorna a inversa da matriz em cache) #
-############################################################################################################
+###############
+# cacheSolve(): get inverse matrix in cache or calculate inverse
+#            #
 cacheSolve <- function(y = matrix(),z) { # return matrix that is inverse of 'y' with cache in 'z'
-                    m  <- z$geti()
+                    m <- z$geti()
                     xm <- z$getm()
                     if(identical(xm,y)) { 
                        if(!is.null(m)) {
