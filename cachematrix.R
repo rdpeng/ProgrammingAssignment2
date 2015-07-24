7fe5f1d9ef0ca99a556d1ae3cf061924547a2252
givensplace <givensplace@gmail.com>
2015-07-23 21:46:22 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,9 +6,27 @@
 makeCacheMatrix <- function(x = matrix()) {
 
 }
+      X <- makeCacheMatrix( matrix(c(1,2,12,13), nrow = 2, ncol = 2) );
 
+    summary(X);
 
 ## Write a short comment describing this function
+    a$getMatrix();
+#>      [,1] [,2]
+#> [1,]    1   12
+#> [2,]    2   13
+
+    cacheSolve<- function(x, ...)
+#> [,1]        [,2]
+#> [1,] -1.1818182  1.09090909
+#> [2,]  0.1818182 -0.09090909
+
+# the 2nd time we run the function,we get the cached value
+    cacheSolve(x)
+#> getting cached data
+#> [,1]        [,2]
+#> [1,] -1.1818182  1.09090909
+#> [2,]  0.1818182 -0.09090909
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
