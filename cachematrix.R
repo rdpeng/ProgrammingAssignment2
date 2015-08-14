37d1e542e9e906ab63658ec58fe5716e71239cf7
sulabs <Sunil@Sunils-MacBook-Pro.local>
2015-08-14 08:22:00 +0530

    Working Code for Inverse Matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    dimension <<- length(y) ^ 0.5
+    x <<- matrix(y,ncol = dimension, nrow = dimension)
+    m <<- NULL
+  }
+  get <- function() x
+  setInverseMatrix <- function(InverseMatrix) m <<- InverseMatrix
+  getInverseMatrix <- function() m
+  list(set = set, get = get,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x' Sunil
+  InverseMatrix <- x$getInverseMatrix()
+  if(!is.null(InverseMatrix)) {
+    message("getting cached data")
+    return(InverseMatrix)
+  }
+  data <- x$get()
+  
+  InverseMatrix <<- solve(data)
+  x$setInverseMatrix(InverseMatrix)
+  InverseMatrix
+  
 }
