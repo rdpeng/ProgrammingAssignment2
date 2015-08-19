cb8f1b215131b25541564559e7a35185ba1bb405
danielaboff <danielabboff@gmail.com>
2015-08-19 20:17:34 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special matrix object that can cache its inverse
 
-## Write a short comment describing this function
+## Input: A matrix 'x'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      
+            ## Setting the matrix
+      
+            m  <- NULL
+            set <- function(y) { 
+                  x  <<- y
+                  m <<- NULL
+            }
+            
+            ## Getting the matrix
+            ## Setting the inverse
+            ## Getting the inverse
+            
+            get        <- function() x     
+            setinverse <- function(inverse) m <<- inverse
+            getinverse <- function() m
+            list(set = set, get = get,
+                 setinverse = setinverse, 
+                 getinverse = getinverse)
+            
+            ## list is the input for cacheSolve()
+      
 }
 
-
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the matrix returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+            ## Checking if the inverse has already been calculated
+      
+            m   <- x$getinverse()
+      
+            if(!is.null(m)) {
+            
+                  message("getting cached data")
+                  return(m)
+            
+            ## Skipping the computation
+            
+            }
+            
+            ## Calculating the inverse
+      
+            data <- x$get()
+            m   <- solve(data,...)
+            x$setinverse(m)
+            m
+      
+            ## Output: a matrix that is the inverse of 'x'
 }
+
+## Example
+## > x <- matrix(1:4,2,2)
+## > m <- makeCacheMatrix(x)
+## > m$get()
+##    [,1] [,2]
+##[1,]   1    3
+##[2,]   2    4
+
+## cacheSolve(m)
+##    [,1] [,2]
+##[1,]  -2  1.5
+##[2,]   1 -0.5
