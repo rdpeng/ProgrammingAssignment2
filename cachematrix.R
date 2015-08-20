93f925654229a6cb70119b7d66d692b79489f169
JhoanDoe <nihilist_@outlook.com>
2015-08-19 22:47:43 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a list of functions to set and get 
+## a given matrix and it's inverse
 
-## Write a short comment describing this function
+##
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+  # set the initial value of i to NULL
+  # in this variable is where the inverse is going to be stored
+  i <- NULL
+  
+  # store the value of the matrix in the variable x
+  # both x and y are set in the environment
+  set <- function(y) {
+    m <<- y
+    i <<- NULL
+  }
+  
+  # retrives the value of the matrix
+  get <- function() m
+  
+  # store the value of the matrix inverse in i
+  setinv <- function(inv) i <<- inv
+  
+  # retrieves the value of the matrix inverse
+  getinv <- function() i
+  
+  # set a named list whose elements are the functions above
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks and returns the value of the matrix inverse
+## if it's in the environment, otherwise it calculates and return it's value
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+  # retrieves the data in the 'getinv' variable
+  i <- x$getinv()
+  
+  # if it's not NULL returns the value in i
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  
+  # otherwise, retrieves the data in the 'get' variable
+  data <- x$get()
+  
+  # calculate the matrix inverse
+  message("calculating the matrix inverse")
+  i <- solve(data)
+  
+  # saves the value of i 
+  x$setinv(i)
+  
+  # returns the matrix inverse
+  i
 }
