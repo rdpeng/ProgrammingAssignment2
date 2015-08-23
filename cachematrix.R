9ab9a42f88906dc5a06e6724ddd703b099f9e958
Roesgen <tobi@lukilaki.de>
2015-08-23 23:47:17 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+### The Functions do what requestet by ProgrammingAssisment2
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function create a Matrix and a Vector of four functions
+#set the values and dimension of the matrix
+#get the values and dimension of the matrix
+#set the values and dimension of the inverse matrix
+#get the values and dimension of the inverse matrix
 
+makeCacheMatrix <- function(x = numeric(), y = numeric(), z = numeric()) {
+  matrix <- matrix(x,nrow = y ,ncol = z)
+  s <- NULL
+  set <- function(x = numeric(), y = numeric(), z = numeric()) {
+    matrix <<- matrix(x,nrow = y ,ncol = z)
+    s <<- NULL
+  }
+  
+  get <- function()
+    matrix
+  setsolve <- function(solve)
+    s <<- solve
+  getsolve <- function()
+    s
+  list(
+    set = set, get = get,
+    setsolve = setsolve,
+    getsolve = getsolve
+  )
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  s <- x$getsolve()
+  if (!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data)
+  x$setsolve(s)
+  s
 }
+
+
+## returns and stores the inverse matrix of the matrix created by makeCacheMatrix()
+
+cacheSolve <- function(x) {
+  s <- x$getsolve()
+  if (!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data)
+  x$setsolve(s)
+  s
+}
+
+
