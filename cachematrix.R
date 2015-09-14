5ce196c6c2ca21f70744a1cc3c8472e5f44831f9
sathishprabahar <prabahar.sathish@gmail.com>
2015-09-14 11:58:25 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -17,6 +17,28 @@ list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(X, ...) 
+{
+if(require("corpcor")){
+	print("corpcor is loaded correctly")
+	} else {
+		print("trying to install corpcor")
+		install.packages("corpcor")
+		if(require(corpcor)){
+			print("corpcor installed and loaded")
+			} else {
+			stop("could not install corpcor")
+			}
+		}
+inverse <- X$getinverse()
+if(!is.null(inverse)){
+	message("matrix is in memory")
+	return(inverse)
+	}
+message("inverse is not in memory so the inverse (if exist) is gonna be computed")
+data <- X$get()
+inverse <- pseudoinverse(data, ...)
+X$setinverse(inverse)
+inverse
 }
