e694abdb278e888fadf89371e77e001c06298d62
chockyspice <chockyspice@gmail.com>
2015-09-21 11:01:02 -0400

    Update catchmatrix.R

--- catchmatrix.R
+++ catchmatrix.R
@@ -20,9 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function computes the inverse of the special "matrix" created by 
-## makeCacheMatrix above. If the inverse has already been calculated (and the 
-## matrix has not changed), then it should retrieve the inverse from the cache.
+## The following function calculates the mean of the special "vector" 
+## created with the above function. However, it first checks to see 
+## if the mean has already been calculated. If so, it gets the mean from 
+## the cache and skips the computation. Otherwise, it calculates the mean 
+## of the data and sets the value of the mean in the cache via the setmean 
+## function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
