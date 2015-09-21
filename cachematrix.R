af9e1264071402e57cbcb4234515209af26c95db
vaibhav0602 <vaibhav0602@gmail.com>
2015-09-22 00:05:03 +0530

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+##makeCacheMatrix function takes a numeric square matrix as input and returns the four funcitons
+##set, get, setinv and getinv as a list to the variable. the variable can be passed to cacheSolve function 
+##as argument to the either the cached matrix inverse or newly calculated matrix inverse
+
+
 makeCacheMatrix <- function(x = numeric()) {
   i <- NULL
   set <- function(y) {
@@ -13,6 +18,10 @@ makeCacheMatrix <- function(x = numeric()) {
 }
 
 
+## cacheSolve function requests cached inverse matrix value through getinv function. 
+## If the value is not found,then it calculates the matrix inverse through built in 
+## R function 'solve'. cacheSolve function accepts a list containing the four functions
+## as column value as the arguments
 
 cacheSolve <- function(x, ...) {
   i <- x$getinv()
