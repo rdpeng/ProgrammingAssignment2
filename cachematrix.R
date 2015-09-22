5133fb0c219d72599afbf17946337848fb0a632a
Pete75244 <jpm7502333@gmail.com>
2015-09-22 14:01:42 -0500

    solution ready

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a solution to the Programming Assignment 2
+## I took the framework straight from the assignment example
+## and only changed var names and calls from "mean" to "solve"/"inverse"
+## but the solution works
 
-## Write a short comment describing this function
+## Instantiates a "special matrix" object with 4 methods
+## (data setter, data getter, inverse setter and inverse getter)
+## line 15 ensures that every time data is changed, the inverse is reset,
+## therefore cachSolve will re-calculate it as opposed to using a cached value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(inverse) inv <<- inverse
+  get_inverse <- function() inv
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## Takes "special matrix" as an argument;
+## tries to loop up inverse (stored previously) using the getter method, if found, returns it;
+## if not found, calculates the inverse using native solve() and stores is
+## in the "super-matrix" instance using the setter method
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$get_inverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$set_inverse(inv)
+  inv
 }
