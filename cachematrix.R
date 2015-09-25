3f2af1fe0bf3a9ce7abee8addb06949aa13bb827
pcarroll78 <pcarroll78@users.noreply.github.com>
2015-09-25 14:53:39 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -10,18 +10,15 @@
 # 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
     inv <- NULL
     set <- function(y) {
         x <<- y
         inv <<- NULL
-        
     }
     get <- function() x
     setinverse <- function(inverse) inv <<- inverse
     getinverse <- function() inv
-    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse
-
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 ## Write a short comment describing this function
