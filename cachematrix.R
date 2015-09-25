b25678ce9bc6697932fa20de121689d334cdb7aa
astropic <astropic@gmail.com>
2015-09-25 18:02:17 -0300

    Assignment 2
    Return a matrix that is the inverse of 'x'
    using a cached value if it exists
    otherwise calculates it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a list of functions to set and get cached values of matrices and their inverses
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)  
 }
 
 
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of 'x'
+## using a cached value if it exists
+## otherwise calculates it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinv(i)
+  i
 }
