596f3a4d06a15b61ef36859352fbccc7ee6cfca8
johnsivill <johnsivill@yahoo.com>
2015-09-25 18:16:17 -0500

    Assignment 2 - corrections
    Corrected the logic to update cache in second function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,37 @@
 ## Two functions in this file; makeCacheMatrix and cacheSolve.
 ##
-## makeCacheMatrix will accept a matrix as an argument and load cache with a object "matrix"
-## with the inverse of the matrix.  It will set this every time.
+## makeCacheMatrix will accept a matrix as an argument and load cache with an object "matrix"
+## that is an empty matrix.  It will then return the original matrix that was passed into the
+## function.
 ##
-## cadheSolve will check to see if the inverse for the input matrix has been calculated and 
-## planced in cache.  If so it will output that cache, if not it will calcuate the inverse.
+## cadheSolve will check to see if the inverse has been calcualted by checking "matrix" for data.
+## If it finds data it will return that value, if not it will calcuate the inverse of the input matrix.
 
-## Sets the inverse of the matrix into "matrix" every time.
+## Creates the cache "matrix" and returns the input matrix data.
 
 makeCacheMatrix <- function(x = matrix()) {
         
-        matrix <<- NULL
-        matrix <<- solve(x)
+        matrix <<- matrix()
+        return (x)
+        #matrix <<- solve(x)
         
 }
 
 
 
-## Checks the cache "matrix" to the input.  If it matches, then it will take form cache.  If not
-## it creates the inverse.  Print messages added for demonstration purposes.  A possible enhancement
-## could be to load the cache from this function, but that was not in the requirements.
+## Checks the cache for values.  If it exists, then it will take form cache.  If not
+## it creates the inverse and sets the cache.  Print messages added for demonstration purposes. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        if (identical(x, matrix) == TRUE) {
-                print("retreiving content from cache")
+        if (is.na(matrix[1]) == TRUE) {
+                print("calculating inverse")
+                matrix <<- solve(x)
                 matrix
         }
         else {
-                print("calculating inverse")
-                solve(x)
-                ##matrix <<- solve(x)
-                ##matrix
+               
+                print("retreiving content from cache")
+                matrix
         }
 }
