b2b354ba064a5bac5b2fd4449a1c76a8d192a79b
aleksey.yas <aleksey.yas@gmail.com>
2015-09-26 21:41:28 +0300

    Adding comments into code

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,15 @@
 ## Write a short comment describing this function..
 
 makeCacheMatrix <- function(x = matrix()) {
-    s <- NULL
-    set <- function(y) {
+    s <- NULL                                  ## Initialise variable "s" as NULL
+    set <- function(y) {                       ## Initialise setter "set" to set the value of the vector
         x <<- y
         s <<- NULL
     }
-    get <- function() x
-    setsolve <- function(solve) s <<- solve
-    getsolve <- function() s
-    list(set = set, get = get,
+    get <- function() x                        ## Initialise getter "get" to get the value of the vector
+    setsolve <- function(solve) s <<- solve    ## Initialise setter "setsolve" to set the value of the solve
+    getsolve <- function() s                   ## Initialise setter "getsolve" to get the value of the solve
+    list(set = set, get = get,                 ## Compile information about "vector" into a list 
          setsolve = setsolve,
          getsolve = getsolve)
 
@@ -24,13 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     
-    s <- x$getsolve()
-    if(!is.null(s)) {
+    s <- x$getsolve()                          ## Check is variable "s" equal NULL or not
+    if(!is.null(s)) {                          ## If "not" - return cached invert matrix result
         message("getting cached data")
         return(s)
     }
-    data <- x$get()
-    s <- solve(data, ...)
-    x$setsolve(s)
-    s
+    data <- x$get()                            ## If "yes" - using the getter "get" to get value of the matrix
+    s <- solve(data, ...)                      ## Making calculations to invert matrix
+    x$setsolve(s)                              ## Using setter "setsolve" - save result in a Cache
+    s                                          ## Printing result to screen
 }
