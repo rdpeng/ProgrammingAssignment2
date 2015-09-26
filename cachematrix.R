527f2e2f2111191330e380f7bf7588c5d8dbcaaf
kanasob <kanako.sobajima@woodmac.com>
2015-09-26 17:45:01 +0100

    Add the comment, delete test results
    Added the comment to the code explaining what the functions does. Also deleted test results.

--- cachematrix.R
+++ cachematrix.R
@@ -1,79 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following pair of functions ("makeCacheMatrix" and "cacheSolve") caches 
+## the invese of a matrix specified by user.
+## This pair of function is one of the solutions which improves times and 
+## the cost of the hardware used for matrix inversion.
 
-## cashed matrix - the cached values
+## The "makeChacheMatrix" function stores the inverse of a matrix created 
+## in the solve function.
+## This also returns a list of function used in the "cacheSolve" function.
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## Make the cached inverse matrix NULL to initialise  
   cachemx <- NULL
+  
+  ## Set a matrix in the working environment
   set <-function (y) {
     x<<-y
     cachemx <<- NULL
   }
+  
+  ## Get the matrix value
   get <- function () x
-    setInverse <-function(inverse) cachemx <<-inverse
-    getInverse <-function () cachemx
-    list (set = set,
-          get = get, 
-          setInverse = setInverse,
-          getInverse = getInverse)
+  ##  Set the inverse of the matrix and stores in cache
+  setInverse <-function(inverse) cachemx <<-inverse
+  ## Get the inverse of the matrix
+  getInverse <-function () cachemx
+  
+  ## Return the above functions to the working environment 
+  list (set = set, get = get, 
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  cachemx <- x$getInverse()
-  
-  if (!is.null(cachemx)) {
-    message ("getting cached data")
-    return (cachemx)
-    }
-  matrix <-x$get()
-  cachemx <-solve(matrix, ...)
-  x$setInverse(cachemx)
-  cachemx
- }
-    
-  
-  
-##> source("cachematrix.R")
-##> set.seed(2)
-##> mx <-makeCacheMatrix (matrix(rnorm(4),2,2))
-##> mx$get()
-##[,1]      [,2]
-##[1,] -0.8969145  1.587845
-##[2,]  0.1848492 -1.130376
-##  > mx$getInverse()
-##NULL
-##> cacheSolve(mx)
-##[,1]      [,2]
-##[1,] -1.5692285 -2.204304
-##[2,] -0.2566143 -1.245129
-##> mx$getInverse()
-##[,1]      [,2]
-##[1,] -1.5692285 -2.204304
-##[2,] -0.2566143 -1.245129
-##> cacheSolve(mx)
-##getting cached data
-##[,1]      [,2]
-##[1,] -1.5692285 -2.204304
-##[2,] -0.2566143 -1.245129
-##> mx$getInverse()
-##[,1]      [,2]
-##1,] -1.5692285 -2.204304
-##[2,] -0.2566143 -1.245129
-
-##> mx <-makeCacheMatrix (matrix(1:9,3,3))
-##> mx$get()
-##[,1] [,2] [,3]
-##[1,]    1    4    7
-##[2,]    2    5    8
-##[3,]    3    6    9
-##> mx$getInverse()
-##NULL
-##> cacheSolve(mx)
-##Error in solve.default(matrix, ...) : 
-##  Lapack routine dgesv: system is exactly singular: U[3,3] = 0
-##> 
-## http://stackoverflow.com/questions/20529062/using-mlogit-in-r-with-variables-that-only-apply-to-certain-alternatives
