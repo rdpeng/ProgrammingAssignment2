036319e9fae7c89e232c0b2dbeba2f1af4c9616d
semioticos <semioticos@gmail.com>
2015-09-26 18:57:25 -0500

    Actualización al 26-sep-2015
    Comentarios

--- cachematrix.R
+++ cachematrix.R
@@ -4,16 +4,19 @@
 makeCacheMatrix <- function(x = matrix()) {
   inv = NULL
 
-  ## Define unafunción para asignar a "x" el valor proporcionado como "y".
+  ## Define una función para asignar a "x" el valor proporcionado como "y".
   set = function(y) {
     x <<- y
     inv <<- NULL ## Asigna nulo a la variable de la función invocante.
   }
   
+  ## Define función para obtener valor de x
   get = function() x
   
+  ## Asigna el valor inverso
   setinv = function(inverse) inv <<- inverse 
   
+  ## Obtiene el valor inverso
   getinv = function() inv
   
   list(set=set, get=get, setinv=setinv, getinv=getinv)
@@ -24,15 +27,19 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   inv = x$getinv()
   
+  ## Si ni es nulo el valor inverso, lo devuelve y termina
   if (!is.null(inv)){
     message("obteniendo datos en cache...")
     return(inv)
   }
   
+  ## Obtiene el valor 
   mat.data = x$get()
   
+  ## Determina elvalor inverso
   inv = solve(mat.data, ...)
   
+  ## Establece elvalor inverso
   x$setinv(inv)
   
   return(inv)
