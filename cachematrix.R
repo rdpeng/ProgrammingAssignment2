d93b2cde1cb24ee768e1eca1c4e7cd216636b801
harishreddyramigani <harishreddyramigani@gmail.com>
2015-09-26 14:43:39 -0700

    Revert "Revert "Assignment2 Harish Reddy""
    This reverts commit 16ecffdf1b1a6340f39b44dd5a7d452bb6b33577.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {  
+  xinv <- NULL 
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL 
+  }
+  
+  get <- function() x 
+  setInv <- function(inv) xinv <<- inv 
+  getInv <- function() xinv 
+  
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv() 
+  
+  if(!is.null(m)) { 
+    message("Getting cached data ||||||")
+    return(m) 
+  }
+  data <- x$get() 
+  m <- solve(data) 
+  x$setInv(m)
+  m 
 }
+
