87dde73c6008cd1511235a96c680d24d018d0bd7
fractala <ignacio.nicolas.serna@gmail.com>
2015-09-26 19:03:33 +0200

    He escrito borrar este texto

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Borrar este texto
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
