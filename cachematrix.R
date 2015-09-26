cbb8c3ad70572ef8da6b691f480b87c5c9ee4ce8
Judy Butler <jabutler66@yahoo.com>
2015-09-26 15:57:57 -0600

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 to conserve resources by 
+## caching function outputs
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix creates and caches the inverse of a matrix
+makeCacheMatrix <- function(x) {
+    cm <- NULL
+    set <- function(y) {
+        x <<- y
+        cm <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function() cm <<- solve(x)
+    getmatrix <- function() cm
+    list(set = set, get = get, setmatrix = setmatrix, 
+         getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve retrieves the cached matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cm <- x$getmatrix()
+    if (!is.null(cm)) {
+        message("getting cached matrix")
+        return(cm)
+    }
+    data <- x$get()
+    cm <- solve(data, ...)
+    x$setmatrix()
+    cm
 }
+  
+  ## Return a matrix that is the inverse of 'x' - these are some tests
+#a <- matrix(data = 1:4, nrow = 2, ncol = 2)
+#b <- makeCacheMatrix(a)
+#cacheSolve(b)
