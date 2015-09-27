54abb94bfec4c1316c4174a29f9b38962dc18464
Nick DelTorto <ndeltorto@demandware.com>
2015-09-27 16:24:41 -0400

    Update cachematrix.R
    GitHub repository contains at least one commit beyond the original fork.
    1 point: Yes, there is at least one commit beyond the original fork

--- cachematrix.R
+++ cachematrix.R
@@ -6,9 +6,41 @@
 ## Write a short comment describing this function
 #1. makeCacheMatrix <- create special matrix object that caches the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+makeVector <- function(x = numeric()) {
+#i. set the value of the vector
+#ii. get the value of the vector
+#iii. set the value of the mean
+#iv. get the value of the mean
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 
+cachemean <- function(x, ...) {
+#i. calculate the mean of vector created with makeVector function
+#ii. check if mean already calculated
+#iii. if already calculated, take mean from cache and skips computation
+#iv. else calculate mean of data
+#v. and sets value of mean in cache via setmean function
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
+}
 
 ## Write a short comment describing this function
 #2. cacheSolve: computes the inverse of the special matrix returned by makeCacheMatrix
