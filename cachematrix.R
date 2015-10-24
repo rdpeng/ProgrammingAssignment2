fb074b852639bf6fb2f3ded804fd03bd50bcc10a
CarrieWG <carrie.gilder@earthlink.net>
2015-10-23 21:54:17 -0500

    Revert "ProgrammingAssignment 2 cachematrix file"
    This reverts commit 62c4c63b4cfbecbf5894741b209ab5340faff98f.

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,15 @@
-## makeCacheMatrix creates a matrix based on user input, solves for the inverse of the matrix,
-## and caches the inverse matrix into the new matrix, cacheMatrix.  No data is returned.
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  cacheMatrix<<-solve(x)  ## Solves inverse of matrix and caches values to new matrix
+
 }
 
-##cacheSolve performs the same task as makeCacheMatrix then checks to determine if the
-##resulting inverse matrix is the same as the previously cached matrix.  If so, it prints the
-##new result.  If not, it prints the cached result.  
 
-cacheSolve <- function(x, ...) {  ## Return a matrix that is the inverse of 'x'
-  xsolved<-solve(x)  ##solves for inverse of x
-  if(xsolved[1,1]==cacheMatrix[1,1]) { ##checks that cacheSolve result is the same as makeCacheMatrix result
-    print(xsolved)    ##if so, prints cacheSolve result
-  } else { 
-    print(cacheMatrix) ##if not, prints cacheMatrix result 
-  }
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
-
