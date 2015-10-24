7e04009392d0718f370abf3de619bee8b3ddaec8
Trinniboy <matphi@SEGUML089.SEC.INTRA>
2015-10-24 14:27:57 +0200

    cachematrix.R proposal

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object 
+## that stores a numeric matrix and caches its mean
 
-## Write a short comment describing this function
+## The first function is a list containing a function to 
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inversed matrix
+## 4. get the value of the inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
 
+  setMatrix <- function(y){
+    x <-- y
+    m <-- NULL
+  }
+  getMatrix <- function() x
+  setInvMatrix <- function(InvMatrix) m <<- InvMatrix
+  getInvMatrix <- function(InvMatrix) m 
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInvMatrix = setInvMatrix,
+       getInvMatrix = getInvMatrix)
 }
 
 
-## Write a short comment describing this function
+## The second function calculates the inversed matrix of the special
+## "matrix" created with the above function. However, it first checks
+## to see if the inversed matrix has already been calculated. If so,
+## it gets it from the cache and skips computation. Otherwise, it
+## calculates the inversed matrix of the data and sets the value of 
+## the inversed matrix in the cache via the setInvMatrix function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInvMatrix()
+  if(!is.null(m)) {
+    message("getting the cached inversed matrix")
+    return(m)
+  }
+  m <- solve(x$getMatrix())
+  x$setInvMatrix(m)
+  m
 }
