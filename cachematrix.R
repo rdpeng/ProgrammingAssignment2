b274153f80f063743aed090c903d3edba00bb773
lezuzan <lezuzan@gmail.com>
2015-10-25 14:14:02 +0100

    Programing assessment 2
    contains two functions, makeCacheMatrix (creating a special matrix
    object that can cache its inverse) and cacheSolve that can compute the
    inverseof the special matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,20 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+  #creates a special matrix object that can cache its inverse
+  {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {  #if inv is already compute, then write a message 
+    #and return the computed inversion
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get() #else compute the inversion
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
