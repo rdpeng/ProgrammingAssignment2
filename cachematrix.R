0b96d4ceebfcf76da34ab57f7112f3e98ced9721
millermor <millermor@gmail.com>
2015-10-25 13:47:58 -0500

    Matrix solve
    This s the full file with the code of matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	i <- NULL
+        set <- function(y){
+                x <<- y
+                i <<- NULL
+        }
+        #
+        get <- function() x
+        #
+        setinverse <- function(solve i <<- solve
+        #
+        getinverse <- function() i
+        #
+        list(set = set, get = get, setinverse = setinverse, 
+             getinverse = getinverse )
 
 }
 
@@ -12,4 +26,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        #
+        if(!is.null(i)){
+                message("Getting Cahe Data")
+                return(i)
+        }
+        #
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
