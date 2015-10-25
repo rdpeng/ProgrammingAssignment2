9fa8bc1d14c68e0910b5cac909f4b33704c2e383
cr00bert <croo.bert@gmail.com>
2015-10-25 16:12:51 +0000

    makeCacheMatrix
    makeCacheMatrix function defined within script

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i1 <- NULL
+        set <- function(y) {
+                x <<- y
+                i1 <<-NULL
+        }
+        get <- function() x
+        setinverse <- function(inv) i1 <<- inv
+        getinverse <- function() i1
+        list(set = set, get = get, setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
