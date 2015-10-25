07e093ba32e0a9bba2f86aebeaaf7b7b5a862eba
Gregory.Wilson <gregory.wilson@pearson.com>
2015-10-25 02:37:06 -0400

    completed assignment 2 - R Programming
    cacheMatrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## well my functions are going to do something I really don't 
+## understand yet.  Hmm well I can run square matrices through 
+## these functions....  And it does similar things as the CacheVector
+## example we were given.
+## I used 
+##       > neo <- stats::rnorm(16)
+##       > dim(neo) <- c(4,4)
+##       > neo
+## and
+##       > trinity <- stats::rnorm(9)
+##       > dim(trinity) <- c(3,3)
+##       > trinity
+## to test
 
-## Write a short comment describing this function
+## a blue Caterpillar smoking a hookah will come and 
+## magically make a chached Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve) 
 }
 
 
-## Write a short comment describing this function
+## Neo will take the Blue pill
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   s <- x$getsolve()
+   if(!is.null(s)) {
+      message("Getting Cached Matrix Inversion")
+      return(s)
+   }
+   data <- x$get()
+   s <- solve(data, ...)
+   x$setsolve(s)
+   s
 }
