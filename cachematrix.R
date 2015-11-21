52fa5b9d47f8e3cc8a434a2e388d61a5e177aa61
rusag <ruslan.aghalarov@gmail.com>
2015-11-21 22:25:42 +0400

    cachematrix
    A code which inverts a matrix. If the matrix has already been inverted
    the code prints the stored matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
 ## functions do
+##  We create a function that will construct a special matrix and then invert it
+## If it has already been inverted, it skips and prints the stored inverted matrix
 
-## Write a short comment describing this function
+## Create Special Matrix that is to be inverted
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setMatrix <- function(solve) m <<- solve
+  getMatrix <- function() m
+  list(set = set, get = get,
+       setMatrix = setMatrix,
+       getMatrix = getMatrix)
 }
 
 
-## Write a short comment describing this function
+
+## Invert the matrix using solve function. If is has already been inverted and stored print
+## "getting cached data" and return m
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getMatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setMatrix(m)
+  m
 }
