2cebce09db35f1cf224bae9a442d77f945c2b9dd
tmodi <tmodi1@outlook.com>
2015-11-22 13:36:37 -0600

    cacheMatrix Assignment
    Tushar Modi code submission for cacheMatrix Assignment - for Course
    rprog-034

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  varInv<-NULL
+  set<-function(y){
+    x<<-y
+    varInv<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) varInv<<- solve
+  getmatrix<-function() varInv
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  varInv<-x$getmatrix()
+  if(!is.null(varInv)){
+    message("Retrieving cached data")
+    return(varInv)
+  }
+  matrix<-x$get()
+  varInv<-solve(matrix, ...)
+  x$setmatrix(varInv)
+  varInv
 }
