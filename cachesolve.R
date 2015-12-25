11c945a11403ad3fbef461252472b27deae25b11
agreene16 <agreene16@wooster.edu>
2015-12-25 13:39:57 -0500

    Create cachesolve.R

---
+++ cachesolve.R
@@ -0,0 +1,11 @@
+cachesolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+}
