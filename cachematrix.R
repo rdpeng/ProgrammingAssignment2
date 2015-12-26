e9dbd85bd86531ccf8691bbf8d1f8d5c9107740e
Jason Kutarnia <jason.kutarnia@gmail.com>
2015-12-26 16:00:32 -0500

    Revert "Completed project 2"
    This reverts commit b4fcb99eb248acc5afc1d9d55054f2c4e607d59a.

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,15 @@
-## Create a cached matrix using scoping rules which also allows user to also cache its inverse
+## Put comments here that give an overall description of what your
+## functions do
 
-
-## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-      x_inv <- NULL
-      set <- function(y) {
-            x <<- y
-            x_inv <<- NULL
-      }
-      get <- function()
-            x
-      setinv <- function(inv)
-            x_inv <<- inv
-      getinv <- function()
-            x_inv
-      list(
-            set = set, get = get,
-            setinv = setinv,
-            getinv = getinv
-      )
+
 }
 
 
-## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-##             If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
-##             should retrieve the inverse from the cache.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-      ## Return a matrix that is the inverse of 'x'
-      inv <- x$getinv()
-      if (!is.null(inv)) {
-            message("getting cached data")
-            return(inv)
-      }
-      mtrx <- x$get()
-      inv <- solve(mtrx)
-      x$setinv(inv)
-      inv
+        ## Return a matrix that is the inverse of 'x'
 }
