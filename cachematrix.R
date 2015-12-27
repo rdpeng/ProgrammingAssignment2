933af72b56761af6303ad6c0ca65006b60622e5a
VasMau <mauricioa.vasquez@gmail.com>
2015-12-27 02:38:07 -0800

    ProgrammingAssignment2
    MauVas

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R-programming - assignment 2 
+## the follwing program creates a matrix, inverts it and display the time differences between cahed and run live
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## This will return a matrix that is the inverse of 'x'
+ }
+
+
+makeCacheMatrix <- function(x = matrix()) 
+
+{
+        ## x: an invertible matrix
+        ## result: a vector list containing functions to
+        ##              1. set the matrix
+        ##              2. get the matrix
+        ##              3. set the inverse
+        ##              4. get the inverse
+        ##         this list is scoped out as the input to the 
+        ##         cacheSolve() function
+        
+        inv = NULL
+        set = function(y) 
+           {
+                # use `<<-` to assign a value to an object in an environment 
+                # different from the current environment. 
+                x <<- y
+                inv <<- NULL
+           }
+        get = function() x
+        setinv = function(inverse) inv <<- inverse 
+        getinv = function() inv
+        list(set=set, 
+             get=get, 
+             setinv=setinv, 
+             getinv=getinv)
 }
