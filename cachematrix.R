f6a258cc28adf9e13c27acf62f73ac823c657944
Rich <richwgonzales@gmail.com>
2015-12-26 16:28:08 -0800

    Changed for test
    Testing github

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# This is a short note check github for my changes
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
