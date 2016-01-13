7d11d1d1fb0166a9f2091a730164466f526e9ceb
McWraith <mprzydacz@gmail.com>
2016-01-13 17:36:26 +0000

    First version
    Scaffold, not functioning

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(nrow = sqrt(length(x)))) {
+  cm <- NULL
+  create <- function(y) {
+    x <<- y
+    cm <<- NULL
+  }
+  retrieve <- function() x
+  solv <- function(solve) cm <<- solve
+  cache <- function() cm
+  list(create = create, retrieve = retrieve, solv = solv, cache = cache)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cm <- x$cache()
+  if(!is.null(cm)) {
+    message("retrieving cached data")
+    return(cm)
+  }
+  work <- x$retrieve()
+  cd <- solve(work)
+  x$sol(cm)
+  cm
 }
