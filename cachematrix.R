7051f11ffc96557aade0d95bf59b4f8ae878d2d6
frydaze1 <frydaze1@hotmail.com>
2016-01-18 10:15:28 -0800

    Completed Assignment
    Created and tested the two functions needed to complete the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a list of matrix functions for the input matrix
+## Cache values as a new matrix is given
 
-## Write a short comment describing this function
+## Create a matrix (assign to a variable - Ex: "v")
+## Pass that variable to this function, assigning the result to a new variable - Ex: "w"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Solve the input matrix using the functions from makeCacheMatrix
+## Use cached values when possible
+
+# Use the variable created when calling "makeCacheMatrix" - Ex: "w"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
