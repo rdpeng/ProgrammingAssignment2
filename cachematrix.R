43bc44e460b5ee46093e92a19d9df9613e65c99f
Kamikaze101 <clanwolfstrikers@gmail.com>
2016-01-24 19:32:34 -0800

    Cache's a matrix then returns the inverse
    Coursera week 3 assignment 2
    This pair of function will take a Matrix input and cache it. Then it
    will use the cached matrix and return its inverse with the Solve
    Function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of function will take a Matrix input and cache it. Then it will use the cached matrix
+## and return its inverse with the Solve Function
 
-## Write a short comment describing this function
+## This function takes a matrix and caches it creating a list of functions for the next function to use
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(a) {
+    x <<- a
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+##  This function uses the matrix cached by the previous function, applies the solve function to create
+## the inverse matrix, then returns the inverse matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
