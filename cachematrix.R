c8efb44f76d2ec5f36b10fffa22daeb4987811c4
Osvaldo <osvaldo.larancuent@gmail.com>
2016-02-07 08:57:25 -0400

    Finished cachematrix.R
    This is the completed assignment by Osvaldo Larancuent.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Programmer : Osvaldo
+## Date : 2/5/2016
+## Program name: cachematrix.R
+## Purpose: The II programming asignment from Coursera R Studio Module
+##          It is required to create function makeCacheMatrix, to assign the inverse of the matrix in memory
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  print(x)                  #Let's print the paramenter
+  m <- NULL                 #Create variable m, assigning null value
+  set <- function(y) {      #Function to store the value in memory
+    x <<- y                 #Double assignment store the value in other environment
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+##          It is required to create function cacheSolve, to get the solved matrix from memory, avoiding to recalculate it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting solved matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
