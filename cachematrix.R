94d891de29784e4e0cff2b1a7c6c2edd9851888b
bc1 <brittchris1@gmail.com>
2016-02-08 20:40:49 +0400

    RProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -23,3 +23,32 @@ my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
       [,1] [,2]
 [1,]   -2  1.5
 [2,]    1 -0.5
+cacheSolve(my_matrix)
+getting cached data
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> my_matrix$getInverse()
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> my_matrix$set(matrix(c(3, 2, 2, 3), 2, 2))
+> my_matrix$get()
+[,1] [,2]
+[1,]    3    2
+[2,]    2    3
+> my_matrix$getInverse()
+NULL
+> cacheSolve(my_matrix)
+[,1] [,2]
+[1,]  0.6 -0.4
+[2,] -0.4  0.6
+> cacheSolve(my_matrix)
+getting cached data
+[,1] [,2]
+[1,]  0.6 -0.4
+[2,] -0.4  0.6
+> my_matrix$getInverse()
+[,1] [,2]
+[1,]  0.6 -0.4
+[2,] -0.4  0.6
