090c8d0e4b2fe505fe6984b3f2141d7c11d1142e
swapnilj88 <swapnilj88@gmail.com>
2016-02-12 18:25:14 +0530

    Cache inverse of a Matrix by swapnilj88
    Functions to calculate and cache inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing matrix inversion is a system intensive task. We can optimize it by  
+## caching the inverse of a matrix when it is computed for the first time rather 
+## than computing it again and again if the matrix is not changed.
 
-## Write a short comment describing this function
+## This function creates special matrix object and returns a list of functions which 
+## are as follows:
+## set(y) -> Use it to set matrix value.
+## get()  -> Use it to get matrix value. 
+## setInverse(inverse)  -> Use it to set inverse of matrix.
+## getInverse()  -> Use it to get inverse of matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invx <- NULL
+  set <- function(y) {
+    x <<- y
+    invx <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) invx <<- inverse
+  getInverse <- function() invx
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of a special matrix object created by using 
+## makeCacheMatrix function. When called it will first check if inverse has already 
+## been calculated. If not then it will calculate the inverse of a given matrix   
+## and cache it so that the next time when function is invoked again for the 
+## same matrix it will return inverse from cache instead of calculating it again.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  mtrx <- x$get()
+  invx <- x$getInverse()
+  if(all(mtrx %*% invx == diag(nrow(mtrx)))) {
+    message("getting cached data")
+    return(invx)
+  }
+  invx <- solve(mtrx, ...)
+  x$setInverse(invx)
+  invx
 }
