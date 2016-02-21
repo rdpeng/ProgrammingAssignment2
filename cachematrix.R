3fe49532a177ae236bb69a8bc23c154143269451
jordan-mc <jordanc@hfgox01.hubb.local>
2016-02-22 10:53:32 +1100

    Invert Matrix
    Added code to invert matrix and cache result. Passed testing

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_matrix <- NULL
+        set <- function(y) {
+                x <<- y
+                inv_matrix <<- NULL
+        }
+        get <- function() x
+        setinv_matrix <- function(solve) inv_matrix <<- solve
+        getinv_matrix <- function() inv_matrix
+        list(set = set, get = get, 
+             setinv_matrix = setinv_matrix,
+             getinv_matrix = getinv_matrix)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv_matrix <- x$getinv_matrix()
+        if(!is.null(inv_matrix)) {
+                message("getting cached data")
+                return(inv_matrix)
+        }
+        data <- x$get()
+        inv_matrix <- solve(data, ...)
+        x$setinv_matrix(inv_matrix)
+        inv_matrix
 }
