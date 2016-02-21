bed4ea08d6111f7d37662ccf52584aa2a78267b8
daiane1989 <audreimarcelo@yahoo.com.br>
2016-02-21 13:57:30 -0300

    Revert "testando"
    This reverts commit 2835a5647173255441f054c6e0e40578f5d00d96.

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,15 @@
+## Put comments here that give an overall description of what your
+## functions do
 
-## Programa da semana 3 R Programming
-## Esta funcao armazena a inversa da matriz, com ela podemos armazenar
-## no cache para usar repetidamente.
-## A funcao abaixo cria um objeto especial "matriz" que pode armazenar
-## em cache a sua inversa.
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inversa <- NULL
-  set <- function(y) {
-    x <<- y
-    inversa <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(inverse) inversa <<- inverse
-  getInverse <- function() inversa
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
+
 }
 
 
-## A segunda funcao calcula a inversa da "matriz" que foi retornada
-## por makeCacheMatrix (a primeira funcao). Se a inversa foi calculada
-## (e a segunda nao mudou), a funcao cacheSolve deve recuperar a
-## inversa da cache.
-
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  inversa <- x$getInverse()
-  if(!is.null(inversa)) {
-    message("getting cached data")
-    return(inversa)
-  }
-  data <- x$get()
-  inversa <- solve(data, ...)
-  x$setInverse(inversa)
-  inversa
+        ## Return a matrix that is the inverse of 'x'
 }
-
-
