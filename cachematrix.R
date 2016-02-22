3fc089781ad450438cd715589810ce29c85bdf90
profun.org <profunorg@gmail.com>
2016-02-22 02:48:08 +0100

    assignment Coursera
    production of functions.

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,9 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+## This function has the purpose to show a contained inverse matrix.
+makeCacheMatrix <- function(x = matrix( c(1:3)), nrow=2, ncol=2) {}
+
+        ## This function returns the inverse of the makeCacheMatrix.
+        cacheSolve = makeCacheMatrix^-1
