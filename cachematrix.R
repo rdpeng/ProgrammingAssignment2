92ff61c94d623c526944a93e1a3151e690c12756
profun.org <profunorg@gmail.com>
2016-02-23 12:04:29 +0100

    cache value
    Inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,16 @@
 ## functions do
 
 ## Write a short comment describing this function
+This function is prompt to skip computation and accelerate processes. In this function we set the inverse of it self. To do it it has to be a squared matrix and its determinant has to be different of zero.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()nrow= 2, ncol=2) { x^-1 <<- cacheSolve
 
 }
 
 
 ## Write a short comment describing this function
+Cache solve returns the inverse of the function ()x  if and only if x is squared.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {if x is ^2 returns inverse matrix, else returns NA vallue
         ## Return a matrix that is the inverse of 'x'
 }
