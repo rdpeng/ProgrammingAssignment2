51f88c0ac28e339dce778da15788d9aa7736e4e7
CraigTedeton <craig.tedeton@gmail.com>
2016-03-01 13:17:29 -0500

    Assignment2
    Tedeton

---
+++ makeCacheMatrix.R
@@ -0,0 +1,29 @@
+makeCacheMatrix <- function(x = numeric()) {
+  cache <- NULL
+  
+  setMatrix <- function(newValue) {
+    x <<- newValue
+    cache <<- NULL
+  }
+  getMatrix <- function() {
+    x
+  }
+  cacheInverse <- function(solve) {
+    cache <<- solve
+  }
+  getInverse <- function() {
+    cache
+  }
+  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+}
+cacheSolve <- function(y, ...) {
+  inverse <- y$getInverse()
+  if(!is.null(inverse)) {
+    message("Get Cached Data...")
+    return(inverse)
+  }
+  data <- y$getMatrix()
+  inverse <- solve(data)
+  y$cacheInverse(inverse)
+  inverse
+}
