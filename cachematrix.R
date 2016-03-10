1e741f6115fc2de890d670429ed2b34a8ecbbd54
Mahmoud Aboaish <mahmoud_abo.aish@hotmail.com>
2016-03-10 18:28:09 +0200

    R-PL

--- cachematrix.R
+++ cachematrix.R
@@ -20,7 +20,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-cacheinv <- function(x, ...) {
+cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$get_inv()
         if(!is.null(inv)) {
