2f7ce7870771b69ea62537f9282d76c5ac283a9b
Neha-Sehrawat <neha.sehrawat100@gmail.com>
2016-03-13 00:15:08 +0530

    demo file only
    one line change

--- cachematrix.R
+++ cachematrix.R
@@ -9,7 +9,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-demo file
+demo file only
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
