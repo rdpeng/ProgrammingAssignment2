b9b664f9ec80d557382c410030b211b4082fd458
cubediva <jwofford01@att.net>
2016-03-27 15:40:54 -0500

    Submitting programming changes, JWofford

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeVector Function
+## from Coursera
+makeVector <- function(x = numeric()) {
+			m<- NULL
+			set <- function(y) {
+				x <<- y
+				m <<- NULL
+				}
+			get <- function() x
+			setmean <- function(mean) m <<- mean
+			getmean <- function() m
+			list(set=set, get=get,
+				setmean = setmean,
+				getmean = getmean)
+		}
 
-## Write a short comment describing this function
+cachemean <- function(x, ...){
+	m<- x$getmean()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+		}
+		data <- x$get()
+		m <- mean(data,...)
+		x$setmean(m)
+		m
+	}
+
+## creates a matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
+## Computes the inverse of the special matrix returned by the function above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
