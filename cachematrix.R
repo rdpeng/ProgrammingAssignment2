a7f9c4ce0580dc8cc4ea05361289457d4ebb1495
javierHernandezColorado <javier.hernandez.colorado@outlook.com>
2016-03-27 20:57:34 -0600

    Asigment 2 R language course
    functions for create a matrix and get its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a matrix with a new atribute call invAux wich is the place where it contains the inverse of the matrix
+## at the begginig this atribute is null.
 makeCacheMatrix <- function(x = matrix()) {
-
+        invAux <- NULL
+        set <- function(y) {
+                x <<- y
+                invAux <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) invAux <<- inverse
+        getInverse <- function() invAux
+        list(set = set,
+                get = get,
+                setInverse = setInverse,
+                getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+## This function recives a matrix created with the function makeCacheMatrix 
+## First evaluete if the matix is not null if is not then it returns the inverse of the matrix 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invAux <- x$getInverse()
+        if (!is.null(invAux)) {
+                message("getting cached data")
+                return(inv)
+        }
+        matAux <- x$get()
+        invAux <- solve(matAux, ...)
+        x$setInverse(invAux)
+        invAux
 }
