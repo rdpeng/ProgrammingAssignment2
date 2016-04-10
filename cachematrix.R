4a98fca6c466fee95bc69d5203419e946c41faca
Pedro <joao.pedro.gomes@icloud.com>
2016-04-11 00:19:23 +0200

    R programming week 3
    Week 3 programming submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions were development as part of the assignment of R Programming Week 3
 
-## Write a short comment describing this function
+## This function will cache the matrix and its inverse inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ##matrix inverse
+        inv<-NULL
+        set<-function(y){
+                x<<-y
+                inv<<-NULL
+        }
+        get<-function() x
+        setInv<-function(inverse)inv<<-inverse
+        getInv<-function() inv
+        list(set=set,get=get,
+             setInv=setInv,getInv=getInv)
 }
 
 
-## Write a short comment describing this function
+## This function determines the inverse of a matrix, but before doing any calculation it first checks the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv<-x$getInv()
+        if(!is.null(inv)){
+                message("Getting cached matrix data")
+                return(inv)
+        }
+        ## verify if the matrix is square
+        matrix<-x$get()
+        if(dim(matrix)[1]==dim(matrix)[2]){
+                inv<-solve(matrix)
+                x$setInv(inv)
+                inv
+        }else {
+                message("attempting to invert a non square matrix")
+                }
 }
