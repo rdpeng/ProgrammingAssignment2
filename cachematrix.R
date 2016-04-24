ef44cd15750986b40bd33453a111cbdc14a9b936
spguerrerot <sandraguerrerot@gmail.com>
2016-04-24 18:34:19 -0500

    Update cachematrix.R
    Hello, this is my submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author: Sandra Guerrero
+## functions do:  Return the inverse of matrix with determinant that is not zero, and save the result in cache
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+inverse <- NULL
+  set <- function(y) {
+    m <<- y
+    inverse <<- NULL
+  }
+  get <- function() m
+  setInverse <- function(solve) inverse <<- solve
+  getInverse <- function() inverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix,
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        inverse <- m$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- m$get()
+  inverse <- solve(data, ...)
+  m$setInverse(inverse)
+  inverse
 }
+
+# Example
+# matriz <- matrix(c(3:10, 10), 3, 3)
+# cacheSolve(makeCacheMatrix(matriz))
+# output
+#           [,1] [,2] [,3]
+# [1,] -3.333333    4   -1
+# [2,]  3.333333   -5    2
+# [3,] -1.000000    2   -1
