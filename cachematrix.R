ce6e7ac89a67f10d004cc8f9af3f3a067efd4baf
Pradyumna <prdmnds04@gmail.com>
2016-05-01 22:13:19 +0530

    Submitted my solution to the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculating the inverse of a square invertible matrix, using the cache mechanism to save time if the inverse is already solved.
 
-## Write a short comment describing this function
+## Initializes the caching mechanism
 
 makeCacheMatrix <- function(x = matrix()) {
+inv=NULL
+  set=function(y)
+  {
+    x<<- y
+    inv<<- NULL
+    
+  }
+get= function() {x}
+set_Inv= function(inverse){ inv<<- inverse }
+get_Inv= function(){ inv }
+list( set= set, get= get, set_Inv= set_Inv, get_Inv= get_Inv)
 
 }
 
 
-## Write a short comment describing this function
+## Retrives matrix inverse from cache if already solved, else solves freshly
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+		 inv= x$get_Inv()
+    if(!is.null(inv)) {
+       message("getting cached data")
+      return(inv)
+      
+    }
+  mat.data= x$get()
+  inv=solve(mat.data, ...)
+  x$set_Inv(inv)
+  return(inv)
+
+ }
