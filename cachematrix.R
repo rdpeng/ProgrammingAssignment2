3eaa9e14a6ed417bd798d01a3e6a7126ddbd39f0
TomBerning <tb3024@gmail.com>
2016-04-30 23:42:43 -0700

    Completed two functions for Assignment 2
    Two R stub files makeCacheMatrix and cacheSolve were given. The code was
    written.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions save computation time by caching the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of functions used to set or get the matrix,
+##         or set or get the value of the inverse.
+## Use:
+## Run this function once (outside the loop), e.g.
+##         mfun <- makeCacheMatrix(), and then mfun$set(x)
+##   OR    mfun <- makeCacheMatrix(x)
+## x is the matrix of which the inverse needs to be calculated
+## mfun is a list of matrix functions relating to the matrix x
+## To have the inverse returned, execute cacheSolve(mfun)
+## See also cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matrix_inverse <- NULL
+        set <- function(new_matrix) {
+                x <<- new_matrix
+                matrix_inverse <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(new_inv) matrix_inverse <<- new_inv
+        getinverse <- function() matrix_inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
+## cacheSolve
+## makeCacheMatrix must be run before running cacheSolve
+##         (see makeCacheMatrix)
+## If mfun was the output of makeCacheMatrix, execute 
+##         cachSolve(mfun) to have the matrix inverse returned
+## Should the origial matrix change to x2, issue mfun$set(x2)
+## cachSolve(mfun) will then return the inverse of x2
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mfun,...) {   ## ... more options for solve
+        inv <- mfun$getinverse()   
+        if(!is.null(inv)) {
+                message("Getting cached data ...")
+                return(inv)
+        }
+        original_matrix <- mfun$get()
+        inv <- solve(original_matrix,...)   ## ... more options for solve
+        mfun$setinverse(inv)
+        inv
 }
