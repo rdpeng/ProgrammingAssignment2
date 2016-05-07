fe7a5176d9deaeb1cb6c14dd3a3c709391ad1b8b
tracyredcross <tracyredcross@gmail.com>
2016-05-07 11:40:22 -0400

    cachematrix.R
    Class assignment / programming 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
 ## Put comments here that give an overall description of what your
-## functions do
-
+## functions do.  
+Functions tell the program what actions to take depending on the code that is entered.
 ## Write a short comment describing this function
-
+This function stores (x = matrix()).... in the makecachematrix
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
-
+This function solves for x.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+(matrix = x ())
