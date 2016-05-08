7c2bef0a6f8702117021171edcba5283a8d706f8
tracyredcross <tracyredcross@gmail.com>
2016-05-08 16:13:41 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@ Functions tell the program what actions to take depending on the code that is en
 ## Write a short comment describing this function
 This function stores (x = matrix()).... in the makecachematrix
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 
 }
 
@@ -12,5 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 This function solves for x.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+} m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
 (matrix = x ())
