9d3bbab108933afe414cb1c84d8519dac89a97d9
krishnabetai <krishbetai@gmail.com>
2016-05-09 08:43:50 +0530

    Coursera R Programming Assignment 2
    Created functions to create and return inverse of a matrix object from
    cache as per the instructions of the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -10,9 +10,15 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  inv = x$getinv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
