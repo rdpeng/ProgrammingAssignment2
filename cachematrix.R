cd9dd5aa801d311c77c0cf07e116864e3c5f50f4
chethankt <chethankt@gmail.com>
2016-06-01 07:13:07 +0530

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  nulvar <- NULL
+  set <- function(y) {
+    x <<- y
+    nulvar <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) nulvar <<- inverse
+  getInverse <- function() nulvar
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  nulval <- x$getInverse()
+  if (!is.null(nulval)) {
+    message("getting cached data")
+    return(nulval)
+  }
+  mat <- x$get()
+  nulval <- solve(mat, ...)
+  x$setInverse(nulval)
+  nulval
 }
