6bc191d30c27df26ba0e2decac07b40eeaf8f48e
jahidjp <jahidul_hasan@hotmail.com>
2016-06-03 15:27:57 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,29 +6,28 @@
 # Creates a list containing to set/get the value of the matrix 
 # set/get the value of inverse of the matrix
 
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
 makeCacheMatrix <- function(x = matrix()) {
-      i <- NULL
-      set <- function(y) {
-            x <<- y
-            i <<- NULL
-      }
-      
-      get <- function() x
-      setInverse <- function(solve) m <<- solve
-      getInverse <- function() m
-      
-      list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
-}
-
-cacheSolve <- function(x, ...) {
-    inv <- x$getInverse()
-    if(!is.null(inv)) {
-        message("Getting cached data.")
-        return(inv)
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
     }
-    data <- x$get()
-    inv <- solve(data)
-    x$setInverse(inv)
-    inv
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
