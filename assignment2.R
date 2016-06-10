c2c94bbb5fc9d269db5806b2e3d171bdd20a9534
faisalkabir198 <faisalkabir.eee@gmail.com>
2016-06-09 19:20:20 -0600

    Update assignment2.R

--- assignment2.R
+++ assignment2.R
@@ -1,26 +1,39 @@
 makeCacheMatrix <- function(x = matrix()){    
-    m <- NULL
+ ##This function create a matrix (square matrix) and perform the following
+  ##1.set the value of the matrix
+  ##2.get the value of the matrix
+  ##3.set the value of the inverse
+  ##4.get the value of the inverse
+
+    m <- NULL ## INVS is inverse of the matrix
    set <- function(y){
-        x <<- y  
+        x <<- y  # Assign a value using <<- to x and INVS different from the current environment
        m <<- NULL
      }
-    get <- function() x 
-    setInverse <- function(solve) m<<- solve 
-    getInverse <- function() m 
+    get <- function() x ## get x the matrix
+    setInverse <- function(solve) m<<- solve  ## set the inverse value using solve function
+
+    getInverse <- function() m ## get the inverse value
    list(set = set, get = get,
                  setInverse = setInverse,
-                 getInverse = getInverse)  s
+                 getInverse = getInverse)  
 }
+## This function check for the matrix inverse if it was calculated using makeCacheMatrix, 
+## and if not then it calculate it.
 
 
   cacheSolve <- function(x, ...) {
+  ## get the inverse of the matrix
        m <- x$getInverse()                 
        if(!is.null(m)){                    
             message("getting cached data")     
            return(m)                        
           }
-        data <- x$get()                    
-        m <- solve(data, ...)               
-        x$setInverse(m)                    
+          ##if the inverse (m) is null then calculate the inverse
+        data <- x$get()    ## get the matrix                 
+        m <- solve(data, ...) ## calculate the inverse of the matrix using solve function              
+        x$setInverse(m)       ## sets the value of the inverse in the cache using setinv
+        return(m)           ##return thr inverse
+
   }
   
