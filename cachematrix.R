2665d5cf0f9ae57bc27fdbe1dcdf7961afee0d32
Prerit-112 <prerit.112@gmail.com>
2016-06-11 20:11:23 +0530

    Revert "Assignment 2"
    This reverts commit 97c564efd2b1ba9700373946cdd444d6b5f0210a.

--- cachematrix.R
+++ cachematrix.R
@@ -1,54 +1,15 @@
-## Caching the Inverse of a Matrix:
+## Put comments here that give an overall description of what your
+## functions do
 
+## Write a short comment describing this function
 
-## This function creates a special "matrix" object that can cache its inverse.
-## The special "matrix" here is named as "Special_mat"
-## The inverse is named as "Inv_mat"
-
-
-makeCacheMatrix <- function(Special_mat = matrix()) {
-  Inv_mat <- NULL
- 
-   set <- function(new_mat) {
-    
-    Special_mat <<- new_mat
-    Inv_mat <<- NULL
-    Special_mat
-  }
-  
-  get <- function() Special_mat
-  
-  setinverse <- function(inverse){
-   Inv_mat <<- inverse
-    
-  }
-  
-  getinverse <- function() Inv_mat
-  
-  list(set = set,
-       get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-## This function computes the inverse of the Special_mat created by makeCacheMatrix above.
-##If the inverse has already been calculated (and the matrix has not changed),
-##then it should retrieve the inverse from the cache.
 
-cacheSolve <- function(Special_mat) {
-        ## Return a matrix that is the inverse of 'Special_mat'
-  
-  Inv_mat <- Special_mat$getinverse()
-  
-  if(!is.null(Inv_mat)) {
-    cat("getting cached data")
-    return(Inv_mat)
-  }
-  
-  data <- Special_mat$get()
-  Inv_mat <- solve(data)
-  Special_mat$setinverse(Inv_mat)
-  Inv_mat
-  
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
