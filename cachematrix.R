0bcb22a841b854aa5bd1d07bb662c2fd860fbdcf
tracyredcross <tracyredcross@gmail.com>
2016-06-13 14:51:42 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,36 @@ Two functions that are used to create a special object that stores a numeric vec
 ## assigns object a value different from current environment
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
         
 
-}
+
 
 
 
 ## calculates the mean and places it in setmean function unless it is already calculated then it will get the mean from the cache
 cacheSolve <- function(x, ...) {
-       
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
+       
+
 
