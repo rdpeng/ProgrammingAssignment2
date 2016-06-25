3ec82c75536a12e186fd5c7a487cf3e3801a7fdd
Wolfgang <wolfgang.spindeler@gmail.com>
2016-06-25 19:22:30 +0200

    added code to solveMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The defined functions yield a method for finding the invesre to a
+## invertibel square matrix, where the result is read from cache in
+## case that it was already calculted before.
+## For a matrix X this is done via cacheSolve(makeCacheMatrix(X))
 
-## Write a short comment describing this function
+
+## makeCacheMatrix takes a solvable square matrix x as an argument
+## and returns a list which serves as input for cacheSolve(). 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setsolve <- function(solve){
+    s <<- solve
+  }
+  getsolve <- function(){
+    s
+  }
+  list(set = set,
+       get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as input a list which is returned by
+## makeCacheMatrix, e.g. makeCacheMatrix(x), and returns 
+## the inverse of x. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
