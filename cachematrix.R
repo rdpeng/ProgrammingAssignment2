9ac820a9a88a6d5b3455bd03d7016ab498cece73
bagp <prashantbagad25@gmail.com>
2016-06-29 12:41:57 +0530

    programming-assignment-2-lexical-scoping
    R file contains both makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+## the function makeCacheMatrix does following:
+## 1. set the matrix 	
+## 2. get the matrix
+## 3. set the inverse of matrix
+## 4. get the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse1) inverse <<- inverse1
+    getinverse <- function() inverse
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## the function cachesolve returns inverse of matrix. It computes inverse if 
+## already not done and returns inverse. But if inverse has already been 
+## calculated , it directly return the inverse values.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached data.")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data)
+    x$setinverse(inverse)
+    Inverse
+
+    }
