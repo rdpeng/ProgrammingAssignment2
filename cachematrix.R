d0a06cfab7c80d39c73cb0130ac61c278ed9de80
aman7soni <aman7soni@gmail.com>
2016-07-03 14:15:17 -0500

    Programming Assignment 2
    makeCachematrix and cachesolve edited.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        setmat <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        getmat <- function() x
+        setmatInverse <- function(inverse) i <<- inverse
+        getmatInverse <- function() i
+        list(
+                setmat = setmat,
+                getmat = getmat,
+                setmatInverse = setmatInverse,
+                getmatInverse = getmatInverse)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getmatInverse()
+        if (!is.null(inv)) {
+                message("Receiving Cache")
+                return(inv)
+        }
+        mat <- x$getmat()
+        inv <- solve(mat, ...)
+        x$setmatInverse(inv)
+        inv
 }
