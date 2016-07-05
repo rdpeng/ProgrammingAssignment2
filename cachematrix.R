e556dcc69560687091b51dbd2e186537be0444e9
nightrose79 <yy235@cornell.edu>
2016-07-04 21:25:31 -0400

    yy235@cornell.edu made changes to the solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Caching the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- matrix()
+    set <- function(y) {
+        x <<- y
+        inv <<- matrix()
+    }
+    get <- function() x
+    setinv <- function(matrix_inv) inv <<- matrix_inv
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## check whether a inverted matrix is cached, if not, cache it, otherwise read from the cached version.  
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.na(inv[1,1])) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
