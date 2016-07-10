df7e8c3de2599fd2246e08f453958cd8be2d4787
alexnovis <a.f.novis@hotmail.com>
2016-07-10 10:37:01 -0400

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,30 +1,38 @@
 
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
-}
-cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data.")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data)
-  x$setinverse(inv)
-  inv
-}
-m <- matrix(1:4, 2, 2)
-x <- makeCacheMatrix(m)
-x$get()
-cacheSolve(x)
-cacheSolve(x)
-
-
+> makeCacheMatrix <- function(x = matrix()) {
++     inv <- NULL
++     set <- function(y) {
++         x <<- y
++         inv <<- NULL
++     }
++     get <- function() x
++     setinverse <- function(inverse) inv <<- inverse
++     getinverse <- function() inv
++     list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
++ }
+> cacheSolve <- function(x, ...) {
++     inv <- x$getinverse()
++     if(!is.null(inv)) {
++         message("getting cached data.")
++         return(inv)
++     }
++     data <- x$get()
++     inv <- solve(data)
++     x$setinverse(inv)
++     inv
++ }
+> m <- matrix(1:4, 2, 2)
+> x <- makeCacheMatrix(m)
+> x$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> cacheSolve(x)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(x)
+getting cached data.
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
