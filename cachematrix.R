e24df1facd4caa363be0fea18eaf9cc10fb8f93f
erblast <erblast2001@gmx.de>
2016-07-11 20:26:42 +0200

    completed
    completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the first function serves as a container for a cached matrix object storing a matrix
+## and its inversion. the second function calculates the inversion of a given matrix and uses 
+## an instance of the first function to store the calculated results.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a namespace for two variables, mat and inv_mat and returns 4 functions for
+## setting and getting the values of those variables
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function() {
+    
+    mat     = matrix()
+    inv_mat = matrix()
+    
+    get_matrix      = function() mat
+    get_inv_matrix  = function() inv_mat
+    set_matrix      = function( new_mat ){ mat <<- new_mat }
+    set_inv_matrix  = function( new_inv_mat ){ inv_mat <<- new_inv_mat }
+    
+    return ( list( get_matrix     = get_matrix,
+                   get_inv_matrix = get_inv_matrix,
+                   set_matrix     = set_matrix,
+                   set_inv_matrix = set_inv_matrix
+                  )
+           )
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inversion of a given matrix if inverted matrix not already stored in cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cache_matrix, mat ) {
+    
+    if ( !identical( cache_matrix$get_matrix(), mat ) ){
+        
+        inv_mat = solve(mat)
+        
+        cache_matrix$set_matrix( mat)
+        cache_matrix$set_inv_matrix( inv_mat )
+        print('new inverted matrix calculated')
+        
+    }
+    else{
+        print('inverted matrix recovered from cache')
+    }
+    return ( cache_matrix$get_inv_matrix() )
 }
+
+# Code Testing the two functions
+
+# instantiate cache
+cache = makeCacheMatrix()
+
+# generate two different matrices
+mat1 = matrix(runif(9), 3, 3)
+mat2 = matrix(runif(9), 3, 3)
+
+# calculate inversions
+cacheSolve (cache, mat1)
+cacheSolve (cache, mat1)
+cacheSolve (cache, mat2)
+
+
