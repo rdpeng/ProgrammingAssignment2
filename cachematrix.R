9e590f9c3df7d8abdb391c8a909607edf1d80530
fouadsel88 <fou_inps88@yahoo.fr>
2016-07-25 02:57:25 -0400

    cache matrix last code

--- cachematrix.R
+++ cachematrix.R
@@ -2,10 +2,9 @@
 ## functions do
 
 ## Write a short comment describing this function
-## this function creat matrix cache the inverse of this matrix .
-
 
 makeCacheMatrix <- function(x = matrix()) {
+<<<<<<< HEAD
   inve <- NULL
   set <- function(y) {
     x <<- y
@@ -20,17 +19,14 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 ## This function calculaate the inverse of the matrix  created by makeCacheMatrix,
 ##if the inverse already existe of the same matrix then the function return inverse from the cache.
+=======
 
+}
+>>>>>>> parent of 442d896... cache matrix
+
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  
-  inve <- x$getinverse()
-  if (!is.null(inve)) {
-    message("getting cached data")
-    return(inve)
-  }
-  matr <- x$get()
-  inve <- solve(matr, ...)
-  x$setinverse(inve)
-  inve
+        ## Return a matrix that is the inverse of 'x'
 }
