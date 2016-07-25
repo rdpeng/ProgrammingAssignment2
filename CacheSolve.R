b33bcc9ab2f0564ee5d9fdedb5f189e469e37199
KaustavGhosh88 <ghosh.kaustav88@yahoo.com>
2016-07-26 00:58:41 +0530

    Add files via upload

--- CacheSolve.R
+++ CacheSolve.R
@@ -1,11 +1,14 @@
+#This function computes the inverse of the special matrix 
+returned by makeCacheMatrix 
+
 CacheSolve <- function(x,...){
-   inv<-x$get_inverse()
-   if(!is.null(inv)){
+   inv<-x$get_inverse() 
+   if(!is.null(inv)){ 						# checks to see if inverse has already been calculated
                     message ("getting cached data")
-                    return (inv)
+                    return (inv) 				# if inverse has been calculated return it and exit
                     }
    data<-x$get()
-   m<-solve(data,...)
-   x$set_inverse(inv)
+   m<-solve(data,...) # calculate inverse if it has not been calculated
+   x$set_inverse(inv) # sets the value of inverse in the cache
    inv
 }
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,12 +1,14 @@
-makeCacheMatrix<-function(x=matrix()){
-    inv<-NULL
-    set<-function(y){
+# funtion to create a special matrix object that can cache its inverse
+
+makeCacheMatrix<-function(x=matrix()){ #argument x defined with default mode of matrix
+    inv<-NULL				   #inv is initilized as NULL. inv will hold the inverse matrix 	
+    set<-function(y){   # set function defined that assigns new value of matrix in parent environment
                      x<<-y
-                     inv<<-NULL
+                     inv<<-NULL # incase of new matrix inverse is set to NULL
                     }
-    get<-function(){ x }
-    set_inverse<-function(solve){ inv<<-solve }
-    get_inverse<-function() { inv }
+    get<-function(){ x }  # get function returns the value of the matrix argument
+    set_inverse<-function(solve){ inv<<-solve } # assigns value of inverse in parent environment
+    get_inverse<-function() { inv }  # to get the value of inverse when called
   
-    list(set=set, get=get, set_inverse=set_inverse, get_inverse=get_inverse)
+    list(set=set, get=get, set_inverse=set_inverse, get_inverse=get_inverse) # needed to refer to the functions with the $ operator
 }
