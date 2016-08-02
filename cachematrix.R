f0f1adecbc37d3605b6ec2cdbabe4698be876af3
FranciscoACS <fcoalberto100@gmail.com>
2016-08-02 16:41:02 -0500

    Programming Assignment 2: Lexical Scoping
    Please let me know if I have commited correctly the Assignment.
    
    Greetings,
    Francisco Castellanos

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a matrix object that can cache its inverse,
+## in addition, it contains four functions
 
-## Write a short comment describing this function
+# get: returns the vector stored
+# set: changes the vector stored
+# setinverse: store the value of the input in a variable m
+# getinverse: returns the value described above in setinverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()) {
+  m<-NULL
+  set<-function(y) {
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setinverse<-function(solve) m<<- solve
+  getinverse<-function() m
+  list(set=set, get=get,
+       setinverse=setinverse,
+       getinverse=getinverse)
 }
 
+## cacheSolve computes the inverse of the matrix,
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x, ...) {
+  m<-x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+## Example with a random matrix
+
+rm<-diag(2,4)
+rm
+
+cmatrix<-makeCacheMatrix(rm)
+cacheSolve(cmatrix)
