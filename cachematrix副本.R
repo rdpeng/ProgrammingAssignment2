84af4ca5036759b7a0190a97fdd5f4dc036d3fb2
gaiissinging <gaiissinging@qq.com>
2016-08-09 11:30:54 +0800

    assignment2
    cache matrix

---
+++ cachematrix副本.R
@@ -0,0 +1,36 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = NULL) {
+    m<- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function()x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list( set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
+}
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x,...){
+    m <- x$getinv()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data,...)
+    x$setinv(m)
+    m
+}
+
+
+
+
