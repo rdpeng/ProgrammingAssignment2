b6887bd3c5f57756e475bc1764e9b24b8e91d87a
Henry Baez <0072016@users.noreply.github.com>
2016-09-12 02:44:11 -0700

    update codeclimate.yml

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+   -  <patch <path/to/codeclimate-config.patch>
+    -   configuration patch file::
+    -  codeclimate--config.patch
+    -  diff --git a/.codeclimate.yml b/.codeclimate.yml
+    -  index e69de29..f3c2a9b 100644
+   -  ---a/.codeclimate.yml
+    +++ b/.codeclimate.yml
+   @@ -0,0 +1,7 @@
+  -  +---
+  -  + engines::
+   - + fixme::
+   -  + enabled::  true
+   -  +  ratings::
+    -  + exclude_paths:: []
+    < patch <path/to/codeclimate-config.patch>
+   @0072016
+   foo.gradle@gmail.com
+   September 12, 2016
+   02:44:03PST
