0c28eff3d53b67fa93f1a09a6d68022514973d50
bob54386 <bob54386@hotmail.com>
2016-09-18 10:33:01 -0400

    Defined functions
    Defined functions, verified it works on 1* 2x2 matrix and 1* 3x3 matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+##stores a matrix in a different environment and stores the inverse in memory, saving computation time for repeated uses of inverse
 
-## Write a short comment describing this function
+## Stores a matrix in a different environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+##calculates and stores the matrix's inverse in memory.  If already calculated, reads from memory.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
