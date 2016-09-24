ce4230a3fb25e3c90e4e41e88e6060b30fd1ab64
NehaJagdeesh <neha.jagdeesh@gmail.com>
2016-09-24 14:48:48 +0530

    solve inverse
    week 3 graded assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+    #Set function and assign y to x
+  }
+  get <- function() x
+  setminverse <- function(minverse) m <<- minverse
+  getminverse <- function() minverse
+  list(set = set, get = get,
+       setminverse = setminverse,
+       getminverse = getminverse)
+       #get inverse
+}
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {m <- x$getminverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setminverse(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
