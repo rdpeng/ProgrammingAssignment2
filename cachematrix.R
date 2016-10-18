f703a551c58efc4b47febed65d573bdeb21d0a8f
Loku Dassanayake <dassa.dml@gmail.com>
2016-10-18 11:23:51 +1100

    Update cachematrix.R
    Initial description added.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## THe perpose of this R function is to calculate the inverse of a square matrix and cache it so that it can be used lateron from the cache.
+## function makeCacheMatrix creates a special "Matrix", which containins the functions to 
+## set the value of the Matrix
+## get the value of the Matrix
+## set the value of the inverse of the Matrix
+## get the value of the inverse of the Matrix
+##
+## The acheSolve function calculates the inverse of the special "Matrix" created with the above function. However, 
+##        it first checks to see if the mean has already been calculated. 
+##                If so, it gets the mean from the cache and skips the computation. Otherwise, 
+##        it calculates the mean of the data and sets the value of the mean in the cache via the setmean function. 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+m
+akeCacheMatrix <- function(x = matrix()) {
 
 }
 
