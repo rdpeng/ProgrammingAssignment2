23e85e43ee9ebaee5905c090873ae498e3b87de2
MAHALAKSHMIPARTHIBAN <rtr.p.mahalakshmi@gmail.com>
2016-10-20 12:02:44 +0530

    some more on typo

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## [function to make cache matrix]
 
 makeCacheMatrix <- function(x = matrix()) {
+nv <- NULL
+        set <- function(y) {
+                x <<- y
+                nv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) nv <<- inverse
+        getinverse <- function() nv
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+nv <- x$getInverse()
+        if (!is.null(nv)) {
+                message("getting cached data")
+                return(nv)
+        }
+        mat <- x$get()
+        nv <- solve(mat, ...)
+        x$setinverse(nv)
+        nv
 }
