2111e16035f24e0db39a8a59a0825e93e76740eb
amymao64 <ymao64@gmail.com>
2016-11-07 01:02:05 -0600

    Second edit on my HW2 code
    I add my code for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -10,6 +10,40 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+
+##Function makeCacheMatrix returns a matrix which will be used as an input to cacheSolve function for the inversion of matrix
+##x is defined as a square invertible matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inv = NULL
+  set = function(y) {
+    # assign a value in an environment 
+    x <<- y
+    inv <<- NULL # to store inversion
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+## return the inverse of the original matrix 
+          
+          
+ #This function calculates the inverse of the special "matrix" created with the above function. 
+ ##Have to checksif the inverse has already been calculated. If not, calculates the inverse of the data and sets the value of the inverse in the cache via the setmatrix function.
+ cacheSolve <- function(x) {
+  
+   inv = x$getinv()
+   if (!is.null(inv)){
+     message("getting cached data")
+     return(inv)
+   }
+ 
+   data = x$get()
+   inv = solve(data)
+   x$setinv(inv)
+   return(inv)
+ }
