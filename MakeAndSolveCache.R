f44cd17cbd3bd1d511de8db64146dca948d5ebb1
andreashaager <andreashaager@46-127-42-19.dynamic.hispeed.ch>
2016-11-27 15:06:55 +0100

    Coursera Data Sicientist , R Programming lexical scoping
    makeCacheMatrix

---
+++ MakeAndSolveCache.R
@@ -0,0 +1,33 @@
+##  Coursera Data Sicientist , R Programming lexical scoping
+##  create, cache and resolve  a inverse matrix 
+##  https://github.com/andreashaager/ProgrammingAssignment2/commit/e4eed4153bd237a2dd4532fc3f40ce0e22fd0cb7
+##
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(
+    set = set,
+    get = get,
+    setinverse = setinverse,
+    getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
