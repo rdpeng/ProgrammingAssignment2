7966c20000eddcabf9debce405b0492885220d92
STsapko <stsapko@gmail.com>
2016-12-13 05:29:47 +0200

    This is changes in template which was given together with the task
    I took an example about myVector in the task and have changed according
    to my understanding how to meet the task.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im <- NULL
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) im <<- solve
+  getsolve <- function() im
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
+  im <- x$getsolve()
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data, ...) %*% data
+  x$setsolve(im)
+  im
         ## Return a matrix that is the inverse of 'x'
 }
