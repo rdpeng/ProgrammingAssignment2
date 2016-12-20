2def8c9a697530688b88daaf75716acb340b87c9
ckcoursera <caitlinkennedy439@gmail.com>
2016-12-20 12:21:27 -0500

    Add files via upload
    CKcoursera/Assignment 2/Looping Functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## overall description of functions for this assignment
+## The functions in this assignment are designed to cache the inverse of a matrix
 
-## Write a short comment describing this function
-
+## Comments describing this function
+##makeCacheMatrix: This function creates a special "matrix"object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+ matinverse <-NULL
+ set <- function(y){
+   x<<- y
+   matinverse <<-NULL
+ }
+ get <-function()x
+ setInverse <-function(inverse) matinverse<<-inverse 
+ getInverse <-function()matinverse
+ list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+##cacheSolve: This function computes the inverse of the special "matrix"returned by makeCacheMatrix
+##above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve
+##should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  matinverse <- x$getInverse()
+  if(!is.null(matinverse)){
+    message("getting cached data")
+    return(matinverse)
+  }
+  matx <-x$get()
+  matinverse <-solve(matx, ...)
+  x$setInverse(matinverse)
+  matinverse
 }
+
