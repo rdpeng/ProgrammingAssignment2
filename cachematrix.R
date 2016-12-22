6abe4c1f02bfdd20f2e9c02fc9173c7c7e92663c
Peter Baumgartner <peter.baumgartner@donau-uni.ac.at>
2016-12-22 11:35:21 +0100

    my first commit
    I changed the comment to describe the function

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
