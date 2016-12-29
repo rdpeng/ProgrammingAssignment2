0aa283a45556c0785ec95e0206f7941deb075db9
etsafe11 <etsafe11@gmail.com>
2016-12-29 18:27:09 -0500

    Eric's Inverse Cached Matrix
    All functions are working fine, unlss I'm supposed to be receiving the
    "getting cached data" message.  (I recall getting it previously but can
    no longer replicate.)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Inverse Caching:
+## The functions below allow you to save processing time in R when finding
+## the inverse of a matrix or series of matrices using caching. These functions
+## save memory and improve calculation speed because the matrix inverse is 
+## cached.
 
-## Write a short comment describing this function
+## makeCacheMatrix() takes a matrix 'x' as argument and makes a list of 
+## functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        B <- NULL
+        set <- function(y){
+                x <<- y
+                B <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) B <<- inverse
+        getInverse <- function() B
+        list(set = set, get = get, setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
+##cacheSolve() prints a cached inverse matrix; the inverse of original matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+        B <- x$getInverse()         
+        if(!is.null(B)){            
+                message("getting cached data")
+                return(B)
+        }
+        data <- x$get()
+        B <- solve(data, ...)
+        x$setInverse(B)
+        B
 }
+
+x <- matrix(c(-4, 0, 0, -4), 2, 2)    #Create the original matrix 'x' here.
+myMatrix <- makeCacheMatrix(x)
+cacheSolve(myMatrix)
