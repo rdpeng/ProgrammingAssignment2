17f3c3ad9969e74f197ab69c8c04222e7a8ef361
Lital Chartarifsky <litalchart@gmail.com>
2017-01-09 16:27:13 -0500

    Future Function Description
    I describe the functions that will be written for this assignment.
    Still no functions written though

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code will cache the inverse of a matrix so we don't have to compute it every time.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix as it's input and returns a list of functions.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve inverts the matrix created with the above function. It first checks to 
+## see if the inverse has already been calculated. If so, it `get`s the inverse from the
+## cache and skips the computation. Otherwise, it calculates the inverse of
+## the data and sets it in the cache via the `setInverse` function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
