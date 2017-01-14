395d2cbf01a141bdfc858a104636573540c60987
jaz96 <ajain19@u.rochester.edu>
2017-01-14 17:03:40 -0500

    Revert "A few more comments on what to do"
    This reverts commit e4eed4153bd237a2dd4532fc3f40ce0e22fd0cb7.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function caches the inverse of a square matrix. Matrix inversion is typically a strenuous computation; caching the inverse can be easier than repeatedly computing the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## This function inputs a list of functions 1) set the matrix 2) get the matrix 3) set the inverse and 4) get the inverse, to cache the inverse
+inv <- NULL
+set <- function (y) {
+	##'<<-' is used to assign a value to an object that is in a different evironment than the current one 
+	x <<- y
+	inv <<- NULL 
+	}
+	get <- function() x
+	setinv <- function(inverse) inv <<- inverse
+	getinv <- function() inv
+	list(set=set, get=get, setinv=setinv, getinverse=getinv)
 }
 
-
-## Write a short comment describing this function
-
+## This function returns the inverse of a square, invertible matrix. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Returns the inverse of matrix 'x'
+        inv <- x$getinv()
+        ## If the inverse was already calculated
+        if(!is.null(inv)){
+        	message("getting cached data")
+        	return(inv)
+        	}
+        ## If not, calculates the inverse
+        c.data <- x$get()
+        inv <- solve(c.data, ...)
+        x$setinv(inv)
+        
+        return(inv)
 }
+
+## Sample test 
+m <- diag(7,4)
+m
+CachedMatrix <- makeCacheMatrix(m)
+cacheSolve(CachedMatrix)
