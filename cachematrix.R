8672d6badc0c3f1a4935a5d860b94f19c8aedc1f
HamedDadras <Hamed.d89@gmail.com>
2017-01-31 23:56:57 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function takes care of setting and getting Matrices as well as caching and returning them
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = numeric()) {
+    
+    # Initializes cache variable with null value
+    cache <- NULL
+            
+    # Sets a matrix
+    setMatrix <- function(mat) {
+    x <<- mat
+    # As the matrix has a new value, the cache variable gets flushed
+    cache <<- NULL
+    }
+  
+    # Returns the stored matrix
+    getMatrix <- function() {
+    return (x)
+    }
+       
+    # Caches the argument as value 
+    cacheInverse <- function(solve) {
+    cache <<- solve
+    }
+          
+    # Gets the cached value
+    getInverse <- function() {
+    cache
+    }
+                
+    # Returns the list of created functions
+    ist(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+
+
+## The cacheSolve function takes care of inversing the matrices
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Gets the cached value
+  inversedMat <- x$getInverse()
+  # Checks if the value is cached and in case of existence, returns that 
+  if(!is.null(inversedMat)) {
+  message("getting cached data")
+  return(inversedMat)
+  }
+  # If cache is empty, inverses the matrix and fills the cache
+  
+  matr <- x$getMatrix()
+  inversedMat <- solve(matr)
+  x$cacheInverse(inversedMat)
+         
+  # returns the inversed Matrix
+  return(inversedMat)
 }
+
+
