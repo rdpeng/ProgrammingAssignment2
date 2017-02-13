d3429e886dff70a1a30e1182309d6ac587b047ec
Encan <encanwang@gmail.com>
2017-02-12 23:50:11 -0500

    Update Cachematrix.R

--- Cachematrix.R
+++ Cachematrix.R
@@ -1,3 +1,8 @@
+#The following two functions are able to speed up the calculation by looking 
+#for results in the cache. 
+
+#The first function creates a list containing several functions which set the
+#value of the matrix, in other words, create a matrix object. 
 makeCacheMatrix <- function(x = matrix()) {         
   o <- NULL
   set <- function(y) {
@@ -11,6 +16,11 @@ makeCacheMatrix <- function(x = matrix()) {
        setinverse = setinverse,
        getinverse = getinverse)
 }
+
+
+#the second function is critical in identifying whether the inverse result 
+#could be taken out from the cache, otherwise, it would implement the inverse 
+#action within the function itself. 
 
 cacheSolve<- function(x, ...) {
   o <- x$getinverse()
