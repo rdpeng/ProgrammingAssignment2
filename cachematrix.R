e0cfc6dd882afa47025b5ad1fb3380eee4a42ea0
jbehrens1 <jeffb@post.harvard.edu>
2017-03-04 17:26:35 -0500

    Jeff's contribution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author Jeff Behrens  jeffb@post.harvard.edu
 
-## Write a short comment describing this function
+## Syntax to test:
+## cacheSolve(makeCacheMatrix(m))
+## Where m is a matrix, assume it is invertible (i.e. not singular)
+
+## create a function/vector that defines 4 functions:
+##      set
+##      get
+##      setsolve
+##      getsolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## function to solve a matrix, but check first if already solved (i.e. m not null)
+## stores result in a cached form, global (parent?) environment, in variable m
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    data <- x$get()
+    
+    ## put some error checking in anyways
+    if (det(data)==0) {
+        print("Sorry, this matrix has a determinant of 0")
+        return()
+    }
+    m <- solve(data)
+    x$setsolve(m)
+    m
 }
