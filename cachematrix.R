d33e68fc97629782c1145ac456d5156fa6d009ea
Reem Zurnaji <[rimi.zurnaji@gmail.com]>
2017-03-20 03:57:54 +0400

    This is my R week 3 assignment done finaaaally :) wood woooop

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
+##  makeCacheMatrics function is for caching the matrix and its inverse where:
+## get funcion: gets the matrics value
+## set function: resets the value of the matrics and the inverse
+## getinverse function: gets the value of the inverse
+## setinverse function: sets the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv_x<-matrix() #inv_x is the variable to store the matrics inverse and here is has one NA value 
+    set<-function(new_matrix)
+        {
+        x<<-new_matrix
+        inv_x<<-matrix()
+        }
 
+    get<-function() x
+    setinverse <-function(inverse) inv_x<<-inverse 
+    getinverse <- function() inv_x
+    list(get=get,set=set,setinverse=setinverse,getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## cacheSolve function calculates the Inverse of a square matrics if it is a new matrics if it was
+##calculated before it will return the cached matrix :)
 cacheSolve <- function(x, ...) {
+    inv_x<-x$getinverse()
+    if ( !is.na(inv_x))
+        {
+        message("getting cached Inverse Matrix")
+        return(inv_x)
+        }
+    x_mat<-x$get()
+   inv_x<-solve(x_mat)
+   x$setinverse(inv_x)
+   inv_x
         ## Return a matrix that is the inverse of 'x'
 }
