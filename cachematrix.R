3588b7462173b9d1e02f7566c003b0cce5b5acb7
Jong-dae Kim <arcgura@gmail.com>
2017-03-26 23:13:08 +0900

    Coursera - ProgrammingAssignment2
    just add blank

--- cachematrix.R
+++ cachematrix.R
@@ -4,16 +4,16 @@
 ## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  list(set = set, get = get,
-       setinverse = setinverse, getinverse = getinverse)
+      inv <- NULL
+      set <- function(y) {
+          x <<- y
+          inv <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inverse) inv <<- inverse
+      getinverse <- function() inv
+      list(set = set, get = get,
+           setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -24,14 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if (!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  raw.mat <- x$get()
-  inv <- solve(raw.mat)
-  x$setinverse(inv)
-  inv
+      inv <- x$getinverse()
+      if (!is.null(inv)) {
+          message("getting cached data")
+          return(inv)
+      }
+      raw.mat <- x$get()
+      inv <- solve(raw.mat)
+      x$setinverse(inv)
+      inv
   ## Return a matrix that is the inverse of 'x'
 }
