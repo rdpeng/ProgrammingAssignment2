5501fa5fab8160ef2b4e40ea06216b06a774813b
eniyei <overthegame.tihomira@gmail.com>
2017-03-27 20:28:59 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -20,7 +20,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-# cacheSolve: the function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+# cacheSolve: the function computes the inverse of the special "matrix" 
+# returned by makeCacheMatrix above. If the inverse has already been calculated 
+# (and the matrix has not changed), then the cachesolve should retrieve 
+# the inverse from the cache.
 
 
 cacheSolve <- function(x, ...) {
