a69ca451cca4740ed2e16c93794c15f549d6c611
Jian He <hejianpitt@gmail.com>
2017-04-02 11:16:14 +0800

    Update cachematrix.R
    Finish the first function, makeCacheMatrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to 
+## 1 
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+                i <- NULL
+                set <- function(y) {
+                        x <<- y
+                        i <<- NULL
+                }
+                get <- function() x
+                setinverse <- function(inverse) i <<- inverse
+                getinverse <- function() m
+                list(set = set, get = get,
+                     setinverse = setinverse,
+                     getinverse = getinverse)
+}                      
 
 
 ## Write a short comment describing this function
