85c1d01d743afd98ad7f7ea012a4da08d32e9e4e
Jian He <hejianpitt@gmail.com>
2017-04-04 21:59:51 +0800

    Update cachematrix.R
    add a return statement to the if block.

--- cachematrix.R
+++ cachematrix.R
@@ -32,6 +32,7 @@ cacheSolve <- function(x, ...) {
     i <- x$getinverse()
     if(!is.null(i)) {
         message("getting cached data")
+        return(i)
     }
     data <- x$get()
     i <- solve(data,...)
