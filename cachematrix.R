4fe50b16c6ccd4a960b0ef14d6ea6e656b53a466
hadoora <eyoland@gmail.com>
2017-04-09 18:52:13 +0100

    solving the inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## this function sets up the matrix
 
-makeCacheMatrix <- function(x=matrix(rnorm(16),4,4)) {
+makeCacheMatrix <- function(x=matrix(rnorm(16),4,4)){
   inv <- NULL
   b <- x
+  set <- function(y){
+    b <<- y
+    inv <<- NULL
+  }
   get <- function() b
   setInverse <- function(inverse) inv <<- inverse
   getInverse <- function() inv
-  cacheSolve <- function(a){
-    ## Return a matrix that is the inverse of 'x'
-    linv <- a$getInverse()
-    if(!is.null(linv) && a$get()==b){
-      message("getting cached inverse")
-      return(linv)
-    }
-    linv<-solve(a$get())
-    a$setInverse(linv)
-    linv
-  }
-  list(cacheSolve=cacheSolve, get=get, setInverse=setInverse, getInverse=getInverse)
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
+## this function computes the inverse of the matrix
+
+cacheSolve <- function(x){
+  inv <- x$getInverse()
+  if(!is.null(inv) && x$get()==b){
+    message("getting cached inverse")
+    return(inv)
+  }
+  mata <- x$get()
+  inv <- solve(mata)
+  x$setInverse(inv)
+  inv
+}
