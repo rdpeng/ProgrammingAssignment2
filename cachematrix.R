f6baaba1739e0ee42474e0aa398d7257ad26e2ac
s.russo <russo.silviapaola@gmail.com>
2017-04-13 14:43:46 -0700

    3rd version of Programming assignment
    Final tweaks being made to verify completeness of code.

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,7 @@
 ##  makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-	s <<- NULL
+	s <- NULL
 	set <- function(y) {
 		x <<- y
 		s <<- NULL
