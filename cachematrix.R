4f52cc9929797a7c34e2633c3754ecb985ca68d1
MissZaza <coll.marion@gmail.com>
2017-04-20 00:58:17 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -14,7 +14,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## If the inverse is not stored in cache cacheSolve calculates it using a gaussian elimination and return the inverse
+## If the inverse is not stored in cache cacheSolve calculates and returns it
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 	y <- x$getinverse()
@@ -23,30 +23,7 @@ cacheSolve <- function(x, ...) {
 		return(y)
 	}
 	m <-x$get()
-	r <- 0
-	id <- diag(ncol(m))
-	for (j in 1:ncol(m)){
-		k <- which(m[,j] == max(abs(m[,j])))
-		if (m[k,j]!= 0){
-			r <- r+1
-			id[k,] <- id[k,]/m[k,j]
-			m[k,] <- m[k,]/m[k,j]
-			if (r==k) {
-				tampon <- m[r,]
-				tampon2 <- id[r,]
-				m[r,] <- m[k,]
-				id[r,] <- id[k,]
-				m[k,] <- tampon
-				id[k,] <- tampon2
-			}
-			for (i in 1:nrow(m)){
-				if (i != r){
-					id[i,] = id[i,] - m[i,j] * id[r,]
-					m[i,] = m[i,] - m[i,j] * m[r,]
-				}
-			}
-		}
-	}
-	x$setinverse(id)
-	id
+	inv <- solve(m)
+	x$setinverse(inv)
+	inv
 }
