787d4d3b2634d0f7247cefe83e7f87c8bcb4a27e
Sang-il Im <imsangil@gmail.com>
2017-04-23 19:30:51 +0900

    Programming Assignment 2: Lexical Scoping
    It's a code to calculate inverse matrix from input matrix. If the
    inverse matrix is already calculated, that function just get calculated
    matrix from cached data(or object).

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix
+## function
+## 1) set : set org matrix to variable x
+## 2) get : get org matrix from variable x
+## 3) setinvm : set inverse matrix from variable invm(invm was calculated by cacheSolve)
+## 4) getinvm : get inverse matrix from variable invm(invm was calculated by cacheSolve)
 
+makeCacheMatrix <- function(x = matrix()) {
+  invm <- NULL
+  set <- function(x) x <<- m
+  get <- function() x
+  setinvm <- function(invx) invm <<- invx
+  getinvm <- function() invm
+  
+  list(set=set,
+       get=get,
+       setinvm=setinvm,
+       getinvm=getinvm)
 }
 
 
 ## Write a short comment describing this function
-
+## cachesolve
+## if matrix x is null, make inverse matrix and save it to cache in object x
+## if not, get calculated inverse matrix from object x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invm <- x$getinvm()
+  if(!is.null(invm)){
+    message("get inverse matrix from cached data :)")
+    return(invm)
+  }
+  
+  mat <- x$get()
+  invm <- solve(mat)
+  x$setinvm(invm)
+  ## Return a matrix that is the inverse of 'x'
+  invm
 }
