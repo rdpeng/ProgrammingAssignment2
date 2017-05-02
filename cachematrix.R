04c54083b059b13f75b98aa7532e539b8a2603e2
BeanData <xingxhendou1992@gmail.com>
2017-05-02 18:49:51 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,4 @@
+## R programming Assignment 2 -- Lexical Scoping
 ## Matrix inversion is usually a costly computation and there may be 
 ## some benefit to caching the inverse of a matrix rather than compute it repeatedly.
 
