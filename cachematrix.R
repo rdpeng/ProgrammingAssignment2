e4d8e7bd9d6be86df092bf8b960d5c247d91e335
CHANG KEUN KANG <ckkang7@naver.com>
2017-06-03 11:21:41 +0900

    commit on 1121
    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## cache data for reusing it.
 ## Write a short comment describing this function
+# makeCacheMatrix: return a list of function:
+# set the matrix, get the matrix, set the inverse of matrix, get the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        set_matrix <- function(matrix) m <<- matrix
+        get_matrix <- function() m
+        list(set = set, get = get,
+                set_matrix = set_matrix, get_matrix = get_matrix)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$get_matrix()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        inverseMatrix <- solve(data)
+        x$set_matrix(inverseMatrix)
+        ruturn(inverseMatrix)
 }
