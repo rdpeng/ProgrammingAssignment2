d9170bca025c49c52965f79221dbc7c81440f922
GodessGomez <shingl1@yahoo.com>
2017-06-06 07:04:17 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,4 @@
-## Put comments here that give an overall description of what your
-##The first function, makeVecor will create a vector containing a function to
- 
-## functions do
-makeVector <- function(x = numeric()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
-}
-## Put comments here that give an overall description of what your
-cachemean <- function(x, ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
-}
+
 ## Write a short comment describing this function
 ## This function creates a matrix object that can cache its inverse
 
@@ -48,18 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" created by the makeCacheMatrix above.
+  cacheSolve <- function(x, ...) {
+## Return a matrix that is the inverse of 'x
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   	  inv <- x$getInverse()
         if (!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
-	  }
-
-	mat <- x$get()
-        inv <- solve(mat, ...)
-        x$setInverse(inv)
-        inv
-
 }
