8e1f11b8eecb4337458167e72ef2faf9e960d771
nightrose <nightrose79@users.noreply.github.com>
2017-06-13 00:03:49 -0400

    update the code

--- Kosaraju_fast3.py
+++ Kosaraju_fast3.py
@@ -15,48 +15,20 @@ path='/Users/yuy/Documents/learning/algorithms/week4/'
 
 #import pandas as pd
 import numpy as np
+from scipy.stats import itemfreq
+
 os.chdir(path)
 
 #target = open(path+'log first leg.txt', 'w')    
 
-start_time = time.time()
-lst=[]
-with open('sample1.txt','r') as f:
-    for l in f.readlines():
-        #print(l)
-        line=l.split('\r')
-        for k in line:
-            lst.append(k.split('\t'))
-        
-#with open('SCC.txt') as f:
-#    for l in f.readlines():
-#        lst.append(l.split())
-        
-may = np.asarray(lst)
-may = may.astype(np.int)
 
-print("--- %s seconds ---" % (time.time() - start_time))     
-print 'read in data'
-#reverse the graph     
-#df = pd.DataFrame(lst, columns=['start','end'])
-may_rev=may[:,np.argsort([1,0])]
-
-set1=set(may[:,0])
-vertex_set=set1.union(set(may[:,1]))#this list is used to contain all vertex
-#vsl=list(vertex_set)
-#the vertex_set contains all vertexes in the graph
-n=len(vertex_set)
-#record columns are: 0: vertex, 1:'visited', 2:'leader',3:'finish_time',4:'index from',5:'index to'
-record = np.column_stack((np.array(list(vertex_set)),np.zeros([n,5],dtype=int)))
-
-#sort the reversed adjacency list 
-may_rev=may_rev[may_rev[:,0].argsort()]
 
 #this function figures out how many outward edges there are from a vertex
 def set_record(array0,record):
     #set the visited and leader records to 0
     record[:,1]=0
     record[:,2]=0
+    record[:,3]=0
     record[:,4]=-1
     record[:,5]=-1
     runner=0
@@ -75,8 +47,7 @@ def set_record(array0,record):
             record[runner,5]=i
     return record
 
-record1=set_record(may_rev,record)
-print 'initialized records '
+
 
 def dfs_loop(array0,record,order):
     global t,s
@@ -93,32 +64,27 @@ def DFS(array0,record,i):
     global t,s,new
     S=[]
     pred=[]
-    skp=[]
+#    skp=[]
     S.append(i)
     s=i
     #s=i
     while len(S)>0:
         u=S.pop(0)
         pred.append(u)
-        #p=pred.pop()
-        print 'pop',u,'from S'
+        print 'pop',u,' from S and add ',u,'to the traveled path'
         if record[u-1,1]==0:
             print u,'has not been visited yet, set it as visited'
             record[u-1,1]=1
-            
-   # print 'depth first search starting with',i
+            #find all the edges going out of the current vertex
             start=record[u-1,4]
             end=record[u-1,5]
        #     print u,' outgoing vertex starts from',start, 'and end at ',end
-        #record.ix[i,'visited']=True
             if start==-1:
                 t=t+1 
                 #target.write('the edge has no outgoing vertex, set finish time for '+str(u)+' as '+str(t)+'\n')
-
-                print 'the edge has no outgoing vertex, set finish time for ',u,'as',t
+                print 'the edge has no outgoing vertex, set finish time for ',u,'as',t,'leader for ',u,'as',u
                 record[u-1,3]=t 
                 record[u-1,2]=u
-                print 'set leader for ',u,'as',u
             else:
                 #record.ix[u,'leader']=s
                 print 'search through all its outgoing vertex'
@@ -126,66 +92,68 @@ def DFS(array0,record,i):
                   #  print 'j is',j-1
                     nn=array0[j,1]
                     print 'neighbor is ',nn
-                    if record[nn-1,1]==0 and nn not in S:
-                        print 'put neighbour',nn,'into stack and its predecesor',u,'into another stack'
+                    if record[nn-1,1]==0 and nn not in S:#if nn hasn't been explored, go to it next
+                        print 'put neighbour',nn,'into stack'
                         S.append(nn)
-                        #pred.append(u)
                         print 'S is',S,'pred is ',pred
                     else:
                         print 'neighbor',nn,'has been visited'
-                        skp.append(nn)
-                while len(skp)>0:
-                        nn=skp.pop(0)   
-                        brk=0
-                        #s=u
-                        while len(pred)>0 and brk==0:
-                            new=pred.pop(0)  
-                            ss=record[new-1,4]
-                            ee=record[new-1,5]
-                            if ss!=-1 and ee!=-1:
-                                for check in range(ss,ee+1):
-                                    print 'check whether ',new,'has remaining edges to visit'
-                                    cn=array0[check,1]
-                                    
-                                    if cn in S:
-                                        print new,'has remaining edge in S to visit, put it back in the qeue'
-                                        pred.append(new)
-                                        brk=1
-                                        
-                                        break
-                                    #else:
-                                     #   print 'no, ',new,'is done, we set finish time as',t,' and leader as',s
-                                        #record[new-1,3]=t
-                                        #record[new-1,2]=s
-                                        #t=t+1
-                if record[new-1,3]==0:
-                    t=t+1
-                                #target.write('set finish time for '+str(new)+' as '+str(t)+'\n')
-                    print 'set finish time for ',new,'as',t
-                    record[new-1,3]=t 
-                    record[new-1,2]=s
-                    print 'set leader for ',new,'as',s
-    #return record
- 
-       
-       
-dfs_loop(may_rev,record1,sorted(list(vertex_set),reverse=True))
-#target.close()
-print 'finish the first loop'
+    #after all queue items are visited, we started going back 
+    while len(pred)>0:
+        t=t+1
+        new=pred.pop()
+        print 'set end time to',new,'as ',t,'leader as',s
+        record[new-1,3]=t
+        record[new-1,2]=s
 
-df_original=df.rename(columns={'start':'end','end':'start'})
-df_original.sort_values(by='end',axis=0,inplace=True)
-df_original.reset_index(inplace=True,drop=True)
-
-record=record.drop(['to','from'],axis=1)
-record=set_record(df_original,record)
-
-target = open(path+'log second leg.txt', 'w')   
-finish_order=record['finish_time']
-new_list=finish_order.sort_values(ascending=False).index.tolist()
-dfs_loop(df_original,record,new_list)
-target.close()
-if record[record.leader.isnull()].shape[0]>0:
-    record.ix[record.leader.isnull(),'leader']=record[record.leader.isnull()].index.values[0]
-final_list=record['leader'].value_counts()
-print final_list[0:4]
+def main():
+    start_time = time.time()
+    lst=[]
+    with open('sample5.txt','r') as f:
+        for l in f.readlines():
+            #print(l)
+            line=l.split('\r')
+            for k in line:
+                lst.append(k.split('\t'))
+            
+    #with open('SCC.txt') as f:
+    #    for l in f.readlines():
+    #        lst.append(l.split())
+            
+    may = np.asarray(lst)
+    may = may.astype(np.int)
+    
+    print("--- %s seconds ---" % (time.time() - start_time))     
+    print 'read in data'
+    #reverse the graph     
+    #df = pd.DataFrame(lst, columns=['start','end'])
+    may_rev=may[:,np.argsort([1,0])]
+    
+    set1=set(may[:,0])
+    vertex_set=set1.union(set(may[:,1]))#this list is used to contain all vertex
+    #vsl=list(vertex_set)
+    #the vertex_set contains all vertexes in the graph
+    n=len(vertex_set)
+    #record columns are: 0: vertex, 1:'visited', 2:'leader',3:'finish_time',4:'index from',5:'index to'
+    record = np.column_stack((np.array(list(vertex_set)),np.zeros([n,5],dtype=int)))
+    
+    #sort the reversed adjacency list 
+    may_rev=may_rev[may_rev[:,0].argsort()]        
+    record1=set_record(may_rev,record)
+    print 'initialized records '
+    
+    dfs_loop(may_rev,record1,sorted(list(vertex_set),reverse=True))
+    #target.close()
+    print 'finish the first loop'
+    finish_order=record1[record1[:,3].argsort()[::-1],0]
+    record2=set_record(may,record)
+    dfs_loop(may,record2,finish_order)
+    
+    fq=itemfreq(record2[:,2])
+    n=5
+    idx=fq[:,1].argsort()[::-1][:n]
+    final_list=fq[idx][:,1]
+    print final_list[0:4]
+    
+if __name__=='__main__':
+    main()
