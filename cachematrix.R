13136b4cde7338f2c691c6f715ce44cdcda598fe
LoriCorcoran <lori.corcoran@cba.com.au>
2017-06-15 13:14:34 +1000

    Create cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-# Lexical Scoping first try
+# Lexical Scoping 2nd try
 
 makeCacheMatrix <- function(x = matrix()) {
   # create a special matrix calculate the inverse of a matrix
-
+m <- NULL
+set <- function(inverse) {
+  x <<- inverse
+  m <<- NULL
+}
+get <- function() x
+setinverse <- function(inverse) m <<- inverse
+getinverse <- function() m
+list(set = set, get = get,
+     setinverse = setinverse,
+     getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  m<-x$getinverse()
   if(!is.null(m)) {return(m)}
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x' if inverse already exist
+  data <- x$get()
+  m <- inverse (data,...)
+  x$setinverse (m)
+  m
+
 }
