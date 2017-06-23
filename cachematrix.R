60673dec730164042f427fba1a9aa2cb393e8776
Krishanu Prabha Sinha <krishanusinha12@gmail.com>
2017-06-22 23:05:25 -0500

    Message to Reviewer
    Respected Reviewer,
    
    I have made edits to this program as per the best of my understanding.
    Please do review it and let me know what you think about it.
    
    Best Regards,
    Krishanu

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## In this function I am retrieving a matrix in a variable 'x' and retrieve its length which gives the number of rows and columns 
+## This is followed by storing the matrix in a cache. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        length(x) <- n
+        setMatrix <- function(y) {
+                A <- matrix(y,nrows=n/2,ncol=n/2, byrow=TRUE)
+                }
+                B <<- A
 }
 
 
-## Write a short comment describing this function
+## In this function we retieve the matrix and inverse it and store it in the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(!is.null(x)){
+            message ("Getting Cached data....") 
+            return(x)    
+                }
+        C <- get("B")
+        D <<- solve(C)
+        D
 }
