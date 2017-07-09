37f188411b03af31748343a26e302f3cae4f0064
Ramya Sigatapu <rsigatapu@gmail.com>
2017-07-09 16:17:39 -0600

    Revert "Adding code to the file."
    This reverts commit b07d5d2c5ba987e0ed180fc34585ffc3f72a2bd9.

--- cachematrix.R
+++ cachematrix.R
@@ -1,40 +1,15 @@
-##makeCacheMatrix creates a list containing a function to
+## Put comments here that give an overall description of what your
+## functions do
 
-#1.set the value of the matrix
-#2.get the value of the matrix
-#3.set the value of the inverse of the matrix
-#4.get the value of the inverse of the matrix
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-	inv_m <- NULL
-    set <- function(y) {
-        x <<- y
-        inv_m <<- NULL
-    }
-    get <- function() x
-    setinv <- function(inverse) inv_m <<- inverse
-    getinv <- function() inv_m
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
 
 }
 
 
-##The following function first checks if the inverse has already been
-##computed. If it has been, it gets the result and skips performing
-##the computation. If not, it computes the inverse and sets the value
-##in the cache using setinverse function.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        inv_m <- x$getinv()
-    if(!is.null(inv_m)) {
-        message("getting cached data.")
-        return(inv_m)
-    }
-    data <- x$get()
-    inv_m <- solve(data)
-    x$setinv(inv_m)
-    inv_m
 }
