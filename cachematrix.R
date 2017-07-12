87fa82249ab03111cc020eb48035afb1b7e2514c
isaaczhao23 <isaaczhao23@yahoo.com>
2017-07-12 16:42:19 -0400

    Program Assignment 2 - Inverse Matrix
    For Coursera R Programming Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix by first reading an input matrix x and caches it. Sets default to NULL 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invert <- NULL
+    set <- function(y) {
+        x <<- y
+        invert <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) invert <<- inverse
+    getInverse <- function() invert
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+## Return a matrix that is the inverse of input matrix x by first checking if matrix already calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invert <- x$getInverse()
+## If inverse matrix has previously been computed and the matrix has not changed, then the inverse from the cache is retrieved instead.
+    if (!is.null(invert)) {
+        message("retrieving cache")
+        return(invert)
+    }
+## If inverse matrix wasn't previously computed, calculates and sets inverse matrix, and outputs inverse matrix
+    data <- x$get()
+    invert <- solve(data, ...)
+    x$setInverse(invert)
+    invert
 }
