14d535a3d1a8509d821b91d41e7604275b6f382c
Vikas Maggo <Vikas Maggo>
2017-08-01 22:55:05 +0800

    added new function

--- cachematrix.R
+++ cachematrix.R
@@ -10,6 +10,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+makeCacheMatrix <- function(x = matrix()) {
+      m  <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+          }
+      get <- function() x
+      setinverse <- function(inverse) m <<- inverse
+      getinverse <- function() m
+      list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+    }
+
+sum <- function(a,b) { 
+    return(a+b)
 }
+
+mul <- function(a,b){
+    return(a*b)
+}
+# end of the program , output coming as expected 
