a734c3bad1abdf1938e8a2a17de9d4072a9229cd
javier-gago <30737720+javier-gago@users.noreply.github.com>
2017-09-17 23:08:11 +0200

    Update CacheMatrix.R

--- CacheMatrix.R
+++ CacheMatrix.R
@@ -1,17 +1,26 @@
+## The function makeCacheMatrix creates a matrix for compute the value of the inverse
+
 makeCacheMatrix <- function(x = matrix()) {
         inmtx <- NULL
+        ## Set the value of the matrix
         set <- function(y) {
                 x <<- y
                 inmtx <<- NULL
         }
+        ## Get the value of the matrix
         get <- function() x
+        ## Set the value of the matrix
         setinmtx <- function(inverse) inmtx <<- inverse
+        ## Get the value of the matrix
         getinmtx <- function() inmtx
         list(set = set, get = get,
              setinmtx = setinmtx,
              getinmtx = getinmtx)
 }
 
+
+
+##The second function check the data, if the data already exists, it wont compute it, else it will compute it
 cacheinverse <- function(x, ...) {
         inmtx <- x$getinmtx()
         if(!is.null(inmtx)) {
