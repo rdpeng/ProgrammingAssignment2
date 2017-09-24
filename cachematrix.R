745daec66aa5f51040e6a8e3ec4c660e112bfb77
Shuai Dong <shuaidong.joyce@gmail.com>
2017-09-23 22:54:23 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,33 +3,37 @@
 
 ## Write a short comment describing this function
 
+
+#makeCacheMatrix is a function that returns a list containing a function to -
+#set value of matrix
+#get value of matrix
+#set value of inverse
+#get value of inverse
+
 makeCacheMatrix <- function(x = matrix()) {
-        v<-NULL
-        set<-function(y){
-                x<<-y
+        v<-NULL #setting the inverse of the matrix NULL to be a place holder
+        set<-function(y){        #define a function to set the matrix y to a new matrix y and reset the inverse of the matrix to NULL
+                x<<-y   
                 v<<-NULL
         }
-        get<-function() x
-        setinverse<-function(inverse) v<<-inverse
-        getinverse<-function() v
-        list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+        get<-function() x       #return the matrix x
+        setinverse<-function(inverse) v<<-inverse #set the inverse of matrix v to inverse
+        getinverse<-function() v #returns the inverse v
+        list(set=set,get=get,setinverse=setinverse,getinverse=getinverse) #return the special vector containg all functions just defined above
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        v<-x$getinverse()
-        if(!is.null(v)) {
+        v<-x$getinverse()       #first check if v has a value or not
+        if(!is.null(v)) {       #if v exist print out message "getting cached data" and return predetermined value v
                 message("getting cached data")
                 return(v)
         }
-        data<-x$get()
-        v<-solve(data,...)
-        x$setinverse(v)
-        v
-        
-        
-        ## Return a matrix that is the inverse of 'x'
+        data<-x$get()           #if v does exist, run function determined above to get data
+        v<-solve(data,...)      #using the data retrieved to calculate the inverse of matrix and store it in v
+        x$setinverse(v)         #set the inverse of matrix calculated above to v
+        v                       ## Return a matrix that is the inverse of 'x'
         }
         
