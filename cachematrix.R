2e2700e64a7ab3a92d769b50be37a26da6856168
kapark2 <32823313+kapark2@users.noreply.github.com>
2017-10-16 21:44:23 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## what I want to do here is to present a function which enables me to get the inverse of x, automatically and taking advantage of cache which makes the calculation faster.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m<-NULL
+set<-function(y)
+  x<<-y
+m<<-NULL
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  get<-function()x   # I get the function
+  setsolve<-function(solve) m <<- solve  # I set the command of the function, here solve, to receive the inverse
+  getsolve<-function()m # it solves the function
+  
+  list(set=set, get=get,
+       setsolve=setsolve,
+       getsolve=getsolve) # it returns our demanded Output in a list
