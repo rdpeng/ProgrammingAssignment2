1213784318bf0e040dd868b07f1dc281bc3f5cb8
Jay <jbenbow141@gmail.com>
2017-11-02 01:22:08 +1100

    step 3 complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,14 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 
 ## The following functions cache and compute the inverse of a matrix
 
 ## "makeCacheMatrix" function creates a special "matrix" object to cache its inverse
 
 makeCacheMatrix <- function(m = matrix()) {
-        
-        inverse <- NULL
+inverse <- NULL
         s <- function(x) {
                 m <<- x;
                 inverse <<- NULL;
@@ -19,13 +22,17 @@ makeCacheMatrix <- function(m = matrix()) {
         return(list(s = s, g = g, sinv = sinv, ginv = ginv))
 }
 
+
+## Write a short comment describing this function
+
 ## "cacheSolve" function executes the inverse of the special
 ## "matrix" thats returned by `makeCacheMatrix` function. If the inverse has
 ## already been calculated and the matrix has not changed, then
 ## `cacheSolve` function should get the inverse from the cache.
 
 cacheSolve <- function(m, ...) {
-        
+        ## Return a matrix that is the inverse of 'x'
+
         inverse <- m$ginv()
         if(!is.null(inverse)) {
                 
@@ -41,4 +48,3 @@ cacheSolve <- function(m, ...) {
         
         return(inverse)
 }
-
