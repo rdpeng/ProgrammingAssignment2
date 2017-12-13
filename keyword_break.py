28f16f43942a4a9153676c0828cb2a4241da96c3
clg68 <32576169+clg68@users.noreply.github.com>
2017-12-13 17:52:56 -0500

    Add files via upload

---
+++ keyword_break.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  4 15:12:57 2017
+
+@author: chrisgaughan
+
+while True:
+    line= input('>')
+    if line == 'done':
+        break
+    print(line)
+print('Done!')
+
+    
+    
---
+++ pay_rate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 13:44:43 2017
+
+@author: chrisgaughan
+"""
+
+xh = input("Enter Hours:")
+xr = input("Enter Rate:")
+xp = (float(xh) * float(xr))
+print("Pay:",xp)
