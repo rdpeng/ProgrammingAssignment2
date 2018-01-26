2f76e982a104231c6863e4a6a3b47ecf78700863
Husam <husamalani@yahoo.com>
2018-01-27 01:20:12 +0300

    I done my Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## below are two functions, one calculate the inverse of the Matrix and cached it, and
+## the other solve matrix for its inverse, if it's cached it will call it directly, if
+## not cached will calculate it.
 
-## Write a short comment describing this function
+## makeCacheMatrix will take Matrix as an argument and calculate the inverse and cached it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+   inverseofx <<- solve(x) 
+   return(x)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will either call the inverse of the Matrix from the cache or calculate and
+## cached it.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(y, ...) {
         ## Return a matrix that is the inverse of 'x'
+  I2 <- matrix(c(1,0,0,1),nrow = 2,ncol = 2)
+  if (all.equal(y %*% inverseofx,I2) == TRUE) {
+    message("getting cached data")
+    return(inverseofx)
+  }
+  inverseofx <<- solve(y)
+  inverseofx
 }
+
