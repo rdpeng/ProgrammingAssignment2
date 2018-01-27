0199fbc5a6d79654c9234430a1400c101c1128d2
Husam <husamalani@yahoo.com>
2018-01-27 21:05:31 +0300

    I make the two required functions here

--- cachematrix.R
+++ cachematrix.R
@@ -2,26 +2,35 @@
 ## the other solve matrix for its inverse, if it's cached it will call it directly, if
 ## not cached will calculate it.
 
-## makeCacheMatrix will take Matrix as an argument and calculate the inverse and cached it
+## makeCacheMatrix will take Matrix as an argument and cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  
-   inverseofx <<- solve(x) 
-   return(x)
+  inverseofx <-NULL
+   setmatrix <-function(y){
+     x <<- y
+     inverseofx <<- NULL
+   }
+   getmatrix <- function() x
+   setinverse <- function(inverse) inverseofx <<- inverse
+   getinverse <- function() inverseofx
+   list (setmatrix=setmatrix, getmatrix=getmatrix,setinverse=setinverse,getinverse=getinverse)
+   
 }
 
 
 ## cacheSolve will either call the inverse of the Matrix from the cache or calculate and
 ## cached it.
 
-cacheSolve <- function(y, ...) {
+cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  I2 <- matrix(c(1,0,0,1),nrow = 2,ncol = 2)
-  if (all.equal(y %*% inverseofx,I2) == TRUE) {
+   inverseofx <- x$getinverse()
+  if (!is.null(inverseofx)) {
     message("getting cached data")
     return(inverseofx)
   }
-  inverseofx <<- solve(y)
+  matrix <- x$getmatrix()
+  inverseofx <- solve(matrix)
+  x$setinverse(inverseofx)
   inverseofx
 }
 
