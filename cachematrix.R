246b87136a9645a30640a0d9ecccceb2f01e8e9f
SUDEEPA PATHAK <35413711+sudeepapathak@users.noreply.github.com>
2018-02-03 08:54:28 -0500

    Add files via upload
    makeCacheMatrix() special vector for calculating and saving inverses

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching The Inverse of a Matrix
+## makeCacheMatrix will create four functions to do the following 
+## set value of matix
+##get value of matrix
+## set inverse of a matrix
+## get inverse of a matrix
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+Inv<-NULL ##NULL value is a position holder for Inv for future reference
+set <- function(y) {
+  x <<- y  ##y is substituted in place of x in parent env
+  Inv <<- NULL ## clears all previous values of Inv
+}
+get <- function() x  ##lexical scoping picks up x from parent envo
+setinverse <- function(inverse) Inv <<- inverse
+getinverse <- function() Inv ## Inv is picked up from parent envo after it acquies new value
+list(set = set,get = get,setinverse = setinverse,getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## CacheSolve calculates inverse,it retrieves the inverse from an object of type makeCacheMAtrix()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  Inv <- x$getinverse()## retrieve inverse from object passed as argument
+  if (!is.null(Inv)) { ##check if Inv is NULL
+    message("getting cached data") ## if not NULL we have a valid Inverse and can return it to parent env
+    return(Inv)
+  }
+  data <- x$get() 
+  Inv <- solve(data, ...)
+  x$setinverse(Inv)
+  Inv
+  ## CacheSolve() gets matrix from input object it calculates its inverse, 
+  ##uses setinverse() on input object to set inverese 
+  ## Then returns inverse Inv to parent env
 }
+
