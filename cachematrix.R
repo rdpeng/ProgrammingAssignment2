c7ba6130c9f0600f7b30a1758ad87663baab819d
RohanKhollamkar <37409247+RohanKhollamkar@users.noreply.github.com>
2018-03-16 11:28:51 +0530

    first assignment on R programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## makeCacheMatrix function will take a matrix as an input and store the matrix as well as its inverse if taken.
+## cacheSolve function will find if the inverse of matrix is in the function makeCacheMatrix, if it is now present
+#  then it will take original matrix from that previous function and set an inverse of it using solve function and
+# returns the inverse matrix.
+
+
+
+
+
+
+
+
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix function will be taking a matrix as an input, set and get the values of matrix
+#  accordingly. After which set and get inevrse of matrix accordingly
 
+## Assign operator '<<-' will assign a value to an object that is in different environmnet.
+
+makeCacheMatrix <- function(x = matrix()) {
+  MatInverse <- NULL
+  
+  setMat <- function(y){
+    x <<- y
+    MatInverse <<- NULL
+    
+  }
+  getMat <- function() 
+    x
+  setInv <- function(inverse)
+    MatInverse <<-inverse
+  getInv <- function()
+    MatInverse
+  list(setMat = setMat,getMat = getMat,setInv = setInv,getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
+## The function 'cacheSolve' returns an inverse of matrix in function 'makeCacheMatrix'
+#  if the MatInverse value in that function is empty, for which it will take original matrix 
+#  from "makeCacheMatrix' and invert the matrix using solve function.
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  MatInverse <- x$getInv()
+  if(!is.null(MatInverse)){
+    return (MatInverse)
+  }
+  orgMat <-x$getMat()
+  MatInverse <- solve(orgMat,...)
+  x$setInv(MatInverse)
+  return(MatInverse)
+        
 }
