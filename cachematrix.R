71094572d02f628b95f44cfec7daae3069972c85
Had-Matter <36919172+Had-Matter@users.noreply.github.com>
2018-03-25 12:27:26 +0200

    solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Function saves a matrix and store it twice, as matrix and invers of himself.
+# We reach the maingoal with a check after input of an new matrix, 
+#if old and new are similar and we don´t need to computed it again.
 
-## Write a short comment describing this function
+
+#Kind of Frontend: We name our function and assign working commands
+#`gatevar` is the variable to communicate with our second function, our `backend` 
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        
+        set <- function(y) 
+        {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() 
+        {
+                x
+        }
+        
+        setInverse <- function(gatevar)
+        {
+                inv <<- gatevar
+        }
+        
+        getInverse <- function() 
+        {
+        inv
+        }
+        
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+# Our ´backend´. We check, if there are already a similar matrix.
+# If it´s not, we creates the invers of our given matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        inv <- x$getInverse()
+        
+        if (!is.null(inv)) 
+        {
+                message("getting cached data")
+                return(inv)
+        }
+        
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
 }
