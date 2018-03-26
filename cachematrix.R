48c0b10933eb2b197df1471da2bb9e8aabe25942
sonchita <sonchi12@gmail.com>
2018-03-27 02:01:05 +0600

    week 3 assignment: makeCacheMatrix and cacheSolve:

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+> A<- matrix(c(1,2,3,4),2,2) 
+>A
+[,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> A1<- makeCacheMatrix(B)
+##This function creates a special "matrix" object that can cache its inverse
+> cacheSolve(A1)
+##inverse returned after computation
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(A1)
+##inverse returned from cache
+getting cached data
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
