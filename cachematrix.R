11f2dead437f9000f874f46d99d4b8449412e68c
sonchita <sonchi12@gmail.com>
2018-03-27 15:33:50 +0600

    Catchmatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,27 @@
-> A<- matrix(c(1,2,3,4),2,2) 
->A
-[,1] [,2]
-[1,]    1    3
-[2,]    2    4
-> A1<- makeCacheMatrix(B)
-##This function creates a special "matrix" object that can cache its inverse
-> cacheSolve(A1)
-##inverse returned after computation
-[,1] [,2]
-[1,]   -2  1.5
-[2,]    1 -0.5
-> cacheSolve(A1)
-##inverse returned from cache
-getting cached data
-[,1] [,2]
-[1,]   -2  1.5
-[2,]    1 -0.5
+makeCacheMatrix <- function(x = matrix()) {
+  ## makes a cachematrix
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
+
+cacheSolve <- function(x=matrix(), ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
+}
