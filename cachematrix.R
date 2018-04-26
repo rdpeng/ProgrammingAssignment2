6612ff79c84b6d638b472899c3b57e6bf4e42391
kimerios <nicclaudian@gmail.com>
2018-04-26 19:45:43 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,25 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##Nik's attempt to define a CacheMatrix in R
 makeCacheMatrix <- function(x = matrix()) {
-
+  dummymean <- NULL
+  setFunction <- function(y=matrix()) {
+    placeholder_x <<- y
+    dummymean <<- NULL
+## x and y variables are set to be matrices
 }
-
-
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(setFunction = setFunction, get = get,
+       setinv = setinv,
+       getinv = getinv)
+## solve function chosen on purpose
+## so it can take matrices in its function arguments
+}
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 }
