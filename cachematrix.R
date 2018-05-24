0e1dd8b88fe8e7385ee8d7c2f004b834b0fdacbe
priyank1574q <34156281+priyank1574q@users.noreply.github.com>
2018-05-24 23:45:23 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-	m <- NULL
-	set <- function(y) {
-		x <<- y
-            m <<- NULL
+	m <- NULL				##initiazing m as NULL, it will contain the inverse of the matrix
+	set <- function(y) {			##set function is defined here
+		x <<- y				##setting value of matrix in parent environment
+            m <<- NULL				##setting m to NULL
       }
-      get <- function() x
-      setinv <- function(inv) m <<- inv
-      getinv <- function() m
+      get <- function() x			##get function is defined here
+      setinv <- function(inv) m <<- inv		##value of m is assigned in parent environment
+      getinv <- function() m			##gets the value of m in the environment called
       list(set = set, get = get,
            setinv = setinv,
-           getinv = getinv)
+           getinv = getinv)			##to allow $ operations we need list
 }
 
 ## Write a short comment describing this function
