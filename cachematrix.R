5d97e2f9ced313c5bf26afc5882bbe9941315d16
Audrey <alow@email.wm.edu>
2018-07-01 23:33:16 -0400

    Create methods

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+  {
+  inverse <- NULL
+  assign <- function(s) {
+    x <<- s
+    inverse <<- NULL
+  }
+  get <- function() {
+    print(x)
+  }
+  
+  assign.inverse <- function(input){
+    inverse <<- input
+  }
+  get.inverse <- function(){
+    print(inverse)
+  }
+  list(assign = assign, get = get,
+       assign.inverse = assign.inverse,
+       get.inverse = get.inverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$get.inverse()
+  if(is.null(inverse)){
+    matrix <- x$get()
+    inverse <- solve(matrix)
+    x$assign.inverse(inverse)
+  }else{
+    return(inverse())
+  }
+  print(inverse)
 }
+
+#test
+
+
+q<-matrix(c(2,0,0,1),2,2)
+qz<-makeCacheMatrix(q)
+cacheSolve(qz)
