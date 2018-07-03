828266fffa33b98e9af97c2db30cb5c07519c7d5
Sotrik <gourav.apprendiz@gmail.com>
2018-07-03 10:45:11 +0530

    Wrote a code to get the value of invertible matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,30 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+    makeCacheMatrix <- function(x = matrix()) {
+    invMatrix <- NULL 
+    setMatrix <- function(y) { 
+    x <<- y 
+    invMatrix <<- NULL 
+} 
+    getMatrix <- function() x                              #get the value of the Matrix 
+    setInverse <- function(inverse) invMatrix <<- inverse  #set the value of the invertible matrix 
+    getInverse <- function() invMatrix                     #get the value of the invertible matrix 
+    list(setMatrix = setMatrix, getMatrix = getMatrix, 
+    setInverse = setInverse, getInverse = getInverse) 
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    cacheSolve <- function(x, ...) {
+    invMatrix <- x$getInverse() 
+    if(!is.null(invMatrix)) {                       #if inverse matrix is not NULL 
+    return(invMatrix)                             #return the invertible matrix 
+} 
+    MatrixData <- x$getMatrix()                     #get the original Matrix Data  
+    invMatrix <- solve(MatrixData, ...)             #use solve function to inverse the matrix 
+    x$setInverse(invMatrix)                         #set the invertible matrix  
+    return(invMatrix)                               #return the invertible matrix 
+    ## Return a matrix that is the inverse of 'x' 
 }
