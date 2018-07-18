33a1c4b6fde2451342ce39b5afd592df8a5cd21f
BusIAnalyst <41396886+BusIAnalyst@users.noreply.github.com>
2018-07-18 17:39:39 -0500

    cachematrix.R
    Diego Saenz 18/07/2018

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,51 @@
 
 ## Write a short comment describing this function
 
+## makeCacheMatrix is a function that will create a special "matrix" (i.e. an inversed matrix)
+
 makeCacheMatrix <- function(x = matrix()) {
+        ##Create the inverse matrix
+        cm <- NULL
+        
+        ##Setting the matrix
+        set <- function(y){
+                x <<- y
+                cm <<- NULL
+        }
+        
+        ##Getting the matrix
+        get <- function() x
+        
+        ##Solving (i.e. finding the inverse) of the matrix
+        setinv <- function(inv) cm <<- inv
+        getinv <- function() cm
+        
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
 ## Write a short comment describing this function
 
+##Calculating the inverse of the matrix if not in cache
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        cm <- x$getinv()
+        
+        ##If in cache, returns inverse
+        if(!is.null(cm)) {
+                message("getting cached data")
+                return(cm)
+        }
+        
+        ##If not in cache, computes inverse
+        
+        data <- x$get()
+        cm <- solve(data, ...)
+        x$setinv(cm)
+        cm
+        
 }
