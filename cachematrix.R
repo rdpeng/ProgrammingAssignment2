75c61715470fe6a0c920b7684bba0db08036054c
MaorGPA <shoshan.maor@gmail.com>
2018-07-28 20:34:00 +0300

    Hey dude

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+# I like food
+
+# ALOT
+
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+    x ^ 2
         ## Return a matrix that is the inverse of 'x'
 }
