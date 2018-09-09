ba4b0c0af9fc75cbe4bf587436f54f18547056bb
tcui2 <43115690+tcui2@users.noreply.github.com>
2018-09-09 14:07:21 -0500

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,24 @@
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
+}	
+
+cacheSolve <- function(x, ...) {	cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'	        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
+}	}
