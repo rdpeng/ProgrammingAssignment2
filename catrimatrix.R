28c5bd3147bf8a287a73ad12764c73cde77e0123
tanyiwen001 <43410932+tanyiwen001@users.noreply.github.com>
2018-09-19 21:45:11 +0800

    Add files via upload

---
+++ catrimatrix.R
@@ -0,0 +1,33 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
+inv <- NULL
+set <- function(y) {
+  x <<- y
+  inv <<- NULL
+}
+get <- function() x
+setinv <- function(inverse) inv <<- inverse
+getinv <- function() inv
+list(set = set, get = get, setinv = setinv, getinv = getinv)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+ if(!is.null(inv)) {
+   message("getting cached result")
+   return(inv)
+ }
+ data <- x$get()
+ inv <- solve(data, ...)
+ x$setinv(inv)
+ inv
+}
