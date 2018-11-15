692cc5e08e59225406444196abad8259130a9580
Carlos Augusto Mar <carlos.augusto.mar@gmail.com>
2018-11-15 15:16:58 -0400

    Answer for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function below apply scoping rules to compute and store a inverse matrix using cache
+## 
 
-## Write a short comment describing this function
+##  makeCacheMatrix creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		mi <- NULL
+        set <- function(y) {
+                x <<- y
+                mi <<- NULL
+        }
+        get <- function() x
+        setim <- function(im) mi <<- im
+        getim <- function() mi
+        list(set = set, get = get,
+             setim = setim,
+             getim = getim)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		mi <- x$getim()
+        if(!is.null(mi)) {
+                message("getting cached data")
+                return(mi)
+        }
+        data <- x$get()
+        mi <- solve(data, ...)
+        x$setim(mi)
+        mi
 }
