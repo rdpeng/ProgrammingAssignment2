7331c09a56bc56a218be561b0bdd4ef6e5e85efe
hyung405 <45776034+hyung405@users.noreply.github.com>
2018-12-11 10:59:41 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
         get<-function()x
         setinverse<-function(inverse)inv<<-inverse
         getinverse<-function()inv
-        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 ## Write a short comment describing this function
