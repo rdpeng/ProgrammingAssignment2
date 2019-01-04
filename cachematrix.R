2ef6ee0f3e23b2287a5f355a2f9b067ee059146a
suraj-l <45360093+suraj-l@users.noreply.github.com>
2019-01-04 23:01:09 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,29 @@
-makeCacheMatrix <- function(x = numeric()) {
-    cache <- NULL
-    
+makeCacheMatrix <- function(x = numeric()) {     # makeCacheMatrix function takes any sqaure matrix (invertible matrix) as an argument and calculates inverse of square matrix using solve function
+    cache <- NULL                                # Initialized cache with NULL    
     setMatrix <- function(newValue) {
-        x <<- newValue
+        x <<- newValue                           # Assign value of matrix i.e. 'x'
         cache <<- NULL
-    }
-    
-    getMatrix <- function() {
+    }   
+    getMatrix <- function() {                    # get value of matrix i.e. 'x'
         x
+    } 
+    cacheInverse <- function(solve) {            # cacheInverse function calcualtes inverse of matrix 'x' and assign the result to cache
+    cache <<- solve                              
     }
     
-    cacheInverse <- function(solve) {
-        cache <<- solve
-    }
-    
-    getInverse <- function() {
+    getInverse <- function() {                  # getInverse function is used to get inverse of matrix 'x'
         cache
     }
-    list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+    list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)    #create a list will all the functions
 }
-cacheSolve <- function(y, ...) {
-    inverse <- y$getInverse()
-    if(!is.null(inverse)) {
+cacheSolve <- function(y, ...) {                # cacheSolve function takes matrix as an argument and checks whether matrix inverse is calculated by makeCacheMatrix function. If it is calculated than it print message by returning a inverse of matrix else calculate Inverse of matrix 'x' and return it
+    inverse <- y$getInverse()                   # Retrieve matrix inverse
+    if(!is.null(inverse)) {                     # If matrix inverse is already calculated
         message("getting cached data")
-        return(inverse)
+        return(inverse)                         # Return a matrix that is the inverse of 'x'
     }
-    data <- y$getMatrix()
-    inverse <- solve(data)
-    y$cacheInverse(inverse)
-    inverse
+    data <- y$getMatrix()                       #  Retrieve a matrix using get method
+    inverse <- solve(data)                      # Calculate inverse of matrix
+    y$cacheInverse(inverse)                     # Set value of inverse of matrix
+    inverse                                     # Return a matrix that is the inverse of 'x'
 }
