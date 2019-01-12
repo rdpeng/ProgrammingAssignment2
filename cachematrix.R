9597a11c45b82b6dd13fe24feacb9e69f09b1325
lyfrafer <chinu.deep100@gmail.com>
2019-01-12 14:38:56 +0530

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCachematrix :: It makes list of different special vectors
+## cachesolve :: It calculates the inverse of matrix after checking condition.
+## Set :: It set the vector value
+## Get :: It get the vector value
+## Setinverse :: It set the inverse
+## getinverse :: It get the inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix function makes a list of function such as, set, get, 
+## setinverse, and getinverse which take the value of vector 
+## this function make the cache matrix which stores already calculated inverse..
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -20,7 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function checks the condition whether the matrix is inverted or not. 
+## If the matrix is inverted already, then it display the result from cached storage.
+## If matrix is not inverted then it calulates the inverse of matrix and display it. 
+## It also set calculated inverse to setinverse function.
 
 cacheSolve <- function(x, ...) {
         i <- x$getinverse()
