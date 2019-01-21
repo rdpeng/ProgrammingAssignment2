09f336f1937f2ce38369e23362035760f1c1600e
edson487 <46858842+edson487@users.noreply.github.com>
2019-01-21 01:15:38 -0600

    Assignment 2: Lexical Scoping
    Matriz inversa

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## En general, vamos a escribir un código que en el que podamos encontrar la inversa de una matriz
 
-## Write a short comment describing this function
+
+#############################################################
+
+## "makeCacheMatrix" almacenará el inverso del objeto que cree; en este caso una matriz
 
 makeCacheMatrix <- function(x = matrix()) {
+         M_inver <- NULL
+  set <- function(y) {
+    x <<- y
+  M_inver <<- NULL
+  }
+  get <- function() x
+  setInversa <- function(inversa) M_inver <<- inversa
+  getInversa <- function() M_inver
+  list(set = set, get = get,
+       setInversa = setInversa,
+       getInversa = getInversa)
 
 }
 
 
-## Write a short comment describing this function
+## Esta función calcula la inversa de la matriz "makeCacheMatrix".
+## si el inverso ya se ha calculado (y la matriz no ha cambiado), entonces la solución es la inversa
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         M_inver <- x$getInversa()
+  if (!is.null(M_inver)) {
+    message("dato en cache")
+    return(M_inver)
+  }
+  mat <- x$get()
+  M_inver <- solve(mat, ...)
+  x$setInversa(M_inver)
+  M_inver
+       
 }
+
