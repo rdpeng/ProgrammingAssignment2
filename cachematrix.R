3aba8fc89ae8506673783b6c6af0755109399a6d
pourkhesalian <pourkhesalian@gmail.com>
2019-04-26 14:07:39 +1000

    Update cachematrix.R
    more readability with better function explanations.

--- cachematrix.R
+++ cachematrix.R
@@ -1,47 +1,42 @@
-## This the second peer-reviewed assignment;to test and try lexical scoping in R; 
-## by Alimohammad Pourkhesalian
-##
-## the function takes in a square convertible matrix and stores it in a special matrix. 
+## the two functions below are able to ceate a special matrix. If the inverse of the matrix is not computed already, the inverse will be calculated. Both the content of the matrix and its inverse are stored for possible later usage.
+
+
+##First funciton makeCachMatrix()
+#The input is an invertible matrix
 ## The outut of the function is a list of four functions
-##1 set the value of the matrix
-##2 get the value of the matrix
-##3 set the value of the inverse matrix
-##4 get the value of the inverse matrix
+##        1 set the value of the matrix
+##        2 get the value of the matrix
+##        3 set the value of the inverse matrix
+##        4 get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
  
-  m <- NULL
+  m.inv <- NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    m.inv <<- NULL
   }
   get <- function() x
-  setinv <- function(solve) m <<- solve
-  getinv <- function() m
+  setinv <- function(inverse) m.inv <<- inverse
+  getinv <- function() m.inv
   list(set = set, get = get,
        setinv = setinv,
-       getinv = getinv) 
-  
-  
+       getinv = getinv)
 }
 
-## The following function calculates the mean of the special "matrix" created with the above function.
-## However, it first checks to see if the inverse has already been calculated.
-## If so, it gets the inverse from the cache and skips the computation. 
-## Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setinv function.
+## 2nd funciton CacheSolve()
+## The function calculates the inverse of the special "matrix" created by the above function only if the inverse is not yet computed.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinv()
-  if(!is.null(m)) {
+  m.inv <- x$getinv()
+  if(!is.null(m.inv)) {
     message("getting cached data")
-    return(m)
+    return(m.inv)
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
-  m
+  m.data <- x$get()
+  m.inv <- solve(m.data, ...)
+  x$setinv(m.inv)
+  m.inv
 }
-
-
 
