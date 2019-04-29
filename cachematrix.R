bcba5ce25ea46d806657aa4cca5eabb89143fd1a
mderryberry <50090289+mderryberry@users.noreply.github.com>
2019-04-28 23:36:50 -0400

    Programming2 md
    Lexical Scoping Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## By creating the two functions below,you could combine them and save time finding the inverse of
+## a large invertible matrix by caching inverses 
 
-## Write a short comment describing this function
+## This function is very similar to the example, it simply sets/gets the value of the matrix and 
+## sets/gets the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes the cached inverse if it already exists and returns it as i; 
+##if it does not exist it calculates the inverse then sets it as i
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- inverse(data, ...)
+  x$setinverse(i)
+  i
 }
