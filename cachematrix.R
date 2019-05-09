d157af79cdfb9c5b3fd8d72a971870736f4d9767
stutika <48391081+stutika@users.noreply.github.com>
2019-05-09 14:49:17 -0700

    Update cachematrix.R
    Challenges with fork and publish.. hopwfully this works

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera Programming Assignment2
+## These two functions improve the performance of the R code where repeated tasks take longer time
+## these functions are used to cache the operation output and return when the operation is repeated
 
-## Write a short comment describing this function
 
+## Set the value of the Matrix, Get the Value of the Matrix, Set and get the Inverse of the Matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  inver <- NULL
+  set <- function(y) {
+    x <<- y
+    inver <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(inverse) inver  <<- inverse
+  getSolve <- function() inver
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
-
 ## Write a short comment describing this function
 
+# If inverse is not available in the cache then only solve it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inver <- x$getSolve()
+  if(!is.null(inver)) {
+    message("getting cached data")
+    return(inver)
+  }
+  mymatrix_data <- x$get()
+  inver <- solve(mymatrix_data, ...)
+  x$setSolve(inver)
+  inver
 }
