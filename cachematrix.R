d1b7810b78f18a45d6efa65031272a2df2258811
stutika <48391081+stutika@users.noreply.github.com>
2019-05-13 12:42:50 -0700

    Revised Assignment2
    Added more comments for both functions and tried fixing the TIE

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,50 @@
 ## Coursera Programming Assignment2
 ## These two functions improve the performance of the R code where repeated tasks take longer time
-## these functions are used to cache the operation output and return when the operation is repeated
+## The functions are used to cache the operation output and return when the operation is repeated
 
 
-## Set the value of the Matrix, Get the Value of the Matrix, Set and get the Inverse of the Matrix
+
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+  #Initialize local variable
   inver <- NULL
+  ## Set the value of the Matrix, Get the Value of the Matrix
   set <- function(y) {
+    ## '<<-' is used to assign the value from a different environment
     x <<- y
     inver <<- NULL
   }
-  get <- function() x
-  setSolve <- function(inverse) inver  <<- inverse
-  getSolve <- function() inver
-  list(set = set, get = get,
-       setSolve = setSolve,
-       getSolve = getSolve)
+  get <- function() {
+    x
+  }
+  ##Set and get the Inverse of the Matrix
+  setSolve <- function(inverse) {
+    inver  <<- inverse
+  }
+  getSolve <- function() {
+    inver
+  }
+  list(
+    set = set,
+    get = get,
+    setSolve = setSolve,
+    getSolve = getSolve
+  )
 }
 
-## Write a short comment describing this function
-
-# If inverse is not available in the cache then only solve it
+## Function to check and return the cache if available
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inver <- x$getSolve()
-  if(!is.null(inver)) {
-    message("getting cached data")
+  ## Check if the inverse has already been calculated, then return the avialable inverse from the cache
+  if (!is.null(inver)) {
+    ## get cached data and return it
     return(inver)
   }
+  ## if the inverse has not already been calculated, then solve the inverse
   mymatrix_data <- x$get()
   inver <- solve(mymatrix_data, ...)
+  ## Set cache and return the new inverse
   x$setSolve(inver)
   inver
 }
