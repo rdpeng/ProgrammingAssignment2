9579f84a4f5d90b4eda13a16cbaffab536d45292
apedata <50496067+apedata@users.noreply.github.com>
2019-05-21 13:56:20 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,50 @@
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(solveMatrix) inv <<- solveMatrix
+        getInverse <- function() inv
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+cacheSolve <- function(x, ...) {
+        inv <- x$getInverse()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setInverse(inv)
+        inv      
+}
+
 ## Matrix Inversion Function  
 ## functions to cache the inverse of matrix
 
