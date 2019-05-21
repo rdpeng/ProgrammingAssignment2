d6d6c49b3b7dbdcef39d678ec9213457266ab8ef
apedata <50496067+apedata@users.noreply.github.com>
2019-05-21 13:26:29 -0700

    Add files via upload
    course submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Inversion Function  
+## functions to cache the inverse of matrix
 
 ## Write a short comment describing this function
 
@@ -8,7 +8,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## creates special matrix object that can cache its inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
