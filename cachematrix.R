179040ac0c4084baf402e4ee406db9261aef3b95
wojtek.rogowiec <wojtek.rogowiec@gmail.com>
2019-07-06 18:47:07 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(pupa) m <<- pupa
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...) #Inverse computation here, rest co code does not need to be changed, tested with many randomly generated matrix to level 100x100
+  x$setmean(m)
+  m
 }
