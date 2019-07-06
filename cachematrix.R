e9eafba703c6b0a0006a52c44b623b197a711474
womaro <47694592+womaro@users.noreply.github.com>
2019-07-06 19:27:12 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## In fact functions nead only to change computation function from mean to solve, rest works at the same way, with setters and getters. 
 ## Small difference is also an function argument, that is not a vector, but matrix, as per assignement, there is not need to check whenever
 ## matris is invertable, as we assume it is. 
 
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
+makeCacheMatrix <- function(x = matrix()) { # first function take matrix argument, we do not check whenever matris is invertible
+  m <- NULL                                 # declare value
+  set <- function(y) {                      # setter function definition
     x <<- y
-    m <<- NULL
+    m <<- NULL      
   }
-  get <- function() x
+  get <- function() x                        # getter function definition
   setinverse <- function(whatever) m <<- whatever
   getinverse <- function() m
-  list(set = set, get = get,
+  list(set = set, get = get,                 # returm list with named values, to allow acces by $
        setinverse = setinverse,
        getinverse = getinverse)
 }
@@ -23,13 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This is function, that calculates inverse (solve) if not yet inverted erlier. 
 
 cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
+  m <- x$getinverse()                     
+  if(!is.null(m)) {                         # check if matrix needs to be inversed, or value can be taken from memory
     message("getting cached data of inveresed matrix")
     return(m)
   }
   data <- x$get()
-  m <- solve(data, ...)
+  m <- solve(data, ...)                     # real inverse matrix computation is done in this place
   x$setinverse(m)
   m
 } 
