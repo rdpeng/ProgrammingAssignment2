c5beb03f7f9f320a3e089f9b0e53a559b286af57
kstepanmpmg <42830400+kstepanmpmg@users.noreply.github.com>
2019-08-06 10:19:30 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+  if (!is.matrix(original.matrix)) {
+    stop("Please give a matrix")
+  }
+  
+  inverted.matrix <- NULL
+  
+  set <- function(y) {
+    original.matrix <<- y
+    inverted.matrix <<- NULL
+  }
+  
+  # Functions for getting and setting cached inv. matrix value
+  get <- function() original.matrix
+  # Inversing the matrix using build in solve() function in R
+  set.inverse <- function(solve) inverted.matrix <<- solve
+  get.inverse <- function() inverted.matrix
+  
+  list(
+    set = set, 
+    get = get,
+    set.inverse = set.inverse,
+    get.inverse = get.inverse)
 }
 
 
@@ -12,4 +34,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+          inverted.matrix <- cacheable.matrix$get.inverse()
+  # Do we have cached matrix available?
+  if(!is.null(inverted.matrix)) {
+    message("Getting cached inverse matrix")
+    return(inverted.matrix)
+  }
+  # Let's create inverted matrix in case
+  # there's no cached matrix available.
+  matrix.to.inverse <- cacheable.matrix$get()
+  inverted.matrix <- solve(matrix.to.inverse)
+  cacheable.matrix$set.inverse(inverted.matrix)
+  inverted.matrix
 }
+
+
