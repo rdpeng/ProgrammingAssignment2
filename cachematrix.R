25fdeef5b565d65df33f9ad059b70ba9e72d8d37
Tinashe <tinashe.m@outlook.com>
2019-08-11 17:49:42 +0200

    Assignment 2
    cachemarix and cachesolve functions for peer graded assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -21,7 +21,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
