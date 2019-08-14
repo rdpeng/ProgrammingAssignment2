31c96c55e20085cf28e6927faf9c4299f5507069
kswathi13 <34700611+kswathi13@users.noreply.github.com>
2019-08-14 18:56:13 +0530

    Update cachematrix.R
    Added makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This function Function makeCacheMatrix gets a matrix as an input, set the value of the matrix,
+#get the value of the matrix, set the inverse Matrix and get the inverse Matrix. The matrix object
+#can cache its own object. 
 
-## Write a short comment describing this function
+#<<- operator is used to assign a value to an object in an environment that is different 
+#from the current environment 
 
+#take the matrix as an input
 makeCacheMatrix <- function(x = matrix()) {
+  invMatrix <- NULL
+
+  #set the value of the Matrix
+  setMatrix <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+
+  getMatrix <- function() x                              #get the value of the Matrix
+  setInverse <- function(inverse) invMatrix <<- inverse  #set the value of the invertible matrix
+  getInverse <- function() invMatrix                     #get the value of the invertible matrix
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse, getInverse = getInverse)
 
 }
+
 
 
 ## Write a short comment describing this function
