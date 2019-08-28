6e9101f7b5574922bed4946f48044b255874dc17
Dale Liebenberg <40733522+dalelieb@users.noreply.github.com>
2019-08-28 10:29:47 +0200

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions together have as result the inverse of a matrix as per the follow example:
+##> myMatrix<-c(1,3,5,8)
+##> dimension(myMatrix)<-c(2,2)
+##> cacheSolve(makeCacheMatrix(myMatrix))
+##           [,1]       [,2]
+##[1,] -1.1428571  0.7142857
+##[2,]  0.4285714 -0.1428571
 
-## Write a short comment describing this function
+## The function below creates a matrix that can be inversed and cached. 
+## As per instuction it is assumed that is is possible to inverse the given function and no error
+## trapping has been included.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+          m <- NULL
+        set <- function(y){
+                 x <<- y
+                 m <<- NULL
+        }
+        get  <- function () x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list (set=set, get=get, setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This returns the inverse of matrix "x" calulated above.
+## If the inverse has already been calculated and is found in the cache, the version in the cache is returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if (!is.null(m)) {
+                message("getting cashed data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse
+        m
 }
