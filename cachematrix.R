5649d15f5edb9e5b7915b6333851f3fe44ea154d
nima14 <52500203+nima14@users.noreply.github.com>
2019-09-06 12:51:31 +0430

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,13 @@
+## These 2 functions return the inverse matrix that is given.
+## If the inverse of the matrix is already calculated then these function will not calculate it again and they will return the cached data
+## makeCacheMatrix is where you give the matrix that want to be inversed and casheSolve is
+## where the inverse matrix is calculated or the cached data is returned
+##  "MatInverse" is the inverse of x. 
+## Function setInverse sets the inverse of the matrix and function getInverse returns the inverse matrix
+## in cacheSolve, the function first looks up for the calculated inverse matrix and if it exists,it will show it
+## but if not, it will calculate it
+
+
 makeCacheMatrix <- function(x = matrix()) {
   MatInverse<- NULL
   set <- function(y) {
