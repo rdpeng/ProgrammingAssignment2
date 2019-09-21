1132b618e8735d45cf73c59f451c500ae452e79c
hasinda <55535032+hasinda@users.noreply.github.com>
2019-09-21 06:31:37 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCachematrix is a fuction that caches the inverse of a given matrix. it does so in the following order
 
-## Write a short comment describing this function
+## first it sets the value of the given matrix
+## then it gets the value otf that matrix
+## then it sets the value of inverse of that matrix
+## finally it returns/get the value of inverse of the matrix
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) { ## setting the vaue of matrix
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x ## getting the value of the matrix
+  setinverse <- function(inverse) i <<- inverse  ## setting the value of the inverse
+  getinverse <- function() i ## getting the value of inverse
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) ## creating the output so it can be used in next functions
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that calculates the inverse of.. 
+##..the matrix set in the cache following makeCacheMatrix function
+
+## first it looks for any inverse which has already been contained in the cache. If so it returns that.
+## if not it does the calculations on its own and returns the output.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse() ## getting the value of inverse of matrix(if any) as specified in previous function
+  if (!is.null(i)) { ##if inverse is already there in cache, returining it. 
+  return(i)
+  }
+  
+  matrix_data <- x$get() ## getting he data of matrix in order to calculate inverse in next function
+  i <- solve(matrix_data, ...) ##using the 'solve' function to calculate the inverse
+  x$setinverse(i)
+  i ## returning the calculated inverse
 }
