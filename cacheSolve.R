2c6d45393c1925d1e7c969057c91da47ac93af98
mohamed-zaky <55677942+mohamed-zaky@users.noreply.github.com>
2019-10-11 16:16:40 +1300

    Initial upload
    Cached matrix inversion

---
+++ cacheSolve.R
@@ -0,0 +1,13 @@
+cacheSolve <- function(x, ...)
+{
+  m <- x$getinverse()
+  if(!is.null(m)) 
+  {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
---
+++ makeCacheMatrix.R
@@ -0,0 +1,15 @@
+makeCacheMatrix <- function(x = int())
+{
+  m <- NULL
+  set <- function(y) 
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
