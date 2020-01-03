6b8a678ebc692d40e64827363117ea0f5e566b3e
VibhavProg <59289640+VibhavProg@users.noreply.github.com>
2020-01-03 13:48:18 -0600

    Completed Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## These functions are part of my code for the Week 3 prgramming assignment of 
+##Coursera's Data Science R Programming course. I started this assignment on 
+##the 3rd of January in the year of 2020; My GitHub username is VibhavProg
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  setMatrix <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  getMatrix <- function(){
+    x
+  }
+  setInverse <- function(inverseTwo){
+    inverse <<- inverseTwo
+  }
+  getInverse <- function(){
+    inverse
+  }
+  list(setMatrix = setMatrix, getMatrix = getMatrix,setInverse = setInverse, getInverse = getInverse)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)){
+    return(inverse)
+  }
+  dat <- x$getMatrix()
+  inverse <- solve(dat, ...)
+  x$setInverse(inverse)
+  inverse
 }
