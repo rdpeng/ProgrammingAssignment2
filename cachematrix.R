3c771d37bd5b472884b87e9bc78d45533c482855
Eugenia Gallardo-Allen <57546864+egallen273@users.noreply.github.com>
2020-01-05 15:04:10 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Firts Function
+## MakeCacheMatrix
 
-## Write a short comment describing this function
+## This function creates a special "matriz" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
