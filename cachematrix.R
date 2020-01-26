f7c63c6ed4e9a269f27a03f39900851242d2ab14
sagyg <sagy.galor@gmail.com>
2020-01-26 22:58:21 +0200

    commite the week3 assigemnt

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
+# the matrix x is the funcation input
 makeCacheMatrix <- function(x = matrix()) {
 
+  inevertMatrix <- NULL
+  setMatrix <- function(y){
+    x <<- y
+    inevertMatrix <<- NULL
+  }
+  ##seters and geters 
+  getMatrix <- function()x
+  setInverse <- function(inverse) inevertMatrix <<- inverse
+  getInverse <- function() inevertMatrix 
+  list(setMatrix = setMatrix, getMatrix = getMatrix, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  MyInverseMatrix <- x$getInverse()
+  if(!is.null(MyInverseMatrix)) {
+    message("getting cached data")
+    return(MyInverseMatrix)
+  }
+  ##if invarse matrix does nnot exsist in cache
+  MyMatrixToInverse <- x$getMatrix()
+  setInverse <- solve(MyMatrixToInverse, ...) # invert the metrix using solve()
+  x$setInverse(MyInverseMatrix) ### set the invert  metrix
+  return (MyInverseMatrix)
 }
