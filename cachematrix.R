21a9cba03d3779f16e96d649a7bca0a9ee36afc3
avgoustos <60583494+avgoustos@users.noreply.github.com>
2020-02-02 21:17:19 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+# My solution
+# The function calculates the inverse of a matrix
+# but if that already has been done for a matrix then,
+# i simply retrieves the solution from the cache... Duh
 
-## Write a short comment describing this function
+# first I create the special "vector"
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  m <-NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+# Then I create the function that either calculates the inverse 
+# or if answer already stored
+# it retrieves it from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
