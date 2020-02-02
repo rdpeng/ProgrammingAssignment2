13c5891bb143a5e02fc6823a4458dfacf750d907
avgoustos <60583494+avgoustos@users.noreply.github.com>
2020-02-02 21:22:53 +0000

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -22,5 +22,16 @@ makeCacheMatrix <- function(x = matrix()){
 # it retrieves it from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  m <- x$getinv()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+} 
+
+# Tadaaaa
+# Thank you, good luck and have fun :)
