b447a0cc347866e50f0ffc08846d9422574dd9fe
Benmarkson <57838231+Benmarkson@users.noreply.github.com>
2020-03-20 13:01:52 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## the funtion MakeCachedMatrix takes as input a matrix, determines whether the matrix inversible and returns and inverse if true.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if(ncol(matx) == nrow(x) && det(x) != 0){
+    set <- function(y){
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function() m <<- inv(x)
+    getinverse <- function() m
+    list( set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  }else{
+    return (message("matrix is not inversible"))
+  }
 }
 
 
-## Write a short comment describing this function
+## This function in wanting to calculate the inverse of a matrix, looks in the cache if the inverse has been calculated. I calculates if not, a returns a value from the cache if available.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }else{
+    data <- matx$get
+    m <- inv(data, ...)
+    x$setinverse(m)
+    m
+  }
 }
