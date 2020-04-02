7cf014d6057df519da25ece3d02c956373e7bd90
yeyuan98 <62569917+yeyuan98@users.noreply.github.com>
2020-04-01 20:24:10 -0400

    Update cachematrix.R
    Version validation with the following test:
    N <- 1000
    mat <- matrix(rnorm(N^2,5,1),N,N)
    
    cmat <- makeCacheMatrix(mat)
    cacheSolve(cmat)
    cacheSolve(cmat)
    
    cmat$set(mat)
    cacheSolve(cmat)
    cacheSolve(cmat)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+# makeCacheMatrix - initialize a matrix wrapper containing its cached inverse
+# cacheSolve - return the cached inverse or compute and cache it if not computed
 
 ## Write a short comment describing this function
-
+# This function defines a matrix wrapper containing cache for its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(m){
+                x <<- m ; inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(i) inv <<- i
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
+# This function returns the cached inverse or compute and cache it.
 cacheSolve <- function(x, ...) {
+        inv = x$getinv()
+        if(is.null(inv)) {
+                message("Caching matrix inverse..")
+                x$setinv(solve(x$get(),...))
+                inv = x$getinv()
+        } else {
+                message("Matrix inverse already stored!")
+        }
         ## Return a matrix that is the inverse of 'x'
+        inv
 }
