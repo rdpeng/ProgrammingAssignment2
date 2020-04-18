4520392adb2e7df7f344c0f27e57339f0c94dbff
Juan Diego Gallego Q <63928388+jdgallegoq@users.noreply.github.com>
2020-04-18 16:25:40 -0500

    Programming Assigment 2
    jdgallegoq

---
+++ ProgrammingAssignment2/ProgrammingAssignment2.R
@@ -0,0 +1,23 @@
+makeCacheMatrix<- function(x = matrix()){
+  inv = NULL
+  set = function(y) {
+    x<<- y
+    inv<<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv<<- inverse
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+
+cacheSolve<- function(x, ...){
+  inv = x$getinv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  x$setinv(inv)
+  return(inv)
+}
