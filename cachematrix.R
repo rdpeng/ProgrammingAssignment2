0f259961f4dd720b46ad948e5c98872fb97e2cbb
nishdogrant <nishant.nhn95@gmail.com>
2020-04-19 20:13:25 +0530

    Add files via upload
    This is my attempt in the Programming Assignment for lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## I will be creating 2 functions that will cache the inverse of a matrix
+##  FIrst function is makeCacheMatrix which will create an inverse of an matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Begin with setting up the matrix
+  i <- NULL
+  
+  ## Assigning function to set the matrix
+  set <- function( matrix ) {
+    m <<- matrix
+    i <<- NULL
+  }
+  
+  ## assigning get to retrieve the already set matrix
+  ## this command will return the matrix
+  get <- function() {
+    m
+  }
+  
+  
+  ## assigning setInverse to set the inverse of the matrix
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  ## Method to get the inverse of the matrix
+  getInverse <- function() {
+    ## Return the inverse property
+    i
+  }
+  
+  ## Return a list of the methods
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the special matrix returned by "makeCacheMatrix"
+## above. If the inverse has already been calculated (and the matrix has not
+## changed), then the "cachesolve" should retrieve the inverse from the cache.
+
+
+
+## This function is to cache the matrix inverse 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  
+  ## Just return the inverse if its already set
+  if( !is.null(m) ) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  ## Get the matrix from our object
+  data <- x$get()
+  
+  ## Calculate the inverse using matrix multiplication
+  m <- solve(data) %*% data
+  
+  ## Set the inverse to the object
+  x$setInverse(m)
+  
+  ## Return the matrix
+  m
 }
