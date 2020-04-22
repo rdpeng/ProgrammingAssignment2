524d0b19b48967539080517030b633e331443acd
Yusong <44549353+Yusong-Z@users.noreply.github.com>
2020-04-22 15:14:55 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file is going to cache the inverse of a matrix, and is largely
+## based on the example of caching the mean of a vector in guidance.
+## Thanks for your reviewing!
 
-## Write a short comment describing this function
+
+## Create a list containing a function to set and get the matrix 
+## and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x   <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list (set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Calculate the inverse of the matrix if not existing
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached inverse")
+        return(m)
+    }
+    mtx <- x$get()
+    m    <- solve(mtx, ...) 
+    x$setinv(m)
+    m
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
