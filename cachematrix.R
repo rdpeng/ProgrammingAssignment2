f6d4282c5863ed41ed2c0d6140537ced01aa7e7f
krishnakeerthich <64297618+krishnakeerthich@users.noreply.github.com>
2020-04-25 13:19:02 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
+## cost computational and used for matrix inverse caching also
 ## functions do
 
-## Write a short comment describing this function
+## function creates a special matrix that can inverse the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+       set <- function(y) {
+              x <<- y
+              m <<- NULL
+       }
+       get <- function() x
+       setmean <- function(mean) m <<- mean
+       getmean <- function() m
+       list(set = set, get = get,
+            setmean = setmean,
+            getmean = getmean)
 }
 
 
-## Write a short comment describing this function
-
+## to solve the cache matrix this is used
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmean()
+       if(!is.null(m)) {
+              message("getting cached data")
+              return(m)
+       }
+       data <- x$get()
+       m <- mean(data, ...)
+       x$setmean(m)
+       
 }
