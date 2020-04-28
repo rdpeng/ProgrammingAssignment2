507bde83927c4bdcf8ed2530a3f1aec93c2f309a
Thomas Simonson <thomas.simonson@gmail.com>
2020-04-28 17:31:46 +0200

    Update cachematrix.R
    Code worked fine. I made a mistake when I tested it.
    I've added comments explaining how the functions work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## MakeCacheMatrix takes a matrix as its argument and...
+## 1) resets the inverse matrix in the cache to NULL
+## 2) creates a list of functions, linked to that matrix:
+# - 'get' allows to retreive the matrix
+# - 'setinv' allows to cache the inverse of the matrix as 'j', once 'cacheSolve' has calculated it.
+# - 'getinv' retreives the inverse of the matrix from the cache
 
 makeCacheMatrix <- function(x = matrix()) {
         j<<-NULL
@@ -14,11 +19,18 @@ makeCacheMatrix <- function(x = matrix()) {
         getinv<-function(){
                 j
         }
-        list(get=get,setinv=setinv,getinv=getinv)
+        list(get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as argument a list of functions of the form 'makeCacheMatrix'
+## Its behavior depends on the following conditions:
+## - If the inverse of that matrix has already been calculated, it is retreived from the cache
+## - If it has not been calculated, the function checks if it is invertible
+##     - If it isn't, it returns the message "This matrix is not invertible"
+##     - If it is, the inverse is calculated, stored in the cache and returned
 
 cacheSolve <- function(ml, ...) {
         ## Return a matrix that is the inverse of 'x'
