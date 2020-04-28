27d5c269293e5ebcf21a1acd2155a1abe08b288b
Thomas Simonson <thomas.simonson@gmail.com>
2020-04-28 15:56:58 +0200

    Update cachematrix.R
    First draft

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        j<<-NULL
+        get<-function(){
+                x
+        }
+        setinv<-function(i){
+                j<<-i
+        }
+        getinv<-function(){
+                j
+        }
+        list(get=get,setinv=setinv,getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(ml, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i<-ml$getinv()
+        if(!is.null(i)){
+                message("getting cached inverse")
+                return(i)
+        }
+        x<-ml$get()
+        if(det(x)!=0){
+                i<-solve(x)
+                ml$setinv(i)
+                return(i)        
+        }
+        message("This matrix is not invertible")
 }
