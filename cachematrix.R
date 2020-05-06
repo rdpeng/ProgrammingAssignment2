1c8caa25468735f4e17a14bd7bc3242356c01c37
Jannekejs <64459316+Jannekejs@users.noreply.github.com>
2020-05-06 09:38:56 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -18,5 +18,13 @@ list (set, get, setinv, getinv)
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if (!is.null(i)) {
+          message("getting cached data")
+          return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
