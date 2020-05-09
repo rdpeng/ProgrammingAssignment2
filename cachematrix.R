d207212113f056591bc9444c5d7c75dd3af36e4c
GitHubrenu <64904572+GitHubrenu@users.noreply.github.com>
2020-05-10 00:54:27 +0700

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -22,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) { ## define the argument with default 
 }
 
 
-## Write a short comment describing this function
+
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 ## If the inverse has already been calculated (and the matrix has not changed),
 ## then cacheSolve will retrieve the inverse from the cache
