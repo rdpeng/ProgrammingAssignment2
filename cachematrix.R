a858cf4fdd02ed03a0e279f1d05da87b0e378763
HAMidtun <65588216+HAMidtun@users.noreply.github.com>
2020-05-20 15:41:53 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,42 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+  #first, we set the value for the matrix
+        j <- NULL 
+        set <- function(y) { 
+              
+        }
+        get <- function() x #Get the value of the Matrix
+        setinverse <- function(inverse) j <<- inverse #Then set the value for the inverse matrix
+        getinverse <- function() j #Get the value of the inverse matrix
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #Get the value of the invertable matrix from the previous function
+  i <- x$getinverse()
+  if (!is.null(i)) { #if the inverse matrix is not NULL 
+          message("Getting cached matrix") #Then return the message "Getting cached matrix"
+          return(i)
+  }
+  #If the value of the ivertible matrix is NULL
+  data <- x$get() #Retrives the original matrix data
+  i <- solve(data, ...) #Uses the solve function to inverse the matrix
+  x$setinverse(i) #Set's the invertible matrix
+  i #returns the invertible matrix.
 }
+        ## Return a matrix that is the inverse of 'x'
+
+###Test###
+B <- matrix(c(1,2,3,4),2,2)
+
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1) 
+##[,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
