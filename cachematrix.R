30d634319d725e1137376fc0281f22846edacf4d
VAISHNAVI P <51691751+vaishnavi8701@users.noreply.github.com>
2020-05-20 21:57:25 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##Initializing j with NULL and setting funtion  to recurse
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+   j <- NULL
+  set <- function(y){
+  x <<- y
+  j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
 
 }
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   j <- x$getInverse()
+  if(!is.null(j)){
+  message("getting cached data")
+  return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
 }
