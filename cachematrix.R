bedcf716fe2c9f0b17b92ddc8162422a180882c8
42oach <62992385+42oach@users.noreply.github.com>
2020-05-23 06:49:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,49 @@
 ## This function creates object that can cache matrix inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-        j <- NULL
-        set <- function(y){
-                x <<- y
-                j <<- NULL
+        xMat<-makeCacheMatrix(x)
+        parent.env(xMat$getenv())$m
+         environment(xMat$getmean)
+        m<-NULL
+        evn <- environment()
+                y<-NULL 
+}
+
+setmatrix<-function(y){  
+	x<<-y  
+	m<<-NULL
+	}
+  
+getmatrix<-function() x  
+setinverse<-function(solve) m<<- solve  
+getinverse<-function() m  
+getenv<- function() environment()
+
+list (setmatrix=setmatrix, getmatrix = getmatrix,   
+setinverse = setinverse,
+getinverse = getinverse,
+getenv = getenv)
 
 }
-}
-
 ##This function computes the inverse of the matrixreturned by function above. 
 ##cacheSolve retrieves the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        j <- x$getInverse()
-        if(!is.null(j)){
-                message("getting cached data")
-                return(j)
-        }
-        mat <- x$get()
-        j <- solve(mat,...)
-        x$setInverse(j)
-        j
+cacheSolve <- function(xMat= m(), ...) {
+	m <- xMat$getinverse() 
+	if(!is.null(m)){ 
+		if(xMat$setmatrix() == xMat$getmatrix()) { 
+    	message("getting cached data")
+    	matrix<-xMat$get()
+    	m<-solve(matrix, ...)
+    	xMat$setmatrix(m)
+    	return(m) 
+    	}
+    	
+    	y <- xMat$getmatrix() 
+    	xMat$setmatrix(y)
+    	m <- solve(y, ...) 
+    	xMat$setinverse(m) 
+    	m 
+    	}
+    	
 }
-
