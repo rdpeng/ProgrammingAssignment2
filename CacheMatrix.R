8892677cbe52bb4db07e1ff619d7885ef4061bb4
kevinluong510 <66141863+kevinluong510@users.noreply.github.com>
2020-06-01 19:35:01 -0400

    Add files via upload

---
+++ CacheMatrix.R
@@ -0,0 +1,25 @@
+makeMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+cachesolve <- function(x, ...) {
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+}
