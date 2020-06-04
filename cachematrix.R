3ae241cce880871b1e91d8c69cc4ae1524a4bf7a
jaya-adler <jayashankar9741@gmail.com>
2020-06-04 23:35:18 +0530

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,43 @@
 ## functions do
 
 ## Write a short comment describing this function
+## The below function creates the object , so that we can give input by using set function and 
+## this object is passed to below function to compute.  
 
 makeCacheMatrix <- function(x = matrix()) {
+  Minverse <- matrix(data = NA)
+  set <- function(y) {
+    x <<- y
+    Minverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) {
+     Minverse <<- solve(x)
+    }
+  getinverse <- function()  Minverse
+list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
-
+ 
 
 ## Write a short comment describing this function
+##It takes the returned values or object and returns inverse matrix if it is cached else it set 
+##the value to  cache and  returns  the  inverse of the matrix
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i) && !is.na(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  nmatrix <- x$get()
+  ## Here  below iam checking whether the matrix is  invertable are not
+  if(ncol(nmatrix) != nrow(nmatrix) || det(nmatrix) == 0) return(" Matrix is not invertable !!!")
+  nm <- solve(nmatrix)
+  x$setinverse(nm)
+  nm
 }
