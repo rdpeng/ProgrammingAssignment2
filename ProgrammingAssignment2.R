d15f0718fa1a617deb52a7c2692ee3313d9b7f69
vdlearngit24 <66055663+vdlearngit24@users.noreply.github.com>
2020-06-17 18:59:46 +0530

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
