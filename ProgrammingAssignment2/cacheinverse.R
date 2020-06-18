2a172c24f949790bd2d5d70036aebba267d5fa49
stutisinha <48326560+stutisinha@users.noreply.github.com>
2020-06-18 07:53:23 -1100

    Add files via upload

---
+++ ProgrammingAssignment2/cacheinverse.R
@@ -0,0 +1,22 @@
+makeCacheMatrix<-function(x=matrix()){
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function() {x}
+  setinverse<-function(inverse){inv<<-inverse}
+  getinverse<-function() {inv}
+  list(set = set , get=get , getinverse=getinverse , setinverse=setinverse)
+}
+cachesolve<-function(x,...){
+  inv<-x$getinverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat<-x$get()
+  inv<-solve(mat,...)
+  x$setinverse(inv)
+  inv
+}
