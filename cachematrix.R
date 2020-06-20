dd4e083429d2ce6a3513ca5bb7cea08e0ba032b1
mdaminurrahmansarker <65933793+mdaminurrahmansarker@users.noreply.github.com>
2020-06-20 07:52:18 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -15,6 +15,7 @@ makeCacheMatrix<- function(x=matrix()){
 }
 ##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 cacheSolve<- function(x, ...){
+  ## Return a matrix that is the inverse of 'x'
     inv<-x$getInverse()
     if(!is.null(inv)){
     message("getting cahced data")
