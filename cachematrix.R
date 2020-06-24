006f750d58607490cc44a014c9db95e1da45de4b
jemanuel-perez <jemanuel.perez@gmail.com>
2020-06-24 11:09:38 -0600

    This is my finalized homework in order to get the inverse of a matrix!! Juan

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        mInverse <-NULL
+        
+        set <- function(y = matrix()){
+                x <<- y
+                mInverse <<- NULL
+        }
+        
+        get <- function(){x}
+        
+        setInverse <- function(tempInverse){
+                mInverse <<-tempInverse
+        }
+        
+        getInverse <- function(){
+                mInverse
+        }
+        
+        list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 
 }
 
@@ -12,4 +30,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invMat <-x$getInverse()
+        if(!is.null(invMat)){
+                message("Getting Inverse Matrix from Cache!!")
+                return(invMat)
+        }
+        
+        origMatrix<-x$get()
+        invMat <- solve(origMatrix)
+        x$setInverse(invMat)
+        invMat
 }
