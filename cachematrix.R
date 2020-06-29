0e569556cfef1d83c21ba96af2a13a95af29377f
sagc13 <silviagarciacabrera@hotmail.com>
2020-06-29 14:07:06 -0500

    Testing commit on changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## The cachematrix.R has 2 functions: 1) creates a special vector of functions and 
+## 2) gets the inverse of a matrix gotten from function no.1
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
