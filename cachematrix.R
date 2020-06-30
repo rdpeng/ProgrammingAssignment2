b349ba15b835bb6b4e48ef5c37da3826a17d93f7
Bohdan Nedostup <nbohdan999@gmail.com>
2020-06-30 17:58:07 +0300

    Coursera week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## Function creates a special "matrix" object that can cache its inverse
+        i <- NULL
+        set <- function(y) {
+          x <<- y
+          i <<- NULL
+        }
+        get <- function() x
+        set_solve <- function(solve) i <<- solve
+        get_solve <- function() i
+        list(set = set, get = get,
+             set_solve = set_solve,
+             get_solve = get_solve)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$get_solve()
+        if (!is.na(i)){
+          message("getting cached data")
+          return(i)
+        }
+        data <- x$get()
+        i <- solve(x)
+        x$set_solve(i)
+        i
 }
