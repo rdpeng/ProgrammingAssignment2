4d4b6019acbfde5d83aa5e4f4d17475985704677
G.Rhodes <63823041+rgg48@users.noreply.github.com>
2020-07-02 18:50:46 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,17 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) {
+  spmx <- NULL
+  set <- function(y){
+    x <<- y
+    spmx <<- NULL
+  }
+  get <- function() x
+  setim <- function(solve) spmx <<- solve
+  getim <- function() spmx
+  list(set=set, get=get, setim=setim, getim=getim)
 }
 
 
@@ -12,4 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  spmx <- x$getim()
+  if(!is.null(spmx)){
+    message("getting cached data")
+    return(spmx)
+  }
+  data <- x$get()
+  spmx <- data**-1
+  x$setim(spmx)
+  spmx
 }
