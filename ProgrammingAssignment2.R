6a69a8080907a255d49beeaec2ef6cd9d4aa3038
Manuel Alejandro <alejombkm01@gmail.com>
2020-07-02 12:08:55 -0500

    programming assignment 2

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,25 @@
+makeCache <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting inversed matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
