d9e7b1231625b58c94867aba17896c6a30b779ef
dkpmnit <66669524+dkpmnit@users.noreply.github.com>
2020-07-03 16:08:37 +0530

    Update chachematrix
    new chachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+
+B <- matrix(c(1,2,3,4),2,2)
+
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1)
+
+cacheSolve(B1) 
