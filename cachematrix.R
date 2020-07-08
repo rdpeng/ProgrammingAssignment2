d726b65ca71ef104fee98197ddc39bce8c624dac
pranitasalunke <46518349+pranitasalunke@users.noreply.github.com>
2020-07-08 18:23:34 +0530

    cachematrix.R
    Edited code

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,7 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 makeCacheMatrix <- function(x=matrix()){
   inv <- NULL
   set <- function(y){
@@ -10,6 +14,10 @@ makeCacheMatrix <- function(x=matrix()){
   list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
+
+
+
+## Write a short comment describing this function
 cacheSolve <- function(x, ...){
   inv <- x$getInverse()
   if(!is.null(inv)) {
