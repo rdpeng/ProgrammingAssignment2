4e255b8d78d85a20ca6421de31237de56a8d3237
deepa11-code <67897555+deepa11-code@users.noreply.github.com>
2020-07-10 01:03:42 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+       inv <- NULL
+       set <- function(y) {
+             x <<- y
+             inv <<- NULL
+       }
+       get <- function() {x}
+       setInverse <- function(inverse) {inv <<- inverse}
+       getInverse <- function() {inv}
+       list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <-function(x, ...){
+      inv <- x$getInverse()
+      if(!is.null(inv)){
+        message("getting catched data")
+        return(inv)
+      }
+      mat <- x$get()
+      inv <- solve(mat ,...)
+      inv
 }
