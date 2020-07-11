424d62f3b4b7045ecc1fac4a94588f9efd292d22
shivamsks219 <68140446+shivamsks219@users.noreply.github.com>
2020-07-11 12:45:06 +0530

    Update cachematrix.R
    This is the programming code for finding the inverse of a matrix using caching method. As given in the programming Assignment of week 3 lexical scoping in R.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Shivam Kumar
+## 10 July 2020
+## This is a R program code for finding inverse of matrix.
+## This program uses caching method of operation.
 
-## Write a short comment describing this function
+## first we have to make cache of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  j <- NULL
+  set <- function(y){
+  x <<- y
+  j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
+
 }
 
 
-## Write a short comment describing this function
+## Getting inverse of the cached matrix by using solve function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+  message("getting cached data")
+  return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
 }
+
