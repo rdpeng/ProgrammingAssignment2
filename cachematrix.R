b8056651ded7e941a8adbfcccf9c83ddfaf35634
UserForCourseDS <67877928+UserForCourseDS@users.noreply.github.com>
2020-07-27 13:12:01 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,35 @@
-makeCacheMatrix <- function ( x = matrix ()) 
-    {
-    inv <- NULL set <- function ( y ) 
-      {                 
-      x << - y inv << - NULL         
-      } 
-    get <- function () x         
-    setInverse <- function ( inverse ) 
-    inv << - inverse         
-    getInverse <- function () inv         
-    list ( set = set , get = get, setInverse = setInverse, getInverse = getInverse) 
+    ## Actividad corespondiente a la semana 3: Léxico
+    ## El código representa una funcion que utiliza los objetos 'datos', 'nrow' y 'ncol'
+    ## la función genera una matriz 'x' usando información de los objetos y genera
+    ## 'inv' como el inverso de 'x'
+    
+    makeCacheMatrix <- function (x = matrix (), data, nrow, ncol) {
+      x <- matriz (datos, nrow, ncol)
+      inv <- NULL
+      establecer <- función (y) {
+        x << - y
+        inv << - NULL
+      }
+      obtener <- función () x
+      setmean <- function (solve) inv << - resolver
+      getmean <- función () inv
+      lista (set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
     }
-  cacheSolve <- function ( x , ...) {         
-    inv <- x $ getInverse () 
-    if (! is. null (inv)) {message ( "obtener datos en caché " ) return (inv)}
-    mat <- x $ get ()         
-    inv <- solve (mat, ...)         
-    x $ setInverse (inv) inv }
+  
+  ## La funcion cacheSolve regresa la inversa si esta no falta, o 
+  ## calcula y devuelve la inversa si es que esta falta, 
+  ## cuyo insumo es la función anterior makeCacheMatrix
+  
+  cacheSolve <- función (x, ...) {
+    inv <- x $ getmean ()
+    if (! is.null (inv)) {
+      mensaje ("obtener datos en caché")
+      volver (inv)
+    }
+    datos <- x $ get ()
+    inv <- resolver (datos)
+    x $ setmean (inv)
+    inv
+  }
