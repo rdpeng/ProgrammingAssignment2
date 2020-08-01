5d2540f15bdf15b7b9edbc42604d5d88978c093f
loki19981204 <69076522+loki19981204@users.noreply.github.com>
2020-08-01 10:23:54 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -11,7 +11,7 @@ makeCacheMatrix <- function(x = matrix()) {
         }
         get <- function() x                     
         setinverse <- function(inverse) inv <<- inverse  
-        getinverse <- function() inv                     
+        getinverse <- function() inv                    
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
