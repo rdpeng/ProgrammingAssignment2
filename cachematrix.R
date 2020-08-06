547523cc3dced3e35a296bb12f8dc399800da77b
nandal22 <67768151+nandal22@users.noreply.github.com>
2020-08-07 05:07:34 +0530

    assignment3
    assignment

--- cachematrix.R
+++ cachematrix.R
@@ -11,5 +11,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
 }
