5cf0858e46449648fd3eb8e346e0b9d736ca2130
JayshreeHajgude <69118865+JayshreeHajgude@users.noreply.github.com>
2020-08-10 01:34:06 +0530

    Create Test1.R

---
+++ Test1.R
@@ -0,0 +1,12 @@
+my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+my_matrix$get()
+matrix$getInverse()
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+my_matrix$set(matrix(c(2, 2, 1, 4), 2, 2))
+my_matrix$get()
+my_matrix$getInverse()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+
