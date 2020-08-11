0f6cbbe6bd08c2ed9df92892c862240bda493641
rlad809118 <50052286+rlad809118@users.noreply.github.com>
2020-08-10 19:13:50 -0700

    Update cachematrix.R
    This file contains an example matrix assigned to a variable, two functions, and the command to call the variable with the two functions. The result should be a squared inverse of the original 1000x1000 matrix that will be cached in memory for rapid subsequent retrieval.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The 1st function will accept and cache a square, invertible matrix.
+## The second function will accept the first function and provide
+## the inverse of the given matrix. The inverted matrix will 
+## then be cached. the cache feature alleviates additional 
+## computational burden associated with the required linear
+## algebra necessary to perform the inverse operation.
+## My functions relied heavily upon the provided examples.
 
-## Write a short comment describing this function
+## This matrix uses a reasonably large matrix to highlight the 
+## benefit of caching data for computationally large workloads.
+example <- matrix(rnorm(1000000), 1000, 1000)
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+  list(set = set, get = get, 
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## This function will take and store the "example" matrix in memory
+## The sub-functions will apply valeues to variables and additional 
+## functions in the form of a list.
+
+## This statement creates a variable for the second function
+makeCacheMatrix.examplePart2 <- makeCacheMatrix(example)
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   m <- x$getSolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  m
 }
+
+## This second function takes a variable that assigns the matrix to the
+## first function producing am inverse matrix. If the results are 
+## already cached then the cached result will be produced. 
+
+
+cacheSolve(makeCacheMatrix.examplePart2)
+## This statement applies the second function to the variable that
+## assigns the initial matrix to the first function.
+
