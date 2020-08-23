c6ba57c6ffb5d7dae4cc2784f1accc35c2c3ba67
rutujashingare <70087327+rutujashingare@users.noreply.github.com>
2020-08-23 19:14:46 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
++   inv <- NULL
++   set <- function(y){
++     x <<- y
++     inv <<- NULL
++     
++   }
++   get <- function() {x}
++   setInverse <- function(inverse) {inv <<- inverse}
++   getInverse <- function() {inv}
++   list(set = set, get=get, setInverse=setInverse, getInverse=getInverse)
++   
++ }
+> 
+> CacheSolve <- function(x, ...) {
++   inv <- x$getInverse()
++   if(!is.null(inv)){
++     message("getting cached data")
++     return(inv)
++     
++   }
++   mat <- x$get()
++   inv <- solve(mat, ...)
++   x$setInverse(inv)
++   inv
++ }
+> 
+> 
+> pmatrix <- makeCacheMatrix(matrix(1:4, nrow=2, ncol=2))
+> pmatrix$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> 
+> pmatrix$getInverse()
+NULL
+> 
+> CacheSolve(pmatrix)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
