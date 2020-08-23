db6744d6fabbd20e895ed79267688df546e23d39
Thiago Silva <thvsilva@gmail.com>
2020-08-23 11:38:04 -0300

    Assgiment done!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Feito a partir dos dados de makeVector e cachemean
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+    #Cria uma matriz, que deve ser inserida como argumento da funcao
+    inv <- NULL
+    set <- function(novaMatriz){
+        #Permite realimentar com nova matriz, zerando a cache e redefinindo a matriz
+        x <<- novaMatriz
+        inv <<- NULL
+    }
+    get <- function(){
+        #Busca a matriz armazenada
+        x
+    }
+    setInverse <- function(matrizInversa){
+        #Guarda a matriz inversa calculada
+        inv <<- matrizInversa
+    }
+    getInverse <- function(){
+        #Busca a matriz inversa armazenada
+        inv
+    }
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+    #A funcao checa se a matriz inversa ja foi calculada ou armazenada.
+    #Se sim, busca o valor desta e retorna.
+    #Se nao, calcula (funcao solve) e armazena empregando o metodo da funcao makeCacheMatrix.
+    inv <- x$getInverse()
+    if(!is.null(inv)){
+        message("Matriz ja criada - Buscando dados da memoria")
+        return(inv)
+    }
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setInverse(inv)
+    inv
 }
