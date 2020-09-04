c5041d014fed20dddefc857310f5b6be927f2fd2
SearchAndI <sninom@unal.edu.co>
2020-09-04 17:46:49 -0500

    with little specific changes in explanation

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## Creates an object, with setters and getters, in the form of a list. Because of closure (and the use of the <<-),
 ## it gives access to the variables from the parent environment.
-##It is meant to store a matrix and/or its inverse in memory; to be used by cachesolve(). 
+##It is meant to store a matrix, with or without its inverse,in memory; to be used by cachesolve(). 
 
 makeCacheMatrix <- function(A = matrix()) {
                    Ainv <- NULL
@@ -19,7 +19,7 @@ makeCacheMatrix <- function(A = matrix()) {
 
 
 ## Takes an object of the form given by makeCacheMatrix(), takes the inverse from memory if it exists, 
-## otherwise, it calculates it with inverse(), e.i. it assumes the matrix is invertible.
+## otherwise, it calculates it with solve(), e.i. it assumes the matrix is invertible.
 
 cacheSolve <- function(A, ...) {
         Ainv <- A$GetInverse()
