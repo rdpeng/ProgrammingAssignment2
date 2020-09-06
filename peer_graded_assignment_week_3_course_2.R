836789ce4dee172567783e7a3898fec24a5a0e19
Saad Ur Rehman <66938658+Hafiz-Saad-Rehman@users.noreply.github.com>
2020-09-06 21:18:10 +0500

    Add files via upload

---
+++ peer_graded_assignment_week_3_course_2.R
@@ -0,0 +1,34 @@
+## There are 2 functions makeCacheMatrix , cacheSolve
+## makeCacheMatric consists of set, get , setinverse , getinversse
+
+
+
+
+makeCacheMatrix <- function(x = matrix())
+{
+  inv <- NULL        # initializing inv as Null
+  
+  set <- function(y){
+                     x   <<- y
+                     inv <<- NULL
+                    }
+  get <- function(){x}
+  setInverse <- function(inverse) { inv <<- inverse}
+  getInverse <- function()  {inv}
+  list( set=set , get=get , setInverse=setInverse , getInverse=getInverse)
+  
+}
+
+
+cacheSolve <- function(x, ... )
+{
+  inv <- x$getInverse()
+  if (!is.null(inv)){
+                     message("your data is being cached")
+                     return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ... )
+  x$setInverse(inv)
+  inv                ## The last Expression gets printed
+}
