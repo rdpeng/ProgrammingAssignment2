34d20bf178ec1d15b81f17366a769e246d66bedf
Saad Ur Rehman <66938658+Hafiz-Saad-Rehman@users.noreply.github.com>
2020-09-06 21:47:15 +0500

    Week#3 assignment by Saad
    This is week#3 peer graded assignment of coursera R Programminh

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## There are 2 functions makeCacheMatrix , cacheSolve
+## makeCacheMatric consists of set, get , setinverse , getinversse
 
+
+
+
+makeCacheMatrix <- function(x = matrix())
+{
+        inv <- NULL        # initializing inv as Null
+        
+        set <- function(y){
+                          x   <<- y
+                          inv <<- NULL
+        }
+        get <- function(){x}
+        setInverse <- function(inverse) { inv <<- inverse}
+        getInverse <- function()  {inv}
+        list( set=set , get=get , setInverse=setInverse , getInverse=getInverse)
+        
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ... )
+{
+        inv <- x$getInverse()
+        if (!is.null(inv)){
+                message("your data is being cached")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ... )
+        x$setInverse(inv)
+        inv                ## The last Expression gets printed
 }
