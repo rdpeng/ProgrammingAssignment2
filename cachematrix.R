965a702bf95ec907024168a6fb731e070d0eeca1
Kamath-123 <karunapandit@nitte.edu.in>
2020-09-12 17:50:37 +0530

    Updated cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,25 @@
 ## The Project is to demonstrate the concept of lexical scoping 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(Inverse){ inv <<- Inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-    
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
 }
