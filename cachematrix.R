fbeb9e2fc77a9ea0d4162c47f4667d12bf78a92f
Nacho Mujica <53453987+NachoMujica@users.noreply.github.com>
2020-09-12 23:11:57 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## following the given example and hint of the makeVector function, I replicate its logic
+## to construct the matrix object.
+
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#to save computer power I assign the matrix x, values from other existing environment.
 
+makeCacheMatrix <- function(x = matrix()) 
+{
+        matrix_inverse <- NULL  
+        set <- function(y) 
+        {
+                x <<- y
+                matrix_inverse <<- NULL
+        }
+ take <- function() x   
+    setinv <- function(inverse) matrix_inverse <<- inverse
+    takeinv <- function() matrix_inverse
+    list(set = set, take = take, setinv = setinv, takeinv = takeinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## as specified in the example cachemean
+
+cacheSolve <- function(x, ...) 
+{
+     matrix_inverse <- x$takeinv()   #bring the inverse from the previous cached matrix
+    if (!is.null(matrix_inverse))   #if exist, display it
+    {       
+        message("getting cached data")
+        return(matrix_inverse)
+    }
+    mat <- x$take()
+    matrix_inverse <- solve(mat, ...) # else calculate the inverse with the solve function
+    x$setinv(matrix_inverse)
+    matrix_inverse
 }
+
