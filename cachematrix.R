e3eade87951fe0226af4ccddbca5ff137a2bcfef
roxtredev <roxana_trejos@yahoo.com>
2020-09-16 23:15:44 -0600

    Initial -  Functions added.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+rm(list = ls())
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  matrix_inverse <- NULL
+  
+  set <- function(y) {
+      x <<- y
+      matrix_inverse <<- NULL
+  }
+  
+  ## ls(environment(funs$set))
+  ## [1] "get"        "getInverse" "inv"        "set"        "setInverse" "x"
+  get <- function() x
+  setInverse <- function() matrix_inverse <<- solve(x) #calculate the inverse
+  getInverse <- function() matrix_inverse
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
-
-
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  matrix <- x$getInverse()
+  
+  if (!is.null(matrix)) {
+    message("getting cached inverse matrix")
+    return(matrix)
+  }
+  data <- x$get()
+  matrix <- solve(data, ...)
+  ##x$setInverse(matrix)
+  setInverse <- function() matrix <<- solve(x)
+  
+  matrix
 }
