ef059d34abb73ad3648bb89d09b4060d3a9de7a7
Jose Francisco Estañon Miranda <72104370+jfran20@users.noreply.github.com>
2020-10-06 16:49:16 -0500

    Changes to cachematrix
    These are the changes of the cachematrix assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These functions first, create a list of functions from which you can: set a matrix, get the matrix you put, 
+##set the inverse of the matrix and get the inverse previously calculated, notice that the only function that 
+##the user should access is $set(), since it's the only way to change the matrix (or by calling the function again with other argument). 
+#The second function, only returns the inverse, to do this first it checks if the argument 
+##(which have the matrix and the list of functions), already has an inverse value store at s,
+##if not it uses the $setinv() of the argument and passes to it solve(data) which
+##contains the inverse of the matrix
 
-## Write a short comment describing this function
+## Creates a list of functions and saves the matrix, also can contain the inverse if it has been already provided
+makeCacheMatrix <- function(x = matrix()){
+  
+  s <- NULL
+  set <- function(y) {
+    if(dim(y)[1]== dim(y)[2]){
+      x <<- y
+      s <<- NULL}
+    else {print("The matrix isn't square")}
+  }
+  get <- function() x
+  setinv <- function(inv) s <<- inv
+  getinv <- function() s
+  list(set=set,get = get,setinv = setinv,getinv = getinv)}
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getinv()
+  if(!is.null(s)) {
+    message("Getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setinv(s)
+  s
 }
