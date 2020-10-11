a702ada885278daabd4ff929539e14b755c051c7
Dustin Johnson <dfjohnson90@gmail.com>
2020-10-11 16:35:18 -0700

    Version 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall this functions takes a single argument that must be a matrix or
+## something that can be coerced into a matrix. It returns a list of pointers to
+## functions inside makeCacheMatrix. It makes the inverse of the matrix and
+## stores it in the object m.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  m <- NULL
+  set <- function(y) { ##set clears out m 
+    x <<- y
+    m <<- NULL
+  }
+  get <- function()x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list (set=set, get=get, setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##This function takes the list made from makeCacheMatrix as its input. It calls
+## the getinv function and puts it into m, tests to see if m is not null. If m
+## is not null it will return m. In this case m will be the inverse of the
+## matrix that was made in makeCacheMatrix. If m is NULL, the function gets the
+## the original matrix from makeCacheMatrix, 
+cacheSolve <- function(x,...){
+  m <- x$getinv()
+  if(!is.null(m)){ 
+    print("getting cached data")
+    return(m)
+  }
+  data <- x$get() 
+  m <- solve(data, ...) 
+  x$setinv(m) 
+  m 
 }
