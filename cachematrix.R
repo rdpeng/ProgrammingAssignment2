06a34e4b7b703b240250a620ce9be455be40166b
javier2511 <69739840+javier2511@users.noreply.github.com>
2020-10-13 22:10:59 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,32 @@
-## Put comments here that give an overall description of what your
+## funcio
 ## functions do
 
-## Write a short comment describing this function
-
+## functions that cache the inverse of a matrix, 
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 makeCacheMatrix <- function(x = matrix()) {
     inversa <- NULL
-    set <- function(y) {
-        x <<- y
-        inversa <<- NULL
+    set <- function(y) { #
+        x <<- y # 
+        inversa <<- NULL ##if null value is not applied in reverse
    }
-    get <- function() x
-    setinversa <- function(inversacalculada)(inversa <<- inversacalculada)
-    getinversa <- function() inversa
-    list(set = set, get = get, setinversa = setinversa,
-       getinversa = getinversa)
+    get <- function() x  #returns the value of the array that has
+    setinversa <- function(inversacalculada)(inversa <<- inversacalculada)  #receives the value of the calculated inverse
+    getinversa <- function() inversa  #observe the value of the calculated inverse
+    list(set = set, get = get, setinversa = setinversa, 
+       getinversa = getinversa)  #the list with the created functions is created
 }
 
 
 cacheSolve <- function(x, ...) {
-    inversa <- x$getinversa()
-    if(!is.null(inversa)) {
+    inversa <- x$getinversa()    #look for the data
+    if(!is.null(inversa)) {  #if it already exists it is obtained from cache, if it is not searched
          message("Datos obtenidos de cache")
-         return(inversa)
+         return(inversa)  #return inverse of X
     }
     
-    dato <- x$get()
-    inversa <- solve(dato, ...)
-    x$setinversa(inversa)
-    inversa
+    dato <- x$get()  # assign the value of list X in get to dato
+    inversa <- solve(dato, ...)  #calculate inverse
+    x$setinversa(inversa) 
+    inversa  #return inverse of X
 }
