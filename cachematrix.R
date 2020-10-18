1d7c31afbcda4357eaed3333ff1571bcc5c02a52
samanthaferrao13 <68849164+samanthaferrao13@users.noreply.github.com>
2020-10-18 11:50:38 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are 2 functions, makeCacheMatrix and cacheSolve, which cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## The below function, makeCacheMatrix, creates a special matrix object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mx = matrix()) {
+    mat <- NULL
 
+    # Sets the matrix
+    setMatrix <- function(matrix){
+        mx <<- matrix
+        mat <<- NULL
+    }
+    
+    # Returns the matrix
+    getMatrix <- function(){
+        mx
+    }
+    
+    # Inverts the matrix
+    setInverse <- function(inverse){
+        mat <<- inverse
+    }
+    
+    # Returns the inverse of the matrix
+    getInverse <- function(){
+        mat
+    }
+    
+    # Creates a list of the methods and returns it
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The below function takes in a special matrix 'x' returned by the makeCacheMatrix function
+## above, and computes its inverse. If the inverse of the matrix is already calculated and
+## the matrix is unchanged, the below function can retrieve this inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    # Returns a matrix that is the inverse of 'x'
+    mx <- x$getInverse()
+    
+    # Returns the inverse if already set
+    if(!is.null(mx)){
+        return(mx)
+    }
+    
+    # Retrieves the matrix from 'x' and calculates the inverse
+    matData <- x$getMatrix()
+    mx <- solve(matData) %*% matData
+    
+    # Sets the inverse of 'x' and returns it
+    x$setInverse(mx)
+    mx
 }
