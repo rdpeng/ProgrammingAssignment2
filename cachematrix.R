f0078cc27d9f6439e8b9189f948eac651af449f8
kanikaverma29apr <73843725+kanikaverma29apr@users.noreply.github.com>
2020-11-03 00:45:32 +0530

    second update

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+## I have made some changes but matrix inverse is not get.
+## Matrix is fetched.
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
