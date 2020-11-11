0e2bb388741b2c3869412a68ad47ccc7df132525
Alejandro-FB <48774286+Alejandro-FB@users.noreply.github.com>
2020-11-11 13:27:30 -0600

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,40 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+# Solve
+
+makeCacheMatrix <- function(x = matrix()) {
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+
+cacheSolve <- function(x, ...) {
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting inversed matrix")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
+}
+
+# Test
+
+my.matrix <- matrix(rnorm(25),5,5)
+
+my.matrix.1 <- makeCacheMatrix(my.matrix)
+
+cacheSolve(my.matrix.1)
