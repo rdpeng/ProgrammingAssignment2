8d5651afb85716502a1386e7c24214a56e94cb0f
felipeaguilera99 <76402589+felipeaguilera99@users.noreply.github.com>
2020-12-20 19:43:52 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <-  NULO
+  set <-  función ( y ) {
+    x  << -  y
+    inv  << -  NULO
+  }
+  obtener <-  función () { x }
+  setInverse <-  función ( inversa ) { inv  << -  inversa }
+  getInverse  <-  function () { inv }
+  lista ( set  =  set , get  =  get , setInverse  =  setInverse , getInverse  =  getInverse )
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'inv <-  x $ getInverse ()
+  if ( ! is.null ( inv )) {
+    mensaje ( " obteniendo datos encajados " )
+    volver ( inv )
+  }
+  mat <-  x $ obtener ()
+  inv <- resolver ( mat , ... )
+  x $ setInverse ( inv )
+  inv
 }
+
