b24c2a6a143058f0e2370472db951d07a67445af
sehunshi <77267244+sehunshi@users.noreply.github.com>
2021-01-11 17:05:19 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions given are assigned to cache the inverse of a matrix
+## Produces an object matrix which can cache its inverse 
 
-## Write a short comment describing this function
+makeCacheMatrix <- function( m- matrix () ) {
 
-makeCacheMatrix <- function(x = matrix()) {
+        ## Load the inverse property 
+       i <- NULL
+        
+        ##Procedure to view the matrix
+        set <- function (matrix) {
+                m <<- matrix
+                i <<- NULL 
+        }
 
+## Procedure to obtain the matrix 
+ get <- function () {
+     ## Return the matrix 
+      m
+}
+        
+## Procedure for the matrix to become an inverse 
+setInverse <- function(inverse) {
+           i <<- inverse
 }
 
+## Procedure to obtain the inverse of the matrix
+getInverse <- function() {
+      ## Return the inverse
+         i 
+}
+        
+## Return the following procedures 
+list (set = set, get = get,
+      setInverse = setInverse, 
+      getInverse = getInverse) 
+}
 
-## Write a short comment describing this function
-
+## Solve for the inverse of the matrix restored by the "makeCacheMatrix" from the previous procedure.  
+## The "cachesolve" should return the inverse from cache if the inverse is solved. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## Restore the matrix with the inverse of 'x'
+        m <- x$getInverse()
+        
+        ## Restore the inverse if it is prepared
+        if( !is.null(m) ) {
+                message("getting cached data")
+                return(m)
+        
 }
+  
+## Retrieve the matrix from item
+data <- x$get()
+        
+## Solve the incerse utilizing the multiplication of matrix 
+m <- solve(data) %*% data
+        
+## Place the inverse to item
+x$setInverse(m)
+        
+## Restore the matrix 
+m
+        
+}
+
