5b6567a5b38d50f2da93706a97b1a3f8b6149002
JoloAndaya <78014560+JoloAndaya@users.noreply.github.com>
2021-01-26 16:36:14 +0800

    Update and rename cachematrix.R to Project2for144.R

---
+++ Project2for144.R
@@ -0,0 +1,29 @@
+## INVERSE MATRIX
+## This creates a special matrix that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+## This computes the inverse of the matrix done by the commands above.
+cacheSolve <- function(x, ...) {
+        
+  ## Return an inverse matrix
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
+}
