30dac2dbf3d49228707be264fb17dcc37773de91
slazcanos <slazcanos@gmail.com>
2021-03-01 17:38:58 -0600

    Update cachematrix.R
    2nd activity R course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##This function receive a square matrix "x" and gives an special objetc
+##like a structure includind matrix "x"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mi <- NULL
+  set <- function(y) {
+    x <<- y
+    mi <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) mi <<- solve
+  getinverse <- function() mi
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The second function receive an "special object" with a square matrix.
+## store and return inverse matrix of X. If the matrix has not changed, 
+## inverse matrix is simply called from memory.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mi <- x$getinverse()
+  if(!is.null(mi)) {
+    message("getting cached data")
+    return(mi)
+  }
+  data <- x$get()
+  mi <- solve(data, ...)
+  x$setinverse(mi)
+  mi
 }
