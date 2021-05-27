64fe7e640e92183fdc1cccf381617789e8aa3fd0
chrispfeiffer9 <chrispfeiffer9@gmail.com>
2021-05-27 15:15:18 -0500

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,38 +1,43 @@
+#function to cache the inverse of a matrix
 makeCacheMatrix <- function(x = matrix() ) {
-         
-          #set/get matrix provided by user
           m <- NULL
+          
+          #set the value of the matrix
           set <- function(y) {
                     x <<- y
                     m <<- NULL
           }
+          
+          #get the value of the matrix
           get <- function() x
+          
+          #set the value of the inverse
           setsolve <- function(solve) m <<- solve
+          
+          #get the value of the inverse
           getsolve <- function() m
           list(set = set, get = get,
                setsolve = setsolve,
                getsolve = getsolve)
 }
 
+#function to calculate the inverse of the matrix as specified above
 cacheSolve <- function(x, ...) {
           m <- x$getsolve()
+          
+          #checks if the inverse has already been calculated. 
+          #if so, it pulls solution from cache.
           if(!is.null(m)) {
                     message("getting cached data")
                     return(m)
           }
           data <- x$get()
+          
+          #calculation of inverse matrix
           m <- solve(data, ...)
           x$setsolve(m)
           m
 }
-
-
-# #example of how makeVector works
-# mvec <- makeCacheMatrix()
-# x <- matrix(1:4, nrow=2, ncol=2)
-# mvec$set(x)
-# mvec$get()
-# mvec$getsolve()
 # mvec$setsolve(x)
 # mvec$getsolve()
 # 
