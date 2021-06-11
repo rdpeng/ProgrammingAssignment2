f2352814209886ddd44a4b28a645a23bd9483162
mygitsathya <85666695+mygitsathya@users.noreply.github.com>
2021-06-11 10:18:55 +0530

    Revert "Update cachematrix.R"
    This reverts commit 8a00150d969730ceea46803535879a4f0b95b5c8.

--- cachematrix.R
+++ cachematrix.R
@@ -4,28 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-   
- 
-  
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    inv <- x$getreverse()
-    if(!is.null(inv)) {
-      message("Get cache data.")
-      return(inv)
-    }else{
-      data <- x$get()
-      inv <- solve(data)
-      x$setreverse(inv)
-      inv
-  }
-  
- 
-  
         ## Return a matrix that is the inverse of 'x'
 }
