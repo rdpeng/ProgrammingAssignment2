3396959a601fe9162e2cf1a8355b42d99586aca4
Nickrou10 <86565826+Nickrou10@users.noreply.github.com>
2021-06-27 16:25:34 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##Function 1: makeCacheMatrix(x)
+##This function plays four critical roles (listed in sequential order)
+##Sets the value of the matrix using the double arrows to modify parent levels
+##Gets the value of the matrix
+##Sets the value of the inverse (uses double arrows again)
+##Gets the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inver <- NULL
+      set <- function(y) { 
+            x <<- y
+            inver <<- NULL
+            }
+      get <- function() {
+            x  } 
+      setinv <- function(inverse){
+            inver <<- inverse }
+      getinv <- function() {
+            inver }
+      list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
+##Function 2: cacheSolve(x,...)
+##This function will be evaluate the inverse of the matrix dvlped prvsly
+##It first checks to see if the "inver" is NULL
+##    -if it is not, it will retrieve the inverse from the cache
+##    -if it is, it will invert the matrix and "setinv"
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inver <- x$getinv()
+        if(!is.null(inver)) { 
+              message("getting cached data")
+              return(inver) }
+        results <- x$get()
+        inver <- solve(results, ...)
+        x$setinv(inver)
+        inver
 }
