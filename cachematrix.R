7ac05cc60aef61fff86e338bc3d009ecc741555d
AngeloRuelan <86650157+AngeloRuelan@users.noreply.github.com>
2021-07-02 15:12:55 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,24 +3,27 @@
 #cachematrix.r by Angelo Ruelan
 
 ## Write a short comment describing this function
-#In here, we utilize two function makeCacheMatrix, makeCacheMatrix
-#It is composed of set, get, setinv, getinv
+#In this code we have the function makeCacheMatrix
+#MakeCacheMatrix creates a matrix object  in order to cache its inverse
+#It is composed of set, get, setinv, getinv as we see in the code below
 
-makeCacheMatrix <- function(x = matrix()) { #Initiate the program with the default mode of matrix
-  k <- NULL #Initializing K as Null
+makeCacheMatrix <- function(x = matrix()) { #This function is used to initialize the program with the default mode of matrix
+  k <- NULL 
   set <- function(y){
-    x <<- y #Function to get Matrix
+    x <<- y 
     k <<- NULL #In a scenario in which a new matrix is created, this function resets k to null
   }
   get <- function() {x} #Defines the get function
-  setInverse <- function(inverse) (k <<- inverse) #Delegate the value of K
+  setInverse <- function(inverse) (k <<- inverse) 
   getInverse <- function() {k}
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
-## This function is used in order to get the cache data
+## As mentioned earlier, MakeCacheMatrix gets the inverse of its created matrix
+## CacheSolve on the other hand computes this inverse.
+## Basically, this is used in order to get the cache data
 
 cacheSolve <- function(x, ...) { #Returns the inverse of X
   k <- x$getInverse()
