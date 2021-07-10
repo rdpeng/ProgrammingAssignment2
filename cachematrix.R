a28ebe0a39841b5b4cb4de9fdba0f2362b0f4a8d
unael23 <leanu.tindugan0323@gmail.com>
2021-07-11 03:03:26 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##Creating the matrix
+##creating a function "invr" to solved value as null
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)){
+  invr <- NULL
+  crea <- function(y) {
+    x <<- y
+    invr <<- NULL
+  }
+  obt <- function() x
+  settheinverse <- function(inverse) {invr <<- inverse} ##As you can see the "mean" is changed to "inverse"
+  gettheinverse <- function() invr
+  list(crea = crea, obt = obt, settheinverse = settheinverse, gettheinverse = gettheinverse)
 }
-
-
-## Write a short comment describing this function
-
+##As i said earlier all "mean" function are changed to "inverse" which is "m" to "invr"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invr <- x$gettheinverse()
+  if(!is.null(invr)) {
+    message("calculating the inversed numeric") ##This message will appear when it tries to load
+    return(invr)
+  }
+  data <- x$obt()
+  invr <- solve(data, ...)
+  x$settheinverse(invr)
+  invr
 }
