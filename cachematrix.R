0bd0df5c83e475988d84f04a426d96e8bb4e5bdb
unael23 <leanu.tindugan0323@gmail.com>
2021-07-11 12:17:12 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,19 @@
 ##Creating the matrix
-##Setting up own variables for personel reference
+##Setting up own variables for personal reference
 ##creating a function "transpre" to solved value as null
-createCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)){
+createapattern <- function(x = matrix(sample(1:100,9),3,3)){
   transpre <- NULL
   crea <- function(y) {
     x <<- y
     transpre <<- NULL ## If the function is empty if the calculations has not cast
   }
-  obtai <- function() x
+  obtai <- function() x ## I used "obtai" variable to get the following calculations
   setthetransposed <- function(transposed) {transpre <<- transposed} ##As you can see the "mean" is changed to "transposed"
   gettheretroverted <- function() transpre
-  list(crea = crea, obtai = obtai, setthetransposed = setthetransposed, gettheretroverted = gettheretroverted)
+  list(crea = crea, obtai = obtai, setthetransposed = setthetransposed, gettheretroverted = gettheretroverted) ##List of my own variables created
 }
 ##As i said earlier all "mean" function are changed to "transposed" which is "m" to "transpre"
-cacheTransposed <- function(x, ...) {
+cacheTransposed <- function(x, ...) { ##Now to calculate the reversed retroverted numeric patterns
   transpre <- x$gettheretroverted()
   if(!is.null(transpre)) {
     message("calculating the inversed numeric") ##This message will appear when it tries to load the retroverted number and returns to the statement
@@ -25,3 +25,4 @@ cacheTransposed <- function(x, ...) {
   transpre
 }
 ##End of statement and repeats to the top
+##That's all
