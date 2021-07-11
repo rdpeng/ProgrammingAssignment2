e978a6cc6080f6401018a3149bfef93404fe05df
unael23 <leanu.tindugan0323@gmail.com>
2021-07-11 11:51:53 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,27 @@
 ##Creating the matrix
-##creating a function "invr" to solved value as null
-makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)){
-  invr <- NULL
+##Setting up own variables for personel reference
+##creating a function "transpre" to solved value as null
+createCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)){
+  transpre <- NULL
   crea <- function(y) {
     x <<- y
-    invr <<- NULL
+    transpre <<- NULL ## If the function is empty if the calculations has not cast
   }
-  obt <- function() x
-  settheinverse <- function(inverse) {invr <<- inverse} ##As you can see the "mean" is changed to "inverse"
-  gettheinverse <- function() invr
-  list(crea = crea, obt = obt, settheinverse = settheinverse, gettheinverse = gettheinverse)
+  obtai <- function() x
+  setthetransposed <- function(transposed) {transpre <<- transposed} ##As you can see the "mean" is changed to "inverse"
+  gettheretroverted <- function() transpre
+  list(crea = crea, obtai = obtai, setthetransposed = setthetransposed, gettheretroverted = gettheretroverted)
 }
-##As i said earlier all "mean" function are changed to "inverse" which is "m" to "invr"
-cacheSolve <- function(x, ...) {
-  invr <- x$gettheinverse()
-  if(!is.null(invr)) {
-    message("calculating the inversed numeric") ##This message will appear when it tries to load
-    return(invr)
+##As i said earlier all "mean" function are changed to "inverse" which is "m" to "transpre"
+cacheTransposed <- function(x, ...) {
+  transpre <- x$gettheretroverted()
+  if(!is.null(transpre)) {
+    message("calculating the inversed numeric") ##This message will appear when it tries to load the retroverted number and returns to the statement
+    return(transpre)
   }
-  data <- x$obt()
-  invr <- solve(data, ...)
-  x$settheinverse(invr)
-  invr
+  data <- x$obtai()
+  transpre <- solve(data, ...)
+  x$setthetransposed(transpre)
+  transpre
 }
+##End of statement
