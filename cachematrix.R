51c081bea5e1e0529dfb8c1226cbe23d87568258
Enrico-Miguel-Racelis <86454383+Enrico-Miguel-Racelis@users.noreply.github.com>
2021-07-11 21:12:35 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+	## First I set input x as "matrix"
+	## after that  I set the solved value "v" as a null
+	## then changed the reference from "mean" to "solve"
+	    makeCacheMatrix <- function(x = matrix(sample(1:50,8),5,5)) {
+	    v <- NULL
+	     set <- function(y) {
+	     x <<- y
+	     s <<- NULL
+	   }
+	   get <- function() x
+	   setsolve <- function(solve) v <<- solve
+	   getsolve <- function() v
+	   list(set = set, get = get,
+	        setsolve = setsolve,
+	        getsolve = getsolve)
+	 }
+	  ## I also did the same procedure here 
+          ## I changed "mean" to "solve" and "v" to "w"
+	  cacheSolve <- function(x, ...) {
+	    w <- x$getsolve()
+	    if(!is.null(w)) {
+	      message("getting inversed matrix")
+	      return(w)
+	    }
+	    data <- x$get()
+	    s <- solve(data, ...)
+	    x$setsolve(w)
+	    s
+	  }
