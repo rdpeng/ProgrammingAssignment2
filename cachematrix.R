218c264aea07529bc09a5b63c0314e15a4116d16
LinChun000 <87946877+LinChun000@users.noreply.github.com>
2021-07-26 18:44:47 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#Create the function that have as argument a matrix called x:
 
 makeCacheMatrix <- function(x = matrix()) {
         inverse <- NULL
     set <- function(y){
-        x <- y
-        inverse <- NULL
+        x <<- y
+        inverse <<- NULL
     }
+    # get is a function that gets the matrix x:
     get <- function()x
-    setinverse <- function(inverse) inverse
+    
+    # set the inverse of the matrix, assign the name of inverse to it:
+    setinverse <- function(inverse) inverse<<-inverse
+    
+    #getinverse returns the inverse variable 
     getinverse <- function() inverse
+    
+    #The function makeCachematrix returns a list of methods defined above:
     list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
-
-
 }
 
-
-## Write a short comment describing this function
-
+ ## cacheSolve return a matrix that is the inverse of 'x'
+#makeCachematrix will be passed as argument in this function:
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inverse <- x$getinverse()
+    #If inverse variable is not null, R will return the following message and the inverse matrix   
+    inverse <- x$getinverse()
     if(!is.null(inverse)){
         message('getting cached data')
         return (inverse)
     }
+    #get the matrix x from the previous function:
     data <- x$get()
+    #calculate the inverse matrix and store the result in a variable called inverse:
     inverse <- solve(data,...)
+    
+    #Set the argument for setinverse equal to inverse (the inverse matrix)
     x$setinverse(inverse)
+    #Return the variable that contains the inverse matrix
     inverse
 }
