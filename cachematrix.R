896d69c5d81d223c53eee1617a4368337a72c715
arlja <92099754+arlja@users.noreply.github.com>
2021-10-11 13:16:47 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,50 +1,44 @@
-<<-  In this example we introduce the <<- operator which can be used to assign a value to an object in an environment that is different from the current environment. Below are two functions that are used to create a special object that stores and calculate derivatives of x and y with the given functions and cache's its mean.
+Assignment: Caching the Inverse of a Matrix
+less 
+Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
 
-The first function, makeVector creates a special “vector”, which is really a list containing a function to:
-1. set the derivative of x
-2. get the derivative of x
-3. set the derivative of y
-4. get the derivative of y
+Write the following functions:
+
+Write the following functions:
+
+<<- makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+
+   
+Answer:
+     makeCacheMatrix <- function(x = matrix()) {
+      inv <- NULL
+     set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+     get <- function() x
+      setInversematrix <- function(solveMatrix) inv <<- solveMatrix
+      getInversematrix <- function() inv
+     list(set = set, get = get, 
+         setInversematrix = setInversematrix, getInversematrix = getInversematrix) 
+         }
+
+
+<<- cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 Answer: 
-
-makeCacheFunction < - function [Curvature_T] = given(V_x.V_y,2) {
-%Input the value of x and y
-%Input the value of time for t
-%Define the radius of Curvature T    }
-
-
-Curvature_T=[(V_x1^2+V_y1^2)^(1/2)/([V_x1*V_y2]-(V_y1*V_x2))];
-
-{    
-get <- Derivative2x = diff(V_x,2);
-           V_x2 = subs(Derivative2x_1);
-setDerivative <- Derivative2x = diff(V_x,2) <<- solveDerivative
-
-get <- Derivative2y= diff(V_x,2);
-           V_y2= subs(Derivative2y,1);
-setDerivative <- Derivative2x = diff(V_x,2) <<- solveDerivative
-
-list(set = set, get = get, 
- 
-             setDerivative = setDerivative, getDerivative = getDerivative)   }
-
-
-<<- The following function calculates the mean of the special "vector" created with the above function. However, it first checks to see if the mean has already been calculated. If so, it gets the mean from the cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
-
-
-The following  calculates the derivative of "x and y” created with the above function. 
-
-Answer:
-
-cachemean <- function(x,y, ...) {
-        d <- x$getDerivative()
-        if(!is.null(d)) {
-                message("getting cached data")
-                return(d)
-        }
-        data <- x$get()
-        d <- Derivative(data, ...)
-        x$setDerivative(V_x,V_y)
-        m
+     cacheSolve <- function(x, ...) {
+     ## Return a matrix that is the inverse of 'x'
+     inv <- x$getInversematrix()
+     if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+     data <- x$getinverse()
+      inv <- solve(data)
+     x$setInversematrix(inv)
+     inv      
 }
+
+For this assignment, assume that the matrix supplied is always invertible.
+
