f55af800c7fe661fe092f706dbe3e794a5ac4f7d
Yiming Shen <56240016+Yiming-S@users.noreply.github.com>
2021-11-01 18:07:33 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,40 +4,51 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL                                ## "inv" is the variable for the inverse matrix
         
-        set <- function(y) {                       ## the function sets the value of the matrix,                            
-                x <<- y                            ## of which the inverse will be calculated
+        ## "inv" is the variable for the inverse matrix
+        inv <- NULL                                
+        
+        ## the function sets the value of the matrix, of which the inverse will be calculated 
+        set <- function(y) {                                               
+                x <<- y                           
                 inv <<- NULL
         }
-        get <- function() x                        ## the function returns the value of the matrix  
-        setinv <- function(solve) inv <<- solve    ## calculating the inverse of the matrix, storing the
-                                                   ## inverse matrix in the variable "inv"
         
-        getinv <- function() inv                   ## return the value of the inverse matrix
-        list(set = set, get = get,                 ## the created vector (list) which contains the functions
+        ## the function returns the value of the matrix
+        get <- function() x     
+        
+        ## calculating the inverse of the matrix, storing the inverse matrix in the variable "inv"
+        setinv <- function(solve) inv <<- solve    
+                                                   
+        ## return the value of the inverse matrix
+        getinv <- function() inv                   
+        list(set = set, get = get,                
              setinv = setinv,
              getinv = getinv)
 
 }
 
-
-## This functions calculates the inverse of a matrix created with the above function.
-## It first checks to see if the inverse matrix has already been calculated. 
-## If so, it geit's the inverse matrix from the cache and skips the computation.
-## Otherwise, it calculates the inverse of the matrix and sets the value of the 
-## inverse matrix in the cache via the 'setinv' function. 
-
-## The function returns a matrix that is the inverse of 'x'
+####
+# This functions calculates the inverse of a matrix created with the above function.
+# It first checks to see if the inverse matrix has already been calculated. If so, it geit's the inverse matrix from the cache and skips the computation. 
+# Otherwise, it calculates the inverse of the matrix and sets the value of the inverse matrix in the cache via the 'setinv' function. 
+# The function returns a matrix that is the inverse of 'x'
+####
 
 cacheSolve <- function(x, ...) {
-        inv <- x$getinv()                         ## getting the inverse matrix out of the cache
-        if(!is.null(inv)) {                       ## checking if the inverse matrix is stored in the cache
+        ## getting the inverse matrix out of the cache
+        inv <- x$getinv()     
+        
+        ## checking if the inverse matrix is stored in the cache
+        if(!is.null(inv)) {                       
                 message("getting cached data")
-                return(inv)                       ## returning the inverse matrix
+                
+                ## returning the inverse matrix
+                return(inv)                       
         }
-        data <- x$get()                           ## getting the matrix to calclulate the inverse if it wasn't 
+        
+        data <- x$get()                           ## calclulate the inverse if it wasn't 
         inv <- solve(data, ...)                   ## stored in the cache
         x$setinv(inv)                             ## calling the setinv function to store the inverse matrix in the cache
-        inv                                       ## Return a matrix that is the inverse of 'x'       
+        inv                                       ## Return the inverse of 'x'       
 }
