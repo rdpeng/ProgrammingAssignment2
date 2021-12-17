4119812cdc4a1c6d6381046a745a900583d2119a
Gutama92 <96239952+Gutama92@users.noreply.github.com>
2021-12-17 21:49:37 +0300

    My codes
    I wrote the codes and tested them

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## First I wrote the codes that can cache the inverse of matrix
+## Then I generated 2 by 2 matrix and tried codes and they worked
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set<- function(y){
+                x<<-y
+                inv<<-NULL
+        }
+        get <- function() {x}
+        setinverse <- function(inverse) {inv <<- inverse}
+        getinverse <- function() {inv}
+        list(set = set, get = get, setinverse = setinverse, 
+             getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setinverse(inv)
+        inv
 }
+
+mymatrix<-makeCacheMatrix(matrix(5:8, nrow=2, ncol=2)) # generating matrix
+mymatrix$get() # to get my matrix
+mymatrix$getinverse() # returns NULL because it is not cached yet
+cacheSolve(mymatrix) # computes the inverse and caches it
+cacheSolve(mymatrix) # "getting cached data"
+mymatrix$getinverse() # Cached perfectly!
