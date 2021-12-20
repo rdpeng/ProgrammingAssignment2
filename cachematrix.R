90e84b9122837b3f3f4cfb29b6c1b01695a081d4
Diones-Leidens <dionesjleidens@gmail.com>
2021-12-20 11:19:07 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
 ##The first function, makeVector creates a special "vector", which is really a list containing a function to
 
