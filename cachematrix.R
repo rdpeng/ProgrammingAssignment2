86bc37478f1a11c23e1fa380583916b252ae0bf4
dhaye7 <delano.haye7@gmail.com>
2022-01-25 10:00:36 -0500

    updated Lex. Scoping Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,19 @@
+
+
+
 ## Below are two functions:
 
 ## 1st = `makeCacheMatrix`: This function creates a special "matrix" object
 ##                           that can cache its inverse.
 
-## 2nd = .  `cacheSolve`: This function computes the inverse of the special
+## 2nd =   `cacheSolve`: This function computes the inverse of the special
 ##                       "matrix" returned by `makeCacheMatrix` above. If the inverse has
 ##                        already been calculated (and the matrix has not changed), then
 ##                       `cacheSolve` should retrieve the inverse from the cache.
 
 
 
-## makecacheMatrix
+## 1st makecacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
     s <- NULL
@@ -19,33 +22,28 @@ makeCacheMatrix <- function(x = matrix()) {
         s <<- NULL
     }
     get <- function() x
-    setinverse <- function(solve) s <<- solve
-    getinverse <- function() s
+    setsolve <- function(solve) s <<- solve 
+    getsolve <- function() s
     list(set = set, get = get,
-         setinverse = setinverse,
-         getinverse = getinverse)
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## cacheSolve
+## 2nd cacheSolve
 
 
 cacheSolve <- function(x, ...) {
-    s <- x$getinverse()
+    s <- x$getsolve()
     if(!is.null(s)) {
         message("getting cached data")
         return(s)
     }
     data <- x$get()
     s <- solve(data, ...)
-    x$setinverse(s)
+    x$setsolve(s)
     s
 }
-
-
-
-
-
 
 
 
