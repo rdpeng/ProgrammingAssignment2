a106447959b486b0df4a7cc5bf8064a89b024a52
AmalWanigatunga <awaniga1@jhu.edu>
2022-06-23 22:24:10 -0400

    For matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Taking two function to first set callable global values of a matrix 
+## to be cached and then outputting the inverse of that matrix
 
-## Write a short comment describing this function
+## Function to set of global environment values for the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL                   
+  set <- function(y) {        
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
+## Call global parameters to generate the inverse of the matrix 
+## from the function makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   i <- x$getinverse()
+   if (!is.null(i)) {
+     message ("getting cached data")
+     return(i)
+   }
+   data <- x$get()
+   i <- solve(data, ...)
+   x$setinverse(i)
+   i
 }
