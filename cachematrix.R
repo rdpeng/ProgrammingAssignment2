e24cde9b365692be36e2da78907abf81adfaf61d
momentumg <109006272+momentumg@users.noreply.github.com>
2022-07-14 06:51:34 +0500

    cachematrix file uploaded

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
 
+## x is a matrix here and s is NULL
+##  "mean" is referencing to "mymean"
+
+
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setmymean <- function(solve) s <<- solve
+  getmyymean <- function() s
+  list(set = set, get = get,
+       setmymean = setmymean,
+       getmyymean = getmyymean)
 }
 
 
-## Write a short comment describing this function
+##  changed "mean" to "mymean"
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+
+cachemymean <- function(x, ...) {
+  s <- x$getmyymean()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setmymean(s)
+  s
 }
