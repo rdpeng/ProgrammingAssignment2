6f46865c96fa2b622009b34ba5b151ce9b4d39f7
suneeth1604 <suneethaneerumalla@gmail.com>
2022-09-08 20:29:16 +0530

    assign2:lexicalscoping

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function()x
+        setinver<-function(mat)m<<-mat
+        getinver<-function()m
+        list(set=set,get=get,setinver=setinver,getinver=getinver)
+        
 
 }
 
@@ -12,4 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<-x$getinver()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data<-x$get()
+        m<-solve(x,...)
+        x$setinver(m)
+        m
+
 }
