7422d7480520e23498387f169cea87016231b579
Reyna99 <115951225+Reyna99@users.noreply.github.com>
2022-10-16 16:08:48 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+setwd('/Users/Reyna/Documents')
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## I set the input x as a matrix and then set the new solved value (v) to be null
+## Then every reference of mean is changed to solve 
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  v <- NULL
+  set <- function(y) {
+    x <<- y
+    v <<- NULL
 }
-
-
-## Write a short comment describing this function
-
+  get <- function() x
+  setsolve <- function(solve) v <<- solve
+  getsolve <- function() v
+  c(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+## Just changed mean to solve 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  v <- x$getsolve()
+  if(!is.null(v)) {
+    message("getting inversed matrix")
+    return(v)
+  }
+  data <- x$get()
+  v <- solve(data, ...)
+  x$setsolve(v)
+  v
 }
+
