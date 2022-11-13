a310fa4259a84cc8f4aa87fad96454bee37aa49e
gam32bit <joseph.caterine@gmail.com>
2022-11-13 16:10:15 -0600

    Updated comments and changed "m" mean variables and references to "i" inverse variables and references

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions that work together to expedite computation of inverting matrices.
 
-## Write a short comment describing this function
+## First function makes special object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  ## where the magic happens ~*~*~
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  # retrieve value of x
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  ## second magic part
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if inverse of matrix from first function cached
+## If not, calculates inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
