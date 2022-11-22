6c6edcd94c5521d7881ee3d4ef9cca3a1a87a40d
Eddie Jonathan García Borbón <edjgarciabo@unal.edu.co>
2022-11-23 01:59:11 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversa <- NULL
+  set <- function(y){
+    x<<-y
+    inversa<<-NULL
+  }
+  get<-function()(x)
+  setInversa <- function(inversaCalculada)(inversa<<-inversaCalculada)
+  getInversa <- function(){inversa}
+  list(set = set, get = get,
+       setInversa = setInversa,
+       getInversa = getInversa) 
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inversa <- x$getInversa()
+  if(!is.null(inversa)){
+    message("getting cahed data")
+    return(inversa)
+  }
+  data <-x$get()
+  inversa <- solve(data, ... )
+  x$setInversa(inversa)
+  inversa
 }
