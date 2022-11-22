0ecf8b3afde0b7b881bdebe11f1112d857fa1529
SophieSuSu <118785398+SophieSuSu@users.noreply.github.com>
2022-11-22 17:05:39 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create matraix and compute its inverse matrix
+## clean the working space
+ls()
+rm(list=ls())
 
-## Write a short comment describing this function
+## Create a function called makeCacheMatrix to create matraix
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inversion
+## 4. get the value of the inversion
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## set x as default value
+makeCacheMatrix <- function(x = matrix(c(1,2,3,4,5, 11,12,13,14), nrow = 3, ncol = 3,)) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinver <- function(solve) m <<- solve
+  getinver <- function() m
+  list(set = set, get = get,
+       setinver = setinver,
+       getinver = getinver)
 }
 
 
-## Write a short comment describing this function
-
+## Create a function called cacheSolve to compute inverse matraix
+## x is the returned matrix by calling makeCacheMatrix 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinver()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinver(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
 }
+
+## Test and validate the functions
+x <- makeCacheMatrix(); x
+data <- x$get(); data
+data1 <-x$getinver(); data1
+inver <- cacheSolve(x); inver
+chk <- round(data %*% inver,3); chk
