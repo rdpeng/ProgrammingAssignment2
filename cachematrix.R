3a7138dec2a93de38a114eaa461c6186a4091fa1
Macarena Ch Bykov <bykov_make@hotmail.com>
2023-03-07 20:34:07 -0300

    make cache matrix and solve cache functions.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ m <- NULL
+ set <- function(y) {
+   x <<- y
+   m <<- NULL
+ }
+ 
+ get <- function() x
+ 
+ seti <- function(inv) m <<- inv
+ geti <- function() m
+ 
+ list( set = set, get = get,
+       seti = seti , geti = geti)
 }
 
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$geti()
+  if(!is.null(m)){
+     message("Cache Inversed Matrix")
+     return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data)
+  x$seti(m)
+  m
 }
