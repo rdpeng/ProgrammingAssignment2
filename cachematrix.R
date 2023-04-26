b097df027f8ad629fce7506ffeba575ad5d52508
sanjana-bot114 <sanjanaeagala14@gmail.com>
2023-04-26 22:49:45 +0530

    cachematrix.R
    My R Programming Week 3 Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,130 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R ---> by Sanjana Sri
+## My aim in this experiment is to write a pair of functions, namely,
+#"makeCacheMatrix" and "cacheSolve" that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function Function makeCacheMatrix gets a
+#matrix as an input, set the value of the matrix,
+#get the value of the matrix, set the inverse Matrix
+#and get the inverse Matrix. The matrix object
+#can cache its own object.
+
+#<<- operator is used to assign a value to an object in an
+#environment that is different from the current environment
+
+#take the matrix as an input
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        invMatrix <- NULL
 
+#set the value of the Matrix
+  setMatrix <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+
+  getMatrix <- function() x
+  setInverse <- function(inverse) invMatrix <<- inverse
+  getInverse <- function() invMatrix
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve takes the output of the previous matrix makeCacheMatrix(matrix) as an input and checks inverse matrix  # nolint: line_length_linter.
+# from makeCacheMatrix(matrix) has any value in it or not.
+# In case inverse matrix from makeCacheMatrix((matrix) is empty, it gets the
+# original matrix data from and set the invertible matrix by using the solve function. # nolint: line_length_linter.
+# In case inverse matrix from makeCacheMatrix((matrix) has some value in it
+# (always works after running the code 1st time), it returns a message
+# "Getting Cached Invertible Matrix"and the cached object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       #get the value of the invertible matrix from the makeCacheMatrix function
+          invMatrix <- x$getInverse()
+        if(!is.null(invMatrix)) {
+          message("Getting Cached Invertible Matrix")
+          return(invMatrix)
+        }
+
+#if value of the invertible matrix is NULL then
+        MatrixData <- x$getMatrix()
+        invMatrix <- solve(MatrixData, ...)
+        x$setInverse(invMatrix)
+        return(invMatrix)
 }
+
+#########Testing##########
+
+####Test 1 [2*2 Matrix] #####
+TestMatrix <- matrix(1:4, 2, 2)
+TestMatrix
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
+cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+
+
+####Test 2 [2*2 Matrix] #####
+TestMatrix <- matrix(c(1, 5, 8, 2), 2, 2)
+TestMatrix
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
+cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+
+
+####Test 3 [3*3 Matrix] --> singulat Matrix #####
+#matrix(1:9,3,3) is not possible (singule matrix) because it is giving det(A)  = 0
+#matrix = 1/det(A)[3,-6,3,-6,12,-6,3,-6,3] |det(A) = 1/(1*3 +4* (-6) + 7 *3) = 1/0
+TestMatrix <- matrix(1:9, 3, 3)
+TestMatrix
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
+cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+
+####Test 4 [3*3 Matrix]#####
+TestMatrix <- matrix(1:8, 3, 3)
+TestMatrix
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
+cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+
+
+####Test 5 [4*4 Matrix]#####
+TestMatrix <- matrix(c(2,3,5,1,3,7,4,5,6,8,0,0,4,5,6,0),4,4)
+TestMatrix
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
+cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+
+
+####Test 6 [4*4 Matrix]  --> singular Matrix #####
+TestMatrix <- matrix(5:21, 4, 4)
+TestMatrix
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
+cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
