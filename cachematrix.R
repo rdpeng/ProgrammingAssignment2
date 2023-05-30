25f4b0cbe60c0923c65a0644059b949a1cf2954a
AliiRaouf <125336077+AliiRaouf@users.noreply.github.com>
2023-05-30 12:36:17 -0400

    Update cachematrix.R
    I guess this way the result is achieved more easily.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ res <- NULL
+set <- function(y){
+  x <<- y
+  res <<- NULL
+}
+get <- function() x
+setsolve <- function(solve) res<- solve()
+getsolve <- function() res
+list(set = set, get = get,
+     setsolve = setsolve,
+     getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        if(!is.na(res)){
+    print("return Data from Cache")
+    print (res)
+  }else{
+  res <- solve(x)
+  return(res)              
+  }
         ## Return a matrix that is the inverse of 'x'
 }
