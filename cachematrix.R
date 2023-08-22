f4a67f1919bf7a99684fa514089615d46c921679
dk7919 <dmuthui@jkuat.ac.ke>
2023-08-22 07:33:40 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 
 # loading data and used MASS dataset 
 library(MASS)
