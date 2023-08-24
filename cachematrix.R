1dba1108566d5ca14159d6ab271a9665dce96777
Adarsh Godia <139886076+Aadii43@users.noreply.github.com>
2023-08-24 21:56:20 +0530

    Implement solution for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix) {
+  inv <- NULL
+  
+  set <- function(newValue) {
+    matrix <<- newValue
+    inv <<- NULL  # Reset cached inverse
+  }
+  
+  get <- function() {
+    matrix
+  }
+  
+  cacheInverse <- function() {
+    if (!is.null(inv)) {
+      message("Getting cached inverse")
+      return(inv)
+    }
+    
+    inv <<- solve(matrix)
+    inv
+  }
+  
+  list(set = set, get = get, cacheInverse = cacheInverse)
+}
+cacheSolve <- function(cacheMatrix) {
+  if (!is.function(cacheMatrix$cacheInverse)) {
+    stop("Input must be a cacheMatrix object.")
+  }
+  
+  cacheMatrix$cacheInverse()
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
