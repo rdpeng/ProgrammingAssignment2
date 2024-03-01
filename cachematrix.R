a4cf6d245f1e524e6cfc470ef948072ae597ea7a
mahi1619 <80911034+mahi1619@users.noreply.github.com>
2024-03-01 13:11:34 +0000

    i have subbmited my programming assignment on catching the inverse of a matrix, please comment on it, Thanks git push

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
@@ -20,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
              getInverse = getInverse)
 
 }
-## Write a short comment describing this function
+
 ## This function computes the inverse of the special "matrix" created by 
 ## makeCacheMatrix above. If the inverse has already been calculated (and the 
 ## matrix has not changed), then it should retrieve the inverse from the cache.
