5a65a5453d65d6365dbeb052f5a11deae1897e15
RodrigoPolidoro <rodrigo.hpolidoro@gmail.com>
2024-03-25 20:47:33 -0300

    Added code for solving the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Clear environment objects before running the code
+#rm(list = ls())
 
-## Write a short comment describing this function
-
+## Function to create a cacheable matrix along with its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # Initialize a variable to store the inverse
+  inv <- matrix()
+  
+  # Function to set the matrix and its inverse
+  set <- function(y) {
+    # Assign the matrix to 'x' in the enclosing environment
+    x <<- y
+    # Initialize the inverse matrix
+    inv <<- matrix()
+  }
+  
+  # Function to retrieve the matrix
+  get <- function() x
+  
+  # Function to compute and set the inverse
+  setinv <- function(x) inv <<- solve(x)
+  
+  # Function to retrieve the inverse
+  getinv <- function() inv
+  
+  # Return a list of functions
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## Function to compute the inverse of a matrix, caching the result if available
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Retrieve the cached inverse and original matrix
+  inv <- x$getinv()
+  
+  # If the inverse is cached, print message and return the cached inverse
+  if(!all(is.na(inv))) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # Retrieve the matrix
+  data <- x$get()
+  
+  # Compute the inverse
+  inv <- x$setinv(data)
+  
+  # Return the computed inverse
+  inv
 }
+
+# Tests
+m <- matrix(c(2, 1, 3, 4), 2, 2)
+inverse1 <- makeCacheMatrix()
+inverse1$set(m)
+original1 <- inverse1$get()
+inverse1$setinv(original1)
+inverse1$getinv()
+cacheSolve(inverse1)
+
+inverse2 <- makeCacheMatrix()
+inverse2$set(m)
+original2 <- inverse2$get()
+cacheSolve(inverse2)
+
+
